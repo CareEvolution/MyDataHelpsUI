f2c53053cf66c0e34582f299152457bd18b054ef
ajmenca <adam@careevolution.com>
2023-03-03 10:44:24 -0500

    Fix Image/ImageIcon Stories

--- src/components/step/StepImage/StepImage.stories.tsx
+++ src/components/step/StepImage/StepImage.stories.tsx
@@ -1,37 +1,27 @@
 ﻿import React from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
-import StepDetailText from "./StepImage";
-import {StepElementProps} from "../shared"
+import StepImage from "./StepImage";
+import {IconElementProps} from "../shared"
 import StepLayout from "../StepLayout";
 
 export default {
-    title: "SurveyStep/Components/StepDetailText",
-    component: StepDetailText,
+    title: "SurveyStep/Components/StepImage",
+    component: StepImage,
     parameters: {
         layout: 'fullscreen',
     }
-} as ComponentMeta<typeof StepDetailText>;
+} as ComponentMeta<typeof StepImage>;
 
-const Template: ComponentStory<typeof StepDetailText> = (args: StepElementProps) =>
+const Template: ComponentStory<typeof StepImage> = (args: IconElementProps) =>
     <StepLayout>
-        <StepDetailText {...args} />
+        <StepImage {...args} />
     </StepLayout>
 
-export const StepDetailTextDefault = Template.bind({});
-StepDetailTextDefault.args = {
-    text: "This is the detail text"
+export const StepImageIconNoSrc = Template.bind({});
+StepImageIconNoSrc.args = {
 }
 
-export const StepDetailTextMarkdown = Template.bind({});
-StepDetailTextMarkdown.args = {
-    text: "\\*\\*Bold\\*\\*\n**Bold**\n\n\\_Italics\\_\n_Italics_\n\n\\*\\*\\_Bold Italics\\_\\*\\*\n**_Bold Italics_**\n\n\\<http://www.example.com\\>\n<http://www.example.com>\n\n\\<user@example.com>\n<user@example.com>\n\n\\[Example](http://www.example.com)\n[Example](http://www.example.com)\n\n\\[123-555-1234](tel:123-555-1234)\n[123-555-1234](tel:123-555-1234)\n\nBullet list with `+`, `-`, or `*`. Indent 2 spaces for sublist.\n- Level 1, Item A\n- Level 1, Item B\n  - Level 2, Item A\n  - Level 2, Item B\n    - Level 3, Item A"
-}
-
-export const StepDetailTextCustomStyle = Template.bind({});
-StepDetailTextCustomStyle.args = {
-    text: "This is the detail text",
-    textAlign: "Left",
-    color: "#0000FF",
-    fontSize: "24",
-    fontWeight: "300"
+export const StepImageIconWithSrc = Template.bind({});
+StepImageIconWithSrc.args = {
+    srcUrl: "https://rkstudio-customer-assets.s3.amazonaws.com/CareEvolution/Images/MDH-Logo-2022.svg"
 }
