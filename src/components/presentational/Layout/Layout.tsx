edfbe8d67dbfe20d53e1330b9c3dc29076ac1216
Chris Nowak <chris@careevolution.com>
2023-04-02 10:37:16 -0400

    Global style injection

--- src/components/presentational/Layout/Layout.tsx
+++ src/components/presentational/Layout/Layout.tsx
@@ -1,14 +1,20 @@
 import React, { createContext } from 'react';
 import MyDataHelps, { StatusBarStyle } from '@careevolution/mydatahelps-js';
+import { Global as EmotionGlobal, css } from '@emotion/react';
 import "./Layout.css"
+import { darkMode, global, defaultVariables } from '../../../helpers/globalCss';
 
 export interface LayoutProps {
 	children?: React.ReactNode;
-	stylesheetPath?: string;
 	bodyBackgroundColor?: string;
 	statusBarStyle?: StatusBarStyle;
 	className?: string;
 	autoDarkMode?: boolean;
+	normalizeCss?: boolean;
+	noGlobalStyles?: boolean;
+	
+	//Deprecated
+	stylesheetPath?: string;
 }
 
 export interface LayoutContext {
@@ -19,10 +25,7 @@ export const LayoutContext = createContext<LayoutContext>({ darkMode: false });
 
 export default function (props: LayoutProps) {
 	let className = "mdhui-layout";
-	if (!props.autoDarkMode) {
-		className += " mdhui-layout-auto-dark-mode";
-	}
-	if(props.className) {
+	if (props.className) {
 		className += " " + props.className;
 	}
 
@@ -43,6 +46,13 @@ export default function (props: LayoutProps) {
 
 	return (
 		<LayoutContext.Provider value={context}>
+			<EmotionGlobal styles={defaultVariables} />
+			{!props.noGlobalStyles &&
+				<EmotionGlobal styles={global} />
+			}
+			{!props.autoDarkMode &&
+				<EmotionGlobal styles={darkMode} />
+			}
 			<div className={className}>
 				{props.stylesheetPath &&
 					<link rel="stylesheet" type="text/css" href={props.stylesheetPath} />
