d615f4ced63f397c264510e754fd3179debf3e8e
Chris Nowak <chris@careevolution.com>
2023-04-03 14:14:11 -0400

    Final customizable properties

--- src/components/presentational/LoadingIndicator/LoadingIndicator.tsx
+++ src/components/presentational/LoadingIndicator/LoadingIndicator.tsx
@@ -8,11 +8,19 @@ import { ProjectSupport } from '../../container';
 export interface LoadingIndicatorProps {
 	variant?: "default" | "inline";
 	color?: string;
+	className?: string;
 }
 
 function LoadingIndicator(props: LoadingIndicatorProps) {
+	let classes = ["mdhui-loading-indicator"];
+	if (props.className) {
+		classes.push(props.className);
+	}
+	if (props.variant == "inline") {
+		classes.push("mdhui-loading-indicator-inline");
+	}
 	return (
-		<div style={{ color: props.color }} className={"mdhui-loading-indicator" + (props.variant == "inline" ? " mdhui-loading-indicator-inline" : "")}>
+		<div style={{ color: props.color }} className={classes.join(" ")}>
 			<FontAwesomeSvgIcon icon={faRefresh} spin />
 		</div>
 	);
