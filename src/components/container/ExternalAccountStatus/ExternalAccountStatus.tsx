7626f008a8634fd83e270664686cceab6a7612b4
Chris Nowak <chris@careevolution.com>
2023-05-07 10:32:55 -0400

    Fixup external account status

--- src/components/container/ExternalAccountStatus/ExternalAccountStatus.tsx
+++ src/components/container/ExternalAccountStatus/ExternalAccountStatus.tsx
@@ -31,7 +31,30 @@ export default function (props: ExternalAccountStatusProps) {
     let [isWeb, setIsWeb] = useState<boolean>(false);
     let [externalAccounts, setExternalAccounts] = useState<ExternalAccount[]>([]);
 
+    function refresh() {
+        MyDataHelps.getExternalAccounts().then(function (accounts) {
+            if (!!props.externalAccountCategories) {
+                accounts = accounts.filter(account => props.externalAccountCategories!.includes(account.provider.category));
+            }
+            accounts.forEach(account => {
+                if (account.status == 'fetchComplete' && isAfter(new Date(), add(parseISO(account.lastRefreshDate), { hours: 3 }))) {
+                    MyDataHelps.refreshExternalAccount(account.id);
+                    account.status = 'fetchingData';
+                }
+            });
+            setExternalAccounts(accounts);
+        });
+    }
+
     function initialize() {
+        MyDataHelps.getDeviceInfo().then(function (deviceInfo) {
+            setIsWeb(deviceInfo.platform == "Web");
+        }).catch(function () {
+            setIsWeb(true);
+        }).finally(function () {
+            refresh();
+        });
+
         if (props.previewState == "externalAccountsFetchingData") {
             previewStateAccounts[0].status = "fetchingData";
             setExternalAccounts(previewStateAccounts);
@@ -47,62 +70,29 @@ export default function (props: ExternalAccountStatusProps) {
             setExternalAccounts(previewStateAccounts);
             return;
         }
-
-        MyDataHelps.getExternalAccounts().then(function (accounts) {
-            if (!!props.externalAccountCategories) {
-                accounts = accounts.filter(account => props.externalAccountCategories!.includes(account.provider.category));
-            }
-            accounts.forEach(account => {
-                if (account.status == 'fetchComplete' && isAfter(new Date(), add(parseISO(account.lastRefreshDate), { hours: 3 }))) {
-                    MyDataHelps.refreshExternalAccount(account.id);
-                    account.status = 'fetchingData';
-                }
-            });
-            setExternalAccounts(accounts);
-        });
     }
 
     useEffect(function () {
         initialize();
 
-        MyDataHelps.on("externalAccountSyncComplete", initialize);
-        MyDataHelps.on("applicationDidBecomeVisible", initialize);
+        MyDataHelps.on("externalAccountSyncComplete", refresh);
+        MyDataHelps.on("applicationDidBecomeVisible", refresh);
         return () => {
-            MyDataHelps.off("externalAccountSyncComplete", initialize);
-            MyDataHelps.off("applicationDidBecomeVisible", initialize);
+            MyDataHelps.off("externalAccountSyncComplete", refresh);
+            MyDataHelps.off("applicationDidBecomeVisible", refresh);
         }
-    });
+    }, []);
 
     let anyRefreshingAccounts = !!externalAccounts.find(e => e.status == "fetchingData");
     let anyErroredAccounts = !!externalAccounts.find(e => e.status == "error" || e.status == "unauthorized");
     useInterval(() => {
-        console.log("Refreshing external accounts");
-        initialize();
-    }, anyRefreshingAccounts ? 3000 : null);
+        refresh();
+    }, (isWeb && anyRefreshingAccounts) ? 5000 : null);
 
+    console.log(externalAccounts);
     if (!anyRefreshingAccounts && !anyErroredAccounts) return null;
 
     return <Action bottomBorder subtitle={anyRefreshingAccounts ? "Retrieving data..." : "One of your accounts requires attention"} onClick={() => props.onClick()}
         icon={anyRefreshingAccounts ? <FontAwesomeSvgIcon color="var(--mdhui-text-color-4)" icon={faRefresh} spin /> : <FontAwesomeSvgIcon color="var(--mdhui-color-danger)" icon={faExclamationTriangle} />}>
     </Action>
-
-    // <div className="wrapper">
-    //     {anyRefreshingAccounts &&
-    //         <div className="health-profile-section" style={{ paddingRight: "16px", cursor: "default" }}>
-    //             <div className="external-accounts-status">
-    //                 <i className="fa fa-refresh fa-spin"></i>
-    //                 <span>&nbsp;Retrieving data...</span>
-    //             </div>
-    //         </div>
-    //     }
-    //     {externalAccounts.find(e => e.status == "error" || e.status == "unauthorized") &&
-    //         <div className="health-profile-section" onClick={() => MyDataHelps.openApplication(new URL("/external-accounts/", document.location as any).href)}>
-    //             <div className="external-accounts-status error-status">
-    //                 <i className="fa fa-exclamation-circle"></i>
-    //                 <span>&nbsp;One of your accounts requires attention</span>
-    //             </div>
-    //             <i className="fa fa-chevron-right"></i>
-    //         </div>
-    //     }
-    // </div>;
 }
