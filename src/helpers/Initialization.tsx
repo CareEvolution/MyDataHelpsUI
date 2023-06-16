dadd78723412f64496ba5236ee205f409972d0e1
Garrett Reinard <garrett@careevolution.com>
2023-06-16 16:10:52 -0400

    Allow for passing additional events and effect dependencies when using the initialize view hook.

--- src/helpers/Initialization.tsx
+++ src/helpers/Initialization.tsx
@@ -1,16 +1,24 @@
-﻿import MyDataHelps from '@careevolution/mydatahelps-js';
-import {useEffect} from 'react';
+﻿import MyDataHelps, { EventName } from '@careevolution/mydatahelps-js';
+import { DependencyList, useEffect } from 'react';
 
 const debounce = require('lodash.debounce');
 
-export function useInitializeView(initialize: () => void): void {
+export function useInitializeView(initialize: () => void, additionalEvents?: EventName[], dependencies?: DependencyList): void {
     useEffect(() => {
         let debouncedInitialize = debounce(initialize, 500);
 
         debouncedInitialize();
+
         MyDataHelps.on('applicationDidBecomeVisible', debouncedInitialize);
+        additionalEvents?.forEach(additionalEvent => {
+            MyDataHelps.on(additionalEvent, debouncedInitialize);
+        });
+
         return () => {
             MyDataHelps.off('applicationDidBecomeVisible', debouncedInitialize);
+            additionalEvents?.forEach(additionalEvent => {
+                MyDataHelps.off(additionalEvent, debouncedInitialize);
+            });
         }
-    }, []);
+    }, dependencies ?? []);
 }
