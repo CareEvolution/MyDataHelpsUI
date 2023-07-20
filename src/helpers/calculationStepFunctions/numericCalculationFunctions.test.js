83e854a2567857823a2c7331f6b5341b3f2ade1a
Thejas Suvarna <suvarnat@careevolution.com>
2023-07-20 17:23:16 -0400

    add non-numeric calculation test

--- src/helpers/calculationStepFunctions/numericCalculationFunctions.test.js
+++ src/helpers/calculationStepFunctions/numericCalculationFunctions.test.js
@@ -14,4 +14,11 @@ describe("NumericCalculationTests", () => {
         [{"date":"2023-07-19T11:14:18.377-04:00","stepIdentifier":"Q1","resultIdentifier":"Q1","answers":["3"]},{"date":"2023-07-19T11:14:18.944-04:00","stepIdentifier":"Form1","resultIdentifier":"Q1.2","answers":["4"]},{"date":"2023-07-19T11:14:20.338-04:00","stepIdentifier":"Q2","resultIdentifier":"Q2","answers":["2"]},{"date":"2023-07-19T11:14:20.849-04:00","stepIdentifier":"Q3","resultIdentifier":"Q3","answers":["3"]}]
       )).toBe('Calculation Error - Unmatched Variable');
     });
+
+    test('Invalid Variable', () => {
+      expect(calculateValue(
+        "(({Q1}*{Form1, Q1.2})+{Q2})-{Q3}",
+        [{"date":"2023-07-19T11:14:18.377-04:00","stepIdentifier":"Q1","resultIdentifier":"Q1","answers":["Jacob"]},{"date":"2023-07-19T11:14:18.944-04:00","stepIdentifier":"Form1","resultIdentifier":"Q1.2","answers":["4"]},{"date":"2023-07-19T11:14:20.338-04:00","stepIdentifier":"Q2","resultIdentifier":"Q2","answers":["2"]},{"date":"2023-07-19T11:14:20.849-04:00","stepIdentifier":"Q3","resultIdentifier":"Q3","answers":["3"]}]
+      )).toBe('Calculation Error - Unmatched Variable');
+    });
 });
