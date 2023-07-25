428551259d96c4484cfc28104e410dd7a18120e4
Thejas Suvarna <suvarnat@careevolution.com>
2023-07-25 16:21:07 -0400

    Remove state and remove onVisibileTrigger

--- src/components/step/NumericCalculationStepContainer/NumericCalculationStepContainer.tsx
+++ src/components/step/NumericCalculationStepContainer/NumericCalculationStepContainer.tsx
@@ -3,37 +3,21 @@ import MyDataHelps, {StepConfiguration} from '@careevolution/mydatahelps-js';
 import { calculateValue } from '../../../helpers/calculationStepFunctions/numericCalculationFunctions';
 import StepLayout from '../StepLayout'
 import { LoadingIndicator } from '../../presentational';
-import OnVisibleTrigger from '../../presentational/OnVisibleTrigger/OnVisibleTrigger';
 
 export default function () {
-
-    const [calculationString, setCalculationString] = useState<string>("");
-    const [calculationReady, setCalculationReady] = useState<boolean>(false);
-    const [calculationResult, setCalculationResult] = useState<string>("");
-
-
+    
     useEffect(() => {
-        setCalculationReady(false);
-        MyDataHelps.getStepConfiguration().then(function(config: StepConfiguration){
-          if (!config) return; // allows test mode to work
-          
-          setCalculationString(config.properties.calculation);
-          return new Promise((resolve, reject) => {
-            resolve(config.properties.calculation);
-          })
-        })
-        .then((() => MyDataHelps.getCurrentSurveyAnswers()))
-        .then((surveyAnswers) => {
-            setCalculationResult(calculateValue(calculationString, surveyAnswers).toString());
-            console.log(calculationResult);
-            setCalculationReady(true);
-        })
+        MyDataHelps.getStepConfiguration().then((config: StepConfiguration) => {
+            if (!config) return; // allows test mode to work
+            MyDataHelps.getCurrentSurveyAnswers().then((surveyAnswers) => {
+                MyDataHelps.completeStep(calculateValue(config.properties.calculation, surveyAnswers).toString());
+            });
+        });
     }, []);
 
     return (
         <StepLayout>
             <LoadingIndicator/>
-            <OnVisibleTrigger enabled={calculationReady} onTrigger={() => {MyDataHelps.completeStep(calculationResult)}}/>
         </StepLayout>
     );
 }
