6355a3211fa0198f70169aeb5bcdc39d6bd3d651
Thejas Suvarna <suvarnat@careevolution.com>
2023-08-17 13:21:06 -0700

    add support to error when Identifier has a space.

--- src/helpers/calculationStepFunctions/numericCalculationFunctions.test.js
+++ src/helpers/calculationStepFunctions/numericCalculationFunctions.test.js
@@ -36,11 +36,18 @@ describe("NumericCalculationTests", () => {
       )).toBe('Calculation Error: Answer not a number (Q1)');
     });
 
+    test('Identifier with a space', () => {
+      expect(calculateValue(
+        "(([Q1]*[Q1 2])+[Q2])-[Q3]",
+        [{"date":"2023-07-19T11:14:18.377-04:00","stepIdentifier":"Q1","resultIdentifier":"Q1","answers":["3"]},{"date":"2023-07-19T11:14:18.944-04:00","stepIdentifier":"Form1","resultIdentifier":"Q1 2","answers":["4"]},{"date":"2023-07-19T11:14:20.338-04:00","stepIdentifier":"Q2","resultIdentifier":"Q2","answers":["2"]},{"date":"2023-07-19T11:14:20.849-04:00","stepIdentifier":"Q3","resultIdentifier":"Q3","answers":["3"]},{"date":"2023-07-19T11:14:21.849-04:00","stepIdentifier":"Q3","resultIdentifier":"Q4.1","answers":["3"]}]
+      )).toBe('Calculation Error: Numeric Calculation Steps do not support StepIdentifier or ResultIdentifiers that have spaces, periods, or multiple-select (Q1 2).');
+    });
+
     test('Unused Identifier contains .', () => {
       expect(calculateValue(
         "(([Q1]*[Q1_2])+[Q2])-[Q3]",
-        [{"date":"2023-07-19T11:14:18.377-04:00","stepIdentifier":"Q1","resultIdentifier":"Q1","answers":["Jacob"]},{"date":"2023-07-19T11:14:18.944-04:00","stepIdentifier":"Form1","resultIdentifier":"Q1_2","answers":["4"]},{"date":"2023-07-19T11:14:20.338-04:00","stepIdentifier":"Q2","resultIdentifier":"Q2","answers":["2"]},{"date":"2023-07-19T11:14:20.849-04:00","stepIdentifier":"Q3","resultIdentifier":"Q3","answers":["3"]},{"date":"2023-07-19T11:14:21.849-04:00","stepIdentifier":"Q3","resultIdentifier":"Q4.1","answers":["3"]}]
-      )).toBe('Calculation Error: Answer not a number (Q1)');
+        [{"date":"2023-07-19T11:14:18.377-04:00","stepIdentifier":"Q1","resultIdentifier":"Q1","answers":["3"]},{"date":"2023-07-19T11:14:18.944-04:00","stepIdentifier":"Form1","resultIdentifier":"Q1_2","answers":["4"]},{"date":"2023-07-19T11:14:20.338-04:00","stepIdentifier":"Q2","resultIdentifier":"Q2","answers":["2"]},{"date":"2023-07-19T11:14:20.849-04:00","stepIdentifier":"Q3","resultIdentifier":"Q3","answers":["3"]},{"date":"2023-07-19T11:14:21.849-04:00","stepIdentifier":"Q3","resultIdentifier":"Q4.1","answers":["3"]}]
+      )).toBe('11');
     });
 
     test('Calculation Passes', () => {
--- src/helpers/calculationStepFunctions/numericCalculationFunctions.ts
+++ src/helpers/calculationStepFunctions/numericCalculationFunctions.ts
@@ -6,20 +6,28 @@ export const calculateValue = (calcString :string, surveyAnswers: CurrentSurveyA
   const math = create(all, config)
 
   try {
-    // const answerDict: {[id: string] : number} = createScope(surveyAnswers);
     const [answerDict, brokenIdentifiers] = createScope(surveyAnswers);
+
+    //finding if any of the variables used in the calculation had errors detected (., ' ', multiple-select, etc.)
+    const regex = /\[(.*?)\]/g;
+    const identifiersUsed = Array.from(calcString.matchAll(regex), (m) => `${m[1]}`);
+    const brokenUsedIdentifiers = brokenIdentifiers.filter(value => identifiersUsed.includes(value));
+
     try {
       let calculation = math.evaluate(calcString, answerDict)._data[0].toString();
       if (calculation == "NaN"){
         let nonNumberIdentifier = Object.keys(answerDict).find(key => isNaN(answerDict[key]));
         throw Error(`Calculation Error: Answer not a number (${nonNumberIdentifier})`);
       }
+      if (brokenUsedIdentifiers.length){
+        throw Error();
+      }
       return(calculation);
     }
     catch(error: unknown) {
       if (error instanceof Error){
         if(!error.message.startsWith("Calculation Error:")) {
-          const errorIdentifierMessage = brokenIdentifiers.length ? brokenIdentifiers : error.message;
+          const errorIdentifierMessage = brokenUsedIdentifiers.length ? brokenUsedIdentifiers : error.message;
           return("Calculation Error: Numeric Calculation Steps do not support StepIdentifier or ResultIdentifiers that have spaces, periods, or multiple-select (" + errorIdentifierMessage + ").");
         }
         else{
@@ -41,7 +49,7 @@ const createScope = (surveyAnswers: CurrentSurveyAnswer[]) => {
   let issueIdentifier: string[] = [];
   surveyAnswers.map((answer) => {
     const value = parseFloat(answer.answers[0]);
-    if (answer.answers.length > 1 || answer.resultIdentifier.includes('.')) {
+    if (answer.answers.length > 1 || answer.resultIdentifier.includes('.') || answer.resultIdentifier.includes(' ')) {
       issueIdentifier.push(answer.resultIdentifier);
       return
     }
