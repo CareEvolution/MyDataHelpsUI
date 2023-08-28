437c85691976bf053062c3203194d908c41f2901
Bangxin Hu <bxh@careevolution.com>
2023-08-28 11:33:20 -0400

    ConnectDeviceAccountStep (#104)
    * Add ConnectDeviceAccountStep and its container
    
    * Add step config property validation
    
    * Make advancing testable
    
    * Handle empty string
    
    * Remove device type check

---
+++ src/components/step/ConnectDeviceAccountStepContainer/ConnectDeviceAccountStepContainer.stories.tsx
@@ -0,0 +1,27 @@
+﻿import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import ConnectDeviceAccountStepContainer, {
+    ConnectDeviceAccountStepContainerProps,
+} from "./ConnectDeviceAccountStepContainer";
+
+export default {
+    title: "SurveyStep/ConnectDeviceAccountStepContainer",
+    component: ConnectDeviceAccountStepContainer,
+    parameters: {
+        layout: "fullscreen",
+    },
+} as ComponentMeta<typeof ConnectDeviceAccountStepContainer>;
+
+const Template: ComponentStory<typeof ConnectDeviceAccountStepContainer> = (
+    args: ConnectDeviceAccountStepContainerProps
+) => <ConnectDeviceAccountStepContainer {...args} />;
+
+export const ConnectDeviceAccountStepContainerDefault = Template.bind({});
+ConnectDeviceAccountStepContainerDefault.args = {
+    providerName: "",
+};
+
+export const ConnectDeviceAccountStepContainerGarmin = Template.bind({});
+ConnectDeviceAccountStepContainerGarmin.args = {
+    providerName: "Garmin QA",
+};
--- src/components/step/index.ts
+++ src/components/step/index.ts
@@ -1,5 +1,7 @@
 export { default as CelebrationStep } from "./CelebrationStep"
 export { default as CelebrationStepContainer } from "./CelebrationStepContainer"
+export { default as ConnectDeviceAccountStep } from "./ConnectDeviceAccountStep"
+export { default as ConnectDeviceAccountStepContainer } from "./ConnectDeviceAccountStepContainer"
 export { default as ConnectEhrStep } from "./ConnectEhrStep"
 export { default as ConnectEhrStepContainer } from "./ConnectEhrStepContainer"
 export { default as StepDetailText} from "./StepDetailText"
