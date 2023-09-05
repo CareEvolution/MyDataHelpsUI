e78b17630708daaa5d71157f73cb9d341fd047c6
Garrett Reinard <garrett@careevolution.com>
2023-09-05 15:25:18 -0400

    Add AnimatedRing component.

---
+++ src/components/presentational/AnimatedRing/AnimatedRing.stories.tsx
@@ -0,0 +1,28 @@
+﻿import React from 'react';
+import { Layout } from '../../presentational';
+import AnimatedRing, { AnimatedRingProps } from './AnimatedRing';
+import './AnimatedRing.stories.css';
+
+export default {
+    title: 'Presentational/AnimatedRing',
+    component: AnimatedRing,
+    parameters: {layout: 'fullscreen'}
+};
+
+const children = <div className="animated-ring-story">Great Job!</div>;
+const render = (args: AnimatedRingProps) => <Layout><AnimatedRing {...args} /></Layout>
+
+export const Default = {
+    args: {
+        children: children
+    },
+    render: render
+};
+
+export const SpecificColor = {
+    args: {
+        children: children,
+        color: '#71b345'
+    },
+    render: render
+};
