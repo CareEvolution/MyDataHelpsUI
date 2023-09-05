112cab8458d6b05189bc1ea3c95457fc44060ef7
Garrett Reinard <garrett@careevolution.com>
2023-09-05 17:13:59 -0400

    Add NewPoints component.

---
+++ src/components/container/NewPoints/NewPoints.stories.tsx
@@ -0,0 +1,144 @@
+﻿import React from 'react';
+import { Layout } from '../../presentational';
+import NewPoints, { NewPointsProps } from './NewPoints';
+
+export default {
+    title: 'Container/NewPoints',
+    component: NewPoints,
+    parameters: {layout: 'fullscreen'}
+};
+
+const render = (args: NewPointsProps) => <Layout><NewPoints {...args} /></Layout>
+
+export const SingleEntry = {
+    args: {
+        entries: [
+            {
+                name: 'Some Activity',
+                points: 125,
+                message: {
+                    title: 'Great Job!',
+                    text: 'Great job completing some activity.'
+                }
+            }
+        ]
+    },
+    render: render
+};
+
+export const SingleEntryWithBonus = {
+    args: {
+        entries: [
+            {
+                name: 'Some Activity',
+                points: 125,
+                bonusPoints: 50,
+                message: {
+                    title: 'Great Job!',
+                    text: 'Great job completing some activity.'
+                }
+            }
+        ]
+    },
+    render: render
+};
+
+export const SingleEntryWithNextReward = {
+    args: {
+        entries: [
+            {
+                name: 'Some Activity',
+                points: 125,
+                message: {
+                    title: 'Great Job!',
+                    text: 'Great job completing some activity.'
+                }
+            }
+        ],
+        pointsToNextReward: 125
+    },
+    render: render
+};
+
+export const MultiEntry = {
+    args: {
+        entries: [
+            {
+                name: 'Some Activity',
+                points: 125,
+                message: {
+                    title: 'Great Job!',
+                    text: 'Great job completing some activity.'
+                }
+            },
+            {
+                name: 'Some Other Activity',
+                points: 85,
+                message: {
+                    title: 'Great Job!',
+                    text: 'Great job completing some other activity.'
+                }
+            }
+        ]
+    },
+    render: render
+};
+
+export const MultiEntryWithBonus = {
+    args: {
+        entries: [
+            {
+                name: 'Some Activity',
+                points: 125,
+                bonusPoints: 30,
+                message: {
+                    title: 'Great Job!',
+                    text: 'Great job completing some activity.'
+                }
+            },
+            {
+                name: 'Some Other Activity',
+                points: 85,
+                bonusPoints: 10,
+                message: {
+                    title: 'Great Job!',
+                    text: 'Great job completing some other activity.'
+                }
+            },
+            {
+                name: 'Yet another activity with a really long name that should be truncated.',
+                points: 100,
+                message: {
+                    title: 'Great Job!',
+                    text: 'Great job completing some other activity.'
+                }
+            }
+        ]
+    },
+    render: render
+};
+
+export const MultiEntryWithNextReward = {
+    args: {
+        entries: [
+            {
+                name: 'Some Activity',
+                points: 125,
+                message: {
+                    title: 'Great Job!',
+                    text: 'Great job completing some activity.'
+                }
+            },
+            {
+                name: 'Some Other Activity',
+                points: 85,
+                message: {
+                    title: 'Great Job!',
+                    text: 'Great job completing some other activity.'
+                }
+            }
+        ],
+        pointsToNextReward: 200
+    },
+    render: render
+};
---
+++ src/components/container/NewPoints/NewPoints.tsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import './NewPoints.css';
+import AnimatedRing from '../../presentational/AnimatedRing';
+
+export interface NewPointsEntry {
+    name: string;
+    points: number;
+    bonusPoints?: number;
+    message?: { title: string, text: string };
+}
+
+export interface NewPointsProps {
+    entries: NewPointsEntry[];
+    pointsToNextReward?: number;
+}
+
+export default function (props: NewPointsProps) {
+
+    let totalBonusPoints = props.entries.reduce((sum, entry) => {
+        return sum + (entry.bonusPoints || 0);
+    }, 0);
+
+    let totalPoints = props.entries.reduce((sum, entry) => {
+        return sum + entry.points;
+    }, totalBonusPoints);
+
+    return (
+        <div className="mdhui-new-points">
+            <AnimatedRing>
+                <div className="mdhui-new-points-total">
+                    <div className="mdhui-new-points-total-points">+{totalPoints}</div>
+                    <div className="mdhui-new-points-total-points-label">points</div>
+                </div>
+            </AnimatedRing>
+            <div className="mdhui-new-points-content">
+                {props.entries.length == 1 && props.entries[0].message && totalBonusPoints === 0 &&
+                <div>
+                    <div className="mdhui-new-points-title">{props.entries[0].message.title}</div>
+                    <div className="mdhui-new-points-text">{props.entries[0].message.text}</div>
+                </div>
+                }
+                {(props.entries.length > 1 || !props.entries[0].message || totalBonusPoints > 0) &&
+                <div>
+                    <div className="mdhui-new-points-title">Well Done!</div>
+                    <div className="mdhui-new-points-text">You've been awarded points for the following:</div>
+                    <div className="mdhui-new-points-entries">
+                        {props.entries.map((entry) =>
+                            <div className="mdhui-new-points-entry">
+                                <div className="mdhui-new-points-entry-name">{entry.name}</div>
+                                <div className="mdhui-new-points-entry-points">+{entry.points}</div>
+                            </div>
+                        )}
+                        {totalBonusPoints > 0 &&
+                        <div className="mdhui-new-points-entry">
+                            <div className="mdhui-new-points-entry-name bonus">Bonus</div>
+                            <div className="mdhui-new-points-entry-points bonus">+{totalBonusPoints}</div>
+                        </div>
+                        }
+                    </div>
+                </div>
+                }
+                {props.pointsToNextReward &&
+                <div className="mdhui-new-points-next-reward">You now need <span className="mdhui-new-points-next-reward-points">{props.pointsToNextReward}</span> points to unlock your next reward.</div>
+                }
+            </div>
+        </div>
+    );
+}
