aedb02e110e790508c66e0b3c7c3d756344a36ab
Garrett Reinard <garrett@careevolution.com>
2023-09-08 19:08:52 -0400

    More work in progress on new points.

--- src/helpers/new-points.ts
+++ src/helpers/new-points.ts
@@ -1,22 +1,22 @@
-import { NewPointsProps } from "../components";
-import MyDataHelps from "@careevolution/mydatahelps-js";
+import { NewPointsProps } from '../components';
+import MyDataHelps, { PersistableDeviceDataPoint } from '@careevolution/mydatahelps-js';
 
-type ColorScheme = 'auto' | 'light' | 'dark';
+export type ColorScheme = 'auto' | 'light' | 'dark';
 
 export function showNewPoints(props: NewPointsProps, colorScheme?: ColorScheme, primaryColor?: string, url?: string): void {
-    let newPointsUrl = url || 'https://viewlibrary.careevolutionapps.dev/newpoints';
-    let newPointsViewParameters: { data: string, colorScheme?: ColorScheme, primaryColor?: string } = {
-        data: window.btoa(JSON.stringify(props))
+    let newPointsDataPoint: PersistableDeviceDataPoint = {
+        type: 'MDHUI-NewPoints',
+        value: JSON.stringify(props)
     };
-    if (colorScheme) {
-        newPointsViewParameters.colorScheme = colorScheme;
-    }
-    if (primaryColor) {
-        newPointsViewParameters.primaryColor = primaryColor;
-    }
-    MyDataHelps.openApplication(newPointsUrl + '?' + new URLSearchParams(newPointsViewParameters).toString(), {modal: true});
-}
-
-export function decodeNewPointsProps(encodedProps: string): NewPointsProps {
-    return JSON.parse(window.atob(encodedProps)) as NewPointsProps;
+    MyDataHelps.persistDeviceData([newPointsDataPoint]).then(() => {
+        let newPointsUrl = url || 'https://viewlibrary.careevolutionapps.dev/newpoints';
+        let newPointsViewParameters: { colorScheme?: ColorScheme, primaryColor?: string } = {};
+        if (colorScheme) {
+            newPointsViewParameters.colorScheme = colorScheme;
+        }
+        if (primaryColor) {
+            newPointsViewParameters.primaryColor = primaryColor;
+        }
+        MyDataHelps.openApplication(newPointsUrl + '?' + new URLSearchParams(newPointsViewParameters).toString(), {modal: true});
+    });
 }
