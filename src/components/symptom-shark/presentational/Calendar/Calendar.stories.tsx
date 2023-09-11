6e91196566c4eb560942f50d2ca4ae0c837f002c
Chris Nowak <chris@Christophers-MacBook-Pro.local>
2023-09-11 14:14:15 -0400

    Fix calendar viz

--- src/components/symptom-shark/presentational/Calendar/Calendar.stories.tsx
+++ src/components/symptom-shark/presentational/Calendar/Calendar.stories.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 import { DateRangeCoordinator, Layout, Section } from "../../../presentational";
 import SymptomSharkCalendar, { SymptomSharkCalendarProps } from "./Calendar";
-import { SymptomSharkLogVisualizationCoordinator } from "../../container";
+import { SymptomSharkVisualizationCoordinator } from "../../container";
 
 export default { title: "SymptomShark/Presentational/Calendar", component: SymptomSharkCalendar, parameters: { layout: 'fullscreen' } };
 let render = (args: SymptomSharkCalendarProps) => <Layout>
     <DateRangeCoordinator intervalType={"Month"} variant="rounded">
-        <SymptomSharkLogVisualizationCoordinator previewState="default" showFilters>
+        <SymptomSharkVisualizationCoordinator previewState="default" showFilters>
             <Section>
                 <SymptomSharkCalendar {...args} />
             </Section>
-        </SymptomSharkLogVisualizationCoordinator>
+        </SymptomSharkVisualizationCoordinator>
     </DateRangeCoordinator>
 </Layout>
 
