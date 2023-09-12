50d4703acc4f01195620e2404d7aa3737fd8c0e4
Chris Nowak <chris@Christophers-MacBook-Pro.local>
2023-09-12 14:42:26 -0400

    Fix steps daily data, other style tweaks

--- src/helpers/daily-data-providers/combined-steps.ts
+++ src/helpers/daily-data-providers/combined-steps.ts
@@ -1,29 +1,40 @@
 ﻿import { add } from "date-fns";
 import { appleHealthStepsDataProvider, fitbitStepsDataProvider, garminStepsDataProvider } from ".";
 import getDayKey from "../get-day-key";
+import MyDataHelps from "@careevolution/mydatahelps-js";
 
 export default function (startDate: Date, endDate: Date) {
-	var fitbitSteps = fitbitStepsDataProvider(startDate, endDate);
-	var appleSteps = appleHealthStepsDataProvider(startDate, endDate);
-	var garminSteps = garminStepsDataProvider(startDate, endDate);
+	let providers: Promise<{ [key: string]: number }>[] = [];
 
-	return Promise.all([fitbitSteps, appleSteps, garminSteps]).then((values) => {
-		var data: { [key: string]: number } = {};
-		while (startDate < endDate) {
-			var dayKey = getDayKey(startDate);
-			var steps: number | null = null;
-			if (values[0][dayKey]) {
-				steps = values[0][dayKey];
-			}
-
-			if (values[1][dayKey] && (steps == null || values[1][dayKey] > steps)) {
-				steps = values[1][dayKey];
-			}
-			if (steps) {
-				data[dayKey] = steps;
-			}
-			startDate = add(startDate, { days: 1 });
+	return MyDataHelps.getDataCollectionSettings().then((settings) => {
+		if (settings.fitbitEnabled) {
+			providers.push(fitbitStepsDataProvider(startDate, endDate));
 		}
-		return data;
+		if (settings.garminEnabled) {
+			providers.push(garminStepsDataProvider(startDate, endDate));
+		}
+		if (settings.queryableDeviceDataTypes.find(s => s.namespace == "AppleHealth" && s.type == "HourlySteps")) {
+			providers.push(appleHealthStepsDataProvider(startDate, endDate));
+		}
+
+		return Promise.all(providers).then((values) => {
+			var data: { [key: string]: number } = {};
+			while (startDate < endDate) {
+				var dayKey = getDayKey(startDate);
+				var steps: number | null = null;
+				values.forEach((value) => {
+					if (value[dayKey] || steps == null || steps < value[dayKey]) {
+						steps = value[dayKey];
+					}
+				});
+				if (steps) {
+					data[dayKey] = steps;
+				}
+				startDate = add(startDate, { days: 1 });
+			}
+			return data;
+		});
 	});
+
+
 }
