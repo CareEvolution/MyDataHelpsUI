2a9ec08bfa62e4620bff700b75f7512a5732666c
Bangxin Hu <bxh@careevolution.com>
2023-09-22 14:17:46 -0400

    Set providerID based on deviceType

--- src/components/step/ConnectDeviceAccountStepContainer/ConnectDeviceAccountStepContainer.tsx
+++ src/components/step/ConnectDeviceAccountStepContainer/ConnectDeviceAccountStepContainer.tsx
@@ -11,15 +11,24 @@ export default function (props: ConnectDeviceAccountStepContainerProps) {
     const [title, setTitle] = useState<string>();
     const [text, setText] = useState<string>();
     const [styles, setStyles] = useState<any>({});
-    const [deviceType, setDeviceType] = useState<string>(props.deviceType ?? "");
+    const [deviceType, setDeviceType] = useState<string>(
+        props.deviceType ?? ""
+    );
     const [providerID, setProviderID] = useState<number>(props.providerID ?? 0);
 
     function onConnect() {
         MyDataHelps.connectExternalAccount(providerID, {
             openNewWindow: true,
-        }).then(function() {
-            MyDataHelps.completeStep(providerID)
+        }).then(function () {
+            MyDataHelps.completeStep(providerID);
         });
+    }
+
+    function isProd() {
+        return (
+            window.location.host.endsWith(".com") ||
+            window.location.host.endsWith(".org")
+        );
     }
 
     useEffect(() => {
@@ -37,26 +46,45 @@ export default function (props: ConnectDeviceAccountStepContainerProps) {
             }
             setDeviceType(config.properties.deviceType);
 
-            if (!config.properties.providerID) {
-                throw new Error("providerID is required");
+            switch (config.properties.deviceType) {
+                case "Fitbit":
+                    setProviderID(
+                        isProd() ? 564 : 18 // 'Fitbit' or 'Fitbit prod'
+                    );
+                    break;
+                case "Omron":
+                    setProviderID(
+                        isProd() ? 1466 : 171 // 'Omron' or 'Omron prod'
+                    );
+                    break;
+                case "Garmin":
+                    setProviderID(
+                        isProd() ? 6327 : 1384 // 'Garmin' or 'Garmin QA'
+                    );
+                    break;
+                default:
+                    throw Error(
+                        "Unsupported device type " +
+                            config.properties.deviceType
+                    );
             }
 
-            setProviderID(config.properties.providerID as number);
-
             setStyles(config.styles ?? {});
-        })
+        });
     }, []);
 
     useEffect(() => {
         // Check connect status once providerID is set.
         if (!providerID) return;
 
-        MyDataHelps.getExternalAccounts().then(function(accounts) {
-            const connected = accounts.some((acc) => acc.provider.id === providerID);
+        MyDataHelps.getExternalAccounts().then(function (accounts) {
+            const connected = accounts.some(
+                (acc) => acc.provider.id === providerID
+            );
             if (connected) {
                 MyDataHelps.completeStep(providerID);
             }
-        })
+        });
     }, [providerID]);
 
     if (!providerID) return <></>;
