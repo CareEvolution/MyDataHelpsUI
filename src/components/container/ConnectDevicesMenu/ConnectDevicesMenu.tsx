d07a3757f6c97d38f2640dec72e3507b5627358d
Chris Nowak <chris@Christophers-MacBook-Pro.local>
2023-09-30 11:01:19 -0400

    Restore loading state

--- src/components/container/ConnectDevicesMenu/ConnectDevicesMenu.tsx
+++ src/components/container/ConnectDevicesMenu/ConnectDevicesMenu.tsx
@@ -163,9 +163,12 @@ export default function (props: ConnectDevicesMenuProps) {
         };
 
         if (externalAccount) {
-            //TODO: Refactor out the action "row" style into a separate component
             if (externalAccount.status == "fetchComplete" || externalAccount.status == "fetchingData") {
                 indicator = <div className="mdhui-connect-devices-menu-connected">Connected</div>;
+                action = undefined;
+            }
+            if (externalAccount.status == "fetchingData") {
+                indicator = <div className="mdhui-connect-devices-menu-connected">Downloading Data&nbsp;&nbsp;<FontAwesomeSvgIcon icon={faRefresh} spin /></div>;
                 action = undefined;
             }
             if (externalAccount.status == "unauthorized") {
