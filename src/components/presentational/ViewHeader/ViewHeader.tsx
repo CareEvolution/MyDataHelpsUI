41ff48d79b6b3aed3a2f7bc252a4d878f8508c07
Chris Nowak <chris@Christophers-MacBook-Pro.local>
2023-10-12 11:23:44 -0400

    Formatting

--- src/components/presentational/ViewHeader/ViewHeader.tsx
+++ src/components/presentational/ViewHeader/ViewHeader.tsx
@@ -6,21 +6,21 @@ import { LayoutContext } from "../Layout";
 export interface ViewHeaderProps {
     title?: string;
     subtitle?: string;
-	titleColor?: ColorDefinition;
-	subtitleColor?: ColorDefinition;
+    titleColor?: ColorDefinition;
+    subtitleColor?: ColorDefinition;
 }
 
 export default function (props: ViewHeaderProps) {
-	let layoutContext = useContext(LayoutContext);
-    
+    let layoutContext = useContext(LayoutContext);
+
     if (!props.title && !props.subtitle) return null;
     return (
         <div className="mdhui-view-header">
             {props.title &&
-                <div style={{color:resolveColor(layoutContext.colorScheme, props.titleColor)}} className="mdhui-view-header-title">{props.title}</div>
+                <div style={{ color: resolveColor(layoutContext.colorScheme, props.titleColor) }} className="mdhui-view-header-title">{props.title}</div>
             }
             {props.subtitle &&
-                <div style={{color:resolveColor(layoutContext.colorScheme, props.subtitleColor)}} className="mdhui-view-header-subtitle">{props.subtitle}</div>
+                <div style={{ color: resolveColor(layoutContext.colorScheme, props.subtitleColor) }} className="mdhui-view-header-subtitle">{props.subtitle}</div>
             }
         </div>
     );
