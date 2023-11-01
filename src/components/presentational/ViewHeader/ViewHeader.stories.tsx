47f7d1a4d6ade2d898d1d683cee5fa994a3c50e7
Garrett Reinard <garrett@careevolution.com>
2023-11-01 13:58:37 -0400

    Add an optional action icon to the ViewHeader component.

--- src/components/presentational/ViewHeader/ViewHeader.stories.tsx
+++ src/components/presentational/ViewHeader/ViewHeader.stories.tsx
@@ -1,21 +1,60 @@
-import React from "react";
-import ViewHeader, { ViewHeaderProps } from "./ViewHeader";
-import Layout from "../../presentational/Layout";
+import React from 'react';
+import ViewHeader, { ViewHeaderProps } from './ViewHeader';
+import Layout from '../../presentational/Layout';
+import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
 
-export default { title: "Presentational/ViewHeader", component: ViewHeader, parameters: { layout: 'fullscreen' } };
+export default {title: 'Presentational/ViewHeader', component: ViewHeader, parameters: {layout: 'fullscreen'}};
+
 let render = (args: ViewHeaderProps) => <Layout colorScheme="auto"><ViewHeader {...args} /></Layout>
-export const titleAndSubtitle = {
-    name: "Title and Subtitle",
-    args: { title: "Surveys", subtitle: "You've completed 12 tasks today.  Take a rest if you need to." },
+
+const action = {
+    icon: faUserCircle,
+    onClick: () => {
+        console.log('Action clicked.');
+    }
+};
+
+export const TitleAndSubtitle = {
+    args: {title: 'Surveys', subtitle: 'You\'ve completed 12 tasks today.  Take a rest if you need to.'},
     render: render
 };
-export const titleOnly = {
-    name: "Title Only",
-    args: { title: "Surveys" },
+
+export const TitleAndSubtitleWithAction = {
+    args: {title: 'Surveys', subtitle: 'You\'ve completed 12 tasks today.  Take a rest if you need to.', action: action},
     render: render
 };
-export const subtitleOnly = {
-    name: "Subtitle Only",
-    args: { subtitle: "You've completed 12 tasks today.  Take a rest if you need to." },
+
+export const TitleAndSubtitleWithActionColor = {
+    args: {title: 'Surveys', subtitle: 'You\'ve completed 12 tasks today.  Take a rest if you need to.', action: {...action, color: 'blue'}},
+    render: render
+};
+
+export const TitleOnly = {
+    args: {title: 'Surveys'},
+    render: render
+};
+
+export const TitleOnlyWithAction = {
+    args: {title: 'Surveys', action: action},
+    render: render
+};
+
+export const TitleOnlyWithActionColor = {
+    args: {title: 'Surveys', action: {...action, color: 'blue'}},
+    render: render
+};
+
+export const SubtitleOnly = {
+    args: {subtitle: 'You\'ve completed 12 tasks today.  Take a rest if you need to.'},
+    render: render
+};
+
+export const SubtitleOnlyWithAction = {
+    args: {subtitle: 'You\'ve completed 12 tasks today.  Take a rest if you need to.', action: action},
+    render: render
+};
+
+export const SubtitleOnlyWithActionColor = {
+    args: {subtitle: 'You\'ve completed 12 tasks today.  Take a rest if you need to.', action: {...action, color: 'blue'}},
     render: render
 };
--- src/components/presentational/ViewHeader/ViewHeader.tsx
+++ src/components/presentational/ViewHeader/ViewHeader.tsx
@@ -1,13 +1,22 @@
-import React, { useContext } from "react";
-import "./ViewHeader.css"
-import { ColorDefinition, resolveColor } from "../../../helpers/colors";
-import { LayoutContext } from "../Layout";
+import React, { useContext } from 'react';
+import './ViewHeader.css'
+import { ColorDefinition, resolveColor } from '../../../helpers/colors';
+import { LayoutContext } from '../Layout';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
+
+export interface ViewHeaderAction {
+    icon: IconDefinition;
+    onClick: () => void;
+    color?: ColorDefinition;
+}
 
 export interface ViewHeaderProps {
     title?: string;
     subtitle?: string;
     titleColor?: ColorDefinition;
     subtitleColor?: ColorDefinition;
+    action?: ViewHeaderAction;
 }
 
 export default function (props: ViewHeaderProps) {
@@ -16,12 +25,21 @@ export default function (props: ViewHeaderProps) {
     if (!props.title && !props.subtitle) return null;
     return (
         <div className="mdhui-view-header">
-            {props.title &&
-                <div style={{ color: resolveColor(layoutContext.colorScheme, props.titleColor) }} className="mdhui-view-header-title">{props.title}</div>
-            }
-            {props.subtitle &&
-                <div style={{ color: resolveColor(layoutContext.colorScheme, props.subtitleColor) }} className="mdhui-view-header-subtitle">{props.subtitle}</div>
-            }
+            <div className="mdhui-view-header-text">
+                {props.title &&
+                    <div style={{color: resolveColor(layoutContext.colorScheme, props.titleColor)}} className="mdhui-view-header-title">{props.title}</div>
+                }
+                {props.subtitle &&
+                    <div style={{color: resolveColor(layoutContext.colorScheme, props.subtitleColor)}} className="mdhui-view-header-subtitle">{props.subtitle}</div>
+                }
+            </div>
+            <div className="mdhui-view-header-action" onClick={props.action ? props.action.onClick : undefined}>
+                {props.action &&
+                    <div className="mdhui-view-header-action-icon" style={{color: resolveColor(layoutContext.colorScheme, props.action.color)}}>
+                        <FontAwesomeIcon icon={props.action.icon}/>
+                    </div>
+                }
+            </div>
         </div>
     );
 }
