d9b294b6f035f804522ad309a26de381fecd1ee1
Adam <adam@careevolution.com>
2023-11-20 09:44:55 -0500

    Fix baseUrl race condition in providerID helpers (#156)
    * Fix baseUrl race condition
    
    * Fix baseUrl race condition in GarminView

--- src/components/view/FitbitView/FitbitView.tsx
+++ src/components/view/FitbitView/FitbitView.tsx
@@ -1,8 +1,9 @@
-import React from 'react'
-import { Layout, Card, StatusBarBackground, ConnectFitbit, FitbitDevices, FitbitMonthCharts } from "../.."
+import React, { useEffect, useState } from 'react'
+import { Layout, Card, StatusBarBackground, ConnectFitbit, FitbitDevices, FitbitMonthCharts, LoadingIndicator } from "../.."
 import { ConnectFitbitPreviewState } from "../../container/ConnectFitbit/ConnectFitbit";
 import { ConnectedDevicesPreviewState } from '../../container/ConnectedDevices/ConnectedDevices';
 import { MonthChartsPreviewState } from '../../container/MonthCharts/MonthCharts';
+import MyDataHelps from '@careevolution/mydatahelps-js';
 
 export interface FitbitViewProps {
 	connectPreview?: ConnectFitbitPreviewState,
@@ -12,18 +13,32 @@ export interface FitbitViewProps {
 }
 
 export default function (props: FitbitViewProps) {
+	var [viewIsReady, setViewIsReady] = useState(false);
+	useEffect(() => {
+		MyDataHelps.connect().then(() => {
+			setViewIsReady(true);
+		})
+	}, [])
+
 	return (
 		<Layout colorScheme={props.colorScheme ?? "auto"}>
 			<StatusBarBackground />
-			<Card>
-				<ConnectFitbit previewState={props.connectPreview} disabledBehavior="displayError" />
-			</Card>
-			<Card>
-				<FitbitDevices previewState={props.devicesPreview} />
-			</Card>
-			<Card allowOverflow={true}>
-				<FitbitMonthCharts previewState={props.chartsPreview} />
-			</Card>
+			{viewIsReady &&
+				<>
+					<Card>
+						<ConnectFitbit previewState={props.connectPreview} disabledBehavior="displayError" />
+					</Card>
+					<Card>
+						<FitbitDevices previewState={props.devicesPreview} />
+					</Card>
+					<Card allowOverflow={true}>
+						<FitbitMonthCharts previewState={props.chartsPreview} />
+					</Card>
+				</>
+			}
+			{!viewIsReady &&
+				<LoadingIndicator />
+			}
 		</Layout>
 	)
 }
