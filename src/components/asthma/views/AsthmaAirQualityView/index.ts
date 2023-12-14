a3636663310823cf126b565fa291f117ed32977c
Garrett Reinard <garrett@careevolution.com>
2023-12-14 17:27:15 -0500

    Work in progress on data drilldowns.

---
+++ src/components/asthma/views/AsthmaAirQualityView/index.ts
@@ -0,0 +1,1 @@
+export { default, AsthmaAirQualityViewPreviewState } from './AsthmaAirQualityView';
--- src/components/asthma/views/index.ts
+++ src/components/asthma/views/index.ts
@@ -1,2 +1,3 @@
+export { default as AsthmaAirQualityView, AsthmaAirQualityViewPreviewState } from './AsthmaAirQualityView';
 export { default as AsthmaDayView, AsthmaDayViewPreviewState } from './AsthmaDayView';
 export { default as AsthmaLogEntryEditorView, AsthmaLogEntryEditorViewPreviewState } from './AsthmaLogEntryEditorView';
