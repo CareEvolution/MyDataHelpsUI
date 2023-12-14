01a800e099546c794afedb9d03f9a13d17091099
Garrett Reinard <garrett@careevolution.com>
2023-12-14 17:35:16 -0500

    Fix export.

--- src/components/asthma/views/index.ts
+++ src/components/asthma/views/index.ts
@@ -1,3 +1,3 @@
-export { default as AsthmaAirQualityView, AsthmaAirQualityViewPreviewState } from './AsthmaAirQualityView';
+export { default as AsthmaAirQualityView } from './AsthmaAirQualityView';
 export { default as AsthmaDayView, AsthmaDayViewPreviewState } from './AsthmaDayView';
 export { default as AsthmaLogEntryEditorView, AsthmaLogEntryEditorViewPreviewState } from './AsthmaLogEntryEditorView';
