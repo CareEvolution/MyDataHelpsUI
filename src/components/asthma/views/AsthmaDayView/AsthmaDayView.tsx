0c429e46f8479abc2dfd7309bf0600d32694e35f
Garrett Reinard <garrett@careevolution.com>
2024-01-19 14:15:46 -0500

    Fix air qualities component usage.

--- src/components/asthma/views/AsthmaDayView/AsthmaDayView.tsx
+++ src/components/asthma/views/AsthmaDayView/AsthmaDayView.tsx
@@ -19,6 +19,7 @@ export interface AsthmaDayViewProps {
     heartAndLungsUrl: string;
     activityUrl: string;
     sleepUrl: string;
+    editZipCodesSurveyName: string;
     airQualityUrl: string;
 }
 
@@ -50,6 +51,7 @@ export default function (props: AsthmaDayViewProps) {
         <Card>
             <AsthmaAirQualities
                 previewState={props.previewState?.airQualitiesPreviewState}
+                editZipCodesSurveyName={props.editZipCodesSurveyName}
                 airQualityUrl={props.airQualityUrl}
                 date={props.date}
             />
