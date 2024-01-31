09ec4dd18f669e8342111ec6829e1d711ed0c23e
Garrett Reinard <garrett@careevolution.com>
2024-01-31 13:38:26 -0500

    Fix new view exports.

--- src/components/asthma/views/AsthmaLibraryCategoryView/index.ts
+++ src/components/asthma/views/AsthmaLibraryCategoryView/index.ts
@@ -1,1 +1,2 @@
 export { default } from './AsthmaLibraryCategoryView';
+export { AsthmaLibraryCategoryViewPreviewState } from './AsthmaLibraryCategoryView.previewData';
--- src/components/asthma/views/index.ts
+++ src/components/asthma/views/index.ts
@@ -4,5 +4,6 @@ export { default as AsthmaAirQualityView } from './AsthmaAirQualityView';
 export { default as AsthmaAlertTakeoverView } from './AsthmaAlertTakeoverView';
 export { default as AsthmaDayView, AsthmaDayViewPreviewState } from './AsthmaDayView';
 export { default as AsthmaHeartAndLungsView } from './AsthmaHeartAndLungsView';
+export { default as AsthmaLibraryCategoryView, AsthmaLibraryCategoryViewPreviewState } from './AsthmaLibraryCategoryView';
 export { default as AsthmaLogEntryEditorView, AsthmaLogEntryEditorViewPreviewState } from './AsthmaLogEntryEditorView';
 export { default as AsthmaSleepView } from './AsthmaSleepView';
