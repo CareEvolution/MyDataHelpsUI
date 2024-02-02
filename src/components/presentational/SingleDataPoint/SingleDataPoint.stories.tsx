22e28b0dbd36e573eda0ad2968ce55b609919fc9
Garrett Reinard <garrett@careevolution.com>
2024-02-02 11:08:55 -0500

    Rename DataSummary to SingleDataPoint.

--- src/components/presentational/SingleDataPoint/SingleDataPoint.stories.tsx
+++ src/components/presentational/SingleDataPoint/SingleDataPoint.stories.tsx
@@ -1,28 +1,30 @@
 import React from 'react';
-import DataSummary, { DataSummaryProps } from './DataSummary';
+import SingleDataPoint, { SingleDataPointProps } from './SingleDataPoint';
 import { Card, Layout } from '..';
 import { noop } from '../../../helpers/functions';
 
 export default {
-    title: 'Presentational/DataSummary',
-    component: DataSummary,
+    title: 'Presentational/SingleDataPoint',
+    component: SingleDataPoint,
     parameters: {layout: 'fullscreen'}
 };
 
-interface DataSummaryStoryProps extends DataSummaryProps {
+interface SingleDataPointStoryProps extends SingleDataPointProps {
+    colorScheme: 'auto' | 'light' | 'dark';
     clickable: boolean;
 }
 
-const render = (args: DataSummaryStoryProps) => {
-    return <Layout colorScheme="auto">
+const render = (args: SingleDataPointStoryProps) => {
+    return <Layout colorScheme={args.colorScheme}>
         <Card style={{padding: '16px'}}>
-            <DataSummary {...args} onClick={args.clickable ? noop : undefined}/>
+            <SingleDataPoint {...args} onClick={args.clickable ? noop : undefined}/>
         </Card>
     </Layout>;
 };
 
 export const Default = {
     args: {
+        colorScheme: 'auto',
         hasValue: true,
         value: 64,
         units: 'BPM',
@@ -32,6 +34,11 @@ export const Default = {
         clickable: false
     },
     argTypes: {
+        colorScheme: {
+            name: 'color scheme',
+            control: 'radio',
+            options: ['auto', 'light', 'dark']
+        },
         hasValue: {
             name: 'has value'
         },
