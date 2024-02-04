cc0b5e11062a53e459160fd00fe200fba478d757
chrisnowak <chris@careevolution.com>
2024-02-04 11:17:37 -0500

    Icon selector tweaks

--- src/components/symptom-shark/presentational/IconSelector/IconSelector.tsx
+++ src/components/symptom-shark/presentational/IconSelector/IconSelector.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import "./IconSelector.css"
 import { UnstyledButton } from "../../../presentational";
-import { logEntryIcons } from "../../helpers/icons";
+import LogEntryIcon, { LogEntryIconKey } from "../LogEntryIcon/LogEntryIcon";
 
 interface IconSelectorProps {
     onIconSelect(icon: string): void;
 }
 
 export default function (props: IconSelectorProps) {
-    var iconOrder = [
+    var iconOrder: LogEntryIconKey[] = [
         'star-o',
         'star',
         'user-md',
@@ -25,10 +25,10 @@ export default function (props: IconSelectorProps) {
     ];
 
 
-    return <div className="icon-selector">
+    return <div className="ss-icon-selector">
         {iconOrder.map(i =>
-            <UnstyledButton key={i} title={i} className="unstyled-button icon-item" onClick={() => props.onIconSelect(i)}>
-                {logEntryIcons[i]}
+            <UnstyledButton key={i} title={i} className="ss-icon-item" onClick={() => props.onIconSelect(i)}>
+                <LogEntryIcon icon={i} />
             </UnstyledButton>
         )}
     </div >;
