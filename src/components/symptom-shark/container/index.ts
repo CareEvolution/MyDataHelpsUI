e8807a399ab379746084251b87532d90145d94ac
chrisnowak <chris@careevolution.com>
2024-02-04 16:55:08 -0500

    Exports

--- src/components/symptom-shark/container/index.ts
+++ src/components/symptom-shark/container/index.ts
@@ -1,4 +1,5 @@
 export { default as SymptomSharkLogEntryList } from "./LogEntryList"
 export { default as SymptomSharkLogToday } from "./LogToday"
 export { default as SymptomSharkLogEntryEdit } from "./LogEntryEdit"
+export { default as SymptomSharkReportBuilder } from "./ReportBuilder"
 export { default as SymptomSharkVisualizationCoordinator } from "./VisualizationCoordinator"
