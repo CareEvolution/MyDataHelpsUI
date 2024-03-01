ada59a703fdb46a1263b7ad50a27800dcafc80f5
Adam Mencarini <adam@careevolution.com>
2024-03-01 12:38:21 -0500

    Fix stories copy/paste

--- src/components/container/SurveyDataChart/SurveyDataChart.stories.tsx
+++ src/components/container/SurveyDataChart/SurveyDataChart.stories.tsx
@@ -14,12 +14,13 @@ export const ffwelCreativeSelfChart = {
         title: "Steps",
         options: {
             domainMin: 0,
-            lineColor: ["#e41a1c", "#377eb8"]
+            lineColor: ["#e41a1c", "#377eb8", "#4daf4a"]
         },
         intervalType: "Week",
         weekStartsOn: "6DaysAgo",
-        lines: [{ label: "Creative Self", surveyName: "FFWEL", stepIdentifier: "CreativeSelf", resultIdentifier: "CreativeSelf", stroke: "#377eb8" },
-                { label: "Coping Self", surveyName: "FFWEL", stepIdentifier: "CopingSelf", resultIdentifier: "CopingSelf", stroke: "#377eb8" }],
+        lines: [{ label: "Creative Self", surveyName: "FFWEL", stepIdentifier: "CreativeSelf", resultIdentifier: "CreativeSelf", stroke: "#e41a1c" },
+                { label: "Coping Self", surveyName: "FFWEL", stepIdentifier: "CopingSelf", resultIdentifier: "CopingSelf", stroke: "#377eb8" },
+                { label: "Social Self", surveyName: "FFWEL", stepIdentifier: "SocialSelf", resultIdentifier: "SocialSelf", stroke: "#4daf4a" }],
         dailyDataType: DailyDataType.Steps,
         valueFormatter: (value: number) => Number(value.toFixed(0)).toLocaleString(),
         chartType: "Line",
@@ -54,9 +55,25 @@ export const ffwelCreativeSelfChart = {
                         "stepIdentifier": "CreativeSelf",
                         "resultIdentifier": "CreativeSelf",
                         "answers": [
-                            "475"
+                            "75"
                         ],
                         "insertedDate": "2023-07-20T12:15:28.02Z",
+                    },
+                    {
+                        "id": "0e991f1c-f726-e211-aabd-cf59b359f543",
+                        "surveyResultID": "0e981f1c-f726-ee11-aabd-cf59b359f543",
+                        "surveyID": "31452ddc-f226-ee11-aabd-cf59b359f543",
+                        "surveyVersion": 1,
+                        "taskID": "5aeaea1e-f426-ee11-aabd-cf59b359f543",
+                        "surveyName": "FFWEL",
+                        "surveyDisplayName": "Five Factor Wellness Inventory",
+                        "date": "2023-07-24T08:15:18.545-04:00",
+                        "stepIdentifier": "CreativeSelf",
+                        "resultIdentifier": "CreativeSelf",
+                        "answers": [
+                            "85"
+                        ],
+                        "insertedDate": "2023-07-24T12:15:28.02Z",
                     }
                 ],
             },
@@ -90,126 +107,83 @@ export const ffwelCreativeSelfChart = {
                         "stepIdentifier": "CopingSelf",
                         "resultIdentifier": "CopingSelf",
                         "answers": [
-                            "407.894736842105"
+                            "70.894736842105"
                         ],
                         "insertedDate": "2023-07-20T12:15:28.05Z",
+                    },
+                    {
+                        "id": "1d991f1c-f726-ee11-aabd-cf59b359f343",
+                        "surveyResultID": "0e981f1c-f726-ee11-aabd-cf59b359f543",
+                        "surveyID": "31452ddc-f226-ee11-aabd-cf59b359f543",
+                        "surveyVersion": 1,
+                        "taskID": "5aeaea1e-f426-ee11-aabd-cf59b359f543",
+                        "surveyName": "FFWEL",
+                        "surveyDisplayName": "Five Factor Wellness Inventory",
+                        "date": "2023-07-24T08:15:20.384-04:00",
+                        "stepIdentifier": "CopingSelf",
+                        "resultIdentifier": "CopingSelf",
+                        "answers": [
+                            "50.894736842105"
+                        ],
+                        "insertedDate": "2023-07-24T12:15:28.05Z",
                     }
+                ],
+            },
+            {
+                "surveyAnswers": [
+                    {
+                        "id": "ac8b29c4-5747-ee11-aabe-c8d78ddb1645",
+                        "surveyResultID": "aa8a29c4-5747-ee11-aabe-c8d78ddb1645",
+                        "surveyID": "31452ddc-f226-ee11-aabd-cf59b359f543",
+                        "surveyVersion": 3,
+                        "taskID": "01b61889-5747-ee11-aabe-c8d78ddb1645",
+                        "surveyName": "FFWEL",
+                        "surveyDisplayName": "Five Factor Wellness Inventory",
+                        "date": "2023-08-30T13:07:51.239-04:00",
+                        "stepIdentifier": "SocialSelf",
+                        "resultIdentifier": "SocialSelf",
+                        "answers": [
+                            "90.7894736842105"
+                        ],
+                        "insertedDate": "2023-08-30T17:07:58.743Z",
+                    },
+                    {
+                        "id": "1d991f1c-f726-ee11-aabd-cf59b359f543",
+                        "surveyResultID": "0e981f1c-f726-ee11-aabd-cf59b359f543",
+                        "surveyID": "31452ddc-f226-ee11-aabd-cf59b359f543",
+                        "surveyVersion": 1,
+                        "taskID": "5aeaea1e-f426-ee11-aabd-cf59b359f543",
+                        "surveyName": "FFWEL",
+                        "surveyDisplayName": "Five Factor Wellness Inventory",
+                        "date": "2023-07-20T08:15:20.384-04:00",
+                        "stepIdentifier": "SocialSelf",
+                        "resultIdentifier": "SocialSelf",
+                        "answers": [
+                            "80.894736842105"
+                        ],
+                        "insertedDate": "2023-07-20T12:15:28.05Z",
+                    },
+                    {
+                        "id": "1d931f1c-f726-ee11-aabd-cf59b359f543",
+                        "surveyResultID": "0e981f1c-f726-ee11-aabd-cf59b359f543",
+                        "surveyID": "31452ddc-f226-ee11-aabd-cf59b359f543",
+                        "surveyVersion": 1,
+                        "taskID": "5aeaea1e-f426-ee11-aabd-cf59b359f543",
+                        "surveyName": "FFWEL",
+                        "surveyDisplayName": "Five Factor Wellness Inventory",
+                        "date": "2023-07-24T08:15:20.384-04:00",
+                        "stepIdentifier": "SocialSelf",
+                        "resultIdentifier": "SocialSelf",
+                        "answers": [
+                            "84.894736842105"
+                        ],
+                        "insertedDate": "2023-07-24T12:15:28.05Z",
+                    }
+
                 ],
             }
         ];
             return Promise.resolve(data);
-        }
-    },
-    render: render
-};
-
-export const stepsBarChart = {
-    args: {
-        title: "Steps",
-        options: {
-
-        },
-        intervalType: "Week",
-        weekStartsOn: "6DaysAgo",
-        dailyDataType: DailyDataType.Steps,
-        valueFormatter: (value: number) => Number(value.toFixed(0)).toLocaleString(),
-        chartType: "Bar",
-        previewDataProvider: (start: Date, end: Date) => {
-            let data: DailyDataQueryResult = {};
-            let currentDate = new Date(start);
-            while (currentDate < end) {
-                let dayKey = getDayKey(currentDate);
-                data[dayKey] = Math.random() * 10000 + 3000;
-                currentDate = add(currentDate, { days: 1 });
-            }
-            return Promise.resolve(data);
-        }
-    },
-    render: render
-};
-
-
-export const stepsLiveBarChart = {
-    args: {
-        title: "Steps",
-        options: {
-
-        },
-        intervalType: "Week",
-        weekStartsOn: "6DaysAgo",
-        dailyDataType: DailyDataType.Steps,
-        valueFormatter: (value: number) => Number(value.toFixed(0)).toLocaleString(),
-        chartType: "Bar"
-    },
-    render: render
-};
-
-export const stepsAreaChart = {
-    args: {
-        title: "Steps",
-        options: {
-            areaColor: "red",
-            lineColor: "green"
-        },
-        intervalType: "Week",
-        weekStartsOn: "6DaysAgo",
-        dailyDataType: DailyDataType.Steps,
-        valueFormatter: (value: number) => Number(value.toFixed(0)).toLocaleString(),
-        chartType: "Area",
-        previewDataProvider: (start: Date, end: Date) => {
-            let data: DailyDataQueryResult = {};
-            let currentDate = new Date(start);
-            while (currentDate < end) {
-                let dayKey = getDayKey(currentDate);
-                data[dayKey] = Math.random() * 10000 + 3000;
-                currentDate = add(currentDate, { days: 1 });
-            }
-            return Promise.resolve(data);
-        }
-    },
-    render: render
-};
-
-
-export const sleepBarChart = {
-    args: {
-        title: "Sleep",
-        intervalType: "Week",
-        weekStartsOn: "6DaysAgo",
-        dailyDataType: DailyDataType.FitbitSleepMinutes,
-        valueFormatter: (value: number) => {
-            var hours = Math.floor(value / 60);
-            var displayValue = hours > 0 ? (hours + "h ") : "";
-            displayValue = displayValue + (Math.round(value - (hours * 60)) + "m");
-            return displayValue;
-        },
-        valueConverter: (value: number) => {
-            return value / 60.0;
-        },
-        chartType: "Bar",
-        previewDataProvider: (start: Date, end: Date) => {
-            let data: DailyDataQueryResult = {};
-            let currentDate = new Date(start);
-            while (currentDate < end) {
-                let dayKey = getDayKey(currentDate);
-                data[dayKey] = Math.random() * 50 + 100;
-                currentDate = add(currentDate, { days: 1 });
-            }
-            return Promise.resolve(data);
-        }
-    },
-    render: render
-};
-
-export const noData = {
-    args: {
-        title: "Steps",
-        intervalType: "Week",
-        dailyDataType: DailyDataType.Steps,
-        valueFormatter: (value: number) => Number(value.toFixed(0)).toLocaleString(),
-        chartType: "Line",
-        previewDataProvider: (start: Date, end: Date) => {
-            return Promise.resolve({});
         }
     },
     render: render
