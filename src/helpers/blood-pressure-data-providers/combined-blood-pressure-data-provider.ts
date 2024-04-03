37270eb3113f7502181d7086813d0d343f69c3c3
Renee Fromhold <reneefromhold@gmail.com>
2024-04-03 12:35:44 -0400

    Support Omron in BP Viz

--- src/helpers/blood-pressure-data-providers/combined-blood-pressure-data-provider.ts
+++ src/helpers/blood-pressure-data-providers/combined-blood-pressure-data-provider.ts
@@ -2,7 +2,7 @@ import { BloodPressureDataPoint, SurveyBloodPressureDataParameters } from "."
 import surveyBloodPressureDataProvider from "./survey-blood-pressure-data-provider"
 import deviceDataBloodPressureDataProvider from "./device-blood-pressure-data-provider"
 
-export type BloodPressureDeviceDataSource = 'AppleHealth' | 'GoogleFit';
+export type BloodPressureDeviceDataSource = 'AppleHealth' | 'GoogleFit' | "Omron";
 
 export default async function (surveyDataSource?: SurveyBloodPressureDataParameters, bloodPressureDeviceDataSources?: BloodPressureDeviceDataSource[]): Promise<BloodPressureDataPoint[]> {
     let providers: Promise<BloodPressureDataPoint[]>[] = [];
@@ -14,9 +14,11 @@ export default async function (surveyDataSource?: SurveyBloodPressureDataParamet
     if (bloodPressureDeviceDataSources) {
         bloodPressureDeviceDataSources.forEach((source) => {
             if (source === 'AppleHealth') {
-                providers.push(deviceDataBloodPressureDataProvider("AppleHealth", "BloodPressureSystolic", "BloodPressureDiastolic"));
+                providers.push(deviceDataBloodPressureDataProvider(source, "BloodPressureSystolic", "BloodPressureDiastolic"));
             } else if (source === 'GoogleFit') {
-                providers.push(deviceDataBloodPressureDataProvider("GoogleFit", "blood_pressure_systolic", "blood_pressure_diastolic"));
+                providers.push(deviceDataBloodPressureDataProvider(source, "blood_pressure_systolic", "blood_pressure_diastolic"));
+            } else if (source === 'Omron') {
+                providers.push(deviceDataBloodPressureDataProvider(source, "BloodPressureSystolic", "BloodPressureDiastolic"));
             }
         });
     }
