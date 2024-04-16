5498f87493f04fccddc95cf57bc2ef08c97d55ac
Brandon Charnesky <brandoncharnesky@careevolution.com>
2024-04-16 11:31:13 -0400

    AppleHealthSleep - new types for Oura (#240)
    * Add new asleep types (AsleepREM, AsleepCore, AsleepDeep)
    * Add AppleHealthSleep to DeviceDataMonthChart

--- src/helpers/strings-es.ts
+++ src/helpers/strings-es.ts
@@ -181,6 +181,8 @@
     "blood-type": "Tipo de sangre",
     "device-data-month-chart-no-data": "Sin Datos",
     "device-data-month-chart-daily-average": "Promedio Diario",
+    "device-data-month-chart-minutes": "Minutos",
+    "device-data-month-chart-sleep": "Dormir",
 };
 
 export default strings;
