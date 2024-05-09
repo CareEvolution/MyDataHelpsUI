42e6eacd56c0c96303c814286868eef1adefcf51
Garrett Reinard <garrett@careevolution.com>
2024-05-09 11:09:17 -0400

    Auto launch logging one time after post enrollment activities have been completed.

--- src/components/asthma/components/AsthmaPostEnrollmentSurveyTrigger/AsthmaPostEnrollmentSurveyTrigger.tsx
+++ src/components/asthma/components/AsthmaPostEnrollmentSurveyTrigger/AsthmaPostEnrollmentSurveyTrigger.tsx
@@ -1,29 +1,52 @@
 import React from 'react';
-import { useInitializeView } from '../../../../helpers/Initialization';
+import { useInitializeView } from '../../../../helpers';
 import { asthmaDataService } from '../../helpers';
-import MyDataHelps from '@careevolution/mydatahelps-js';
+import MyDataHelps, { SurveyAnswer } from '@careevolution/mydatahelps-js';
 
 export interface AsthmaPostEnrollmentSurveyTriggerProps {
     previewState?: boolean;
     postEnrollmentSurveyName: string;
     postEnrollmentMobileSurveyName: string;
+    logTodayEntrySurveyName: string;
     innerRef?: React.Ref<HTMLDivElement>;
 }
 
 export default function (props: AsthmaPostEnrollmentSurveyTriggerProps) {
 
+    const launchLoggingIfNecessary = (surveyAnswers: SurveyAnswer[]) => {
+        if (!surveyAnswers.find(sa => sa.surveyName === props.logTodayEntrySurveyName)) {
+            MyDataHelps.queryDeviceData({namespace: 'Project', type: 'LogAutoLaunched'}).then(results => {
+                if (!results.deviceDataPoints.length) {
+                    MyDataHelps.persistDeviceData([{type: 'LogAutoLaunched', value: new Date().toISOString()}]).then(() => {
+                        MyDataHelps.startSurvey(props.logTodayEntrySurveyName);
+                    });
+                }
+            });
+        }
+    };
+
     useInitializeView(() => {
         if (props.previewState) return;
 
-        asthmaDataService.loadSurveyAnswers([props.postEnrollmentSurveyName, props.postEnrollmentMobileSurveyName]).then(surveyAnswers => {
+        let surveyNames = [
+            props.postEnrollmentSurveyName,
+            props.postEnrollmentMobileSurveyName,
+            props.logTodayEntrySurveyName
+        ];
+
+        asthmaDataService.loadSurveyAnswers(surveyNames).then(surveyAnswers => {
             if (!surveyAnswers.find(sa => sa.surveyName === props.postEnrollmentSurveyName)) {
                 MyDataHelps.startSurvey(props.postEnrollmentSurveyName);
             } else if (!surveyAnswers.find(sa => sa.surveyName === props.postEnrollmentMobileSurveyName)) {
                 MyDataHelps.getDeviceInfo().then(deviceInfo => {
                     if (['iOS', 'Android'].includes(deviceInfo.platform)) {
                         MyDataHelps.startSurvey(props.postEnrollmentMobileSurveyName);
+                    } else {
+                        launchLoggingIfNecessary(surveyAnswers);
                     }
-                })
+                });
+            } else {
+                launchLoggingIfNecessary(surveyAnswers);
             }
         });
     });
