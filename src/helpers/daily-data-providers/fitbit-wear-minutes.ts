1a93c0bf4957555fba4584cce646d1dff88c1c34
chrisnowak <chris@careevolution.com>
2024-05-29 11:29:13 -0400

    Fix fitbit wear time calculation to use minutecount

--- src/helpers/daily-data-providers/fitbit-wear-minutes.ts
+++ src/helpers/daily-data-providers/fitbit-wear-minutes.ts
@@ -1,5 +1,21 @@
-import queryFitbitSingleDailyValue from "./query-fitbit-single-daily-value";
+import { add } from "date-fns";
+import queryAllDeviceData from "./query-all-device-data";
 
 export default function (startDate: Date, endDate: Date) {
-	return queryFitbitSingleDailyValue("HeartRateIntradayCount", startDate, endDate);
+	return queryAllDeviceData({
+		namespace: "Fitbit",
+		type: "HeartRateIntradayCount",
+		observedAfter: add(startDate, { days: -1 }).toISOString(),
+		observedBefore: add(endDate, { days: 1 }).toISOString()
+	}).then(function (result) {
+		var data: { [key: string]: number } = {};
+		result.forEach((d) => {
+			if (!d.startDate) { return; }
+			let minuteCount = parseInt(d.properties?.["MinuteCount"] || "0");
+			if (minuteCount == 0) { return; }
+			var dayKey = d.startDate!.substr(0, 10);
+			data[dayKey] = minuteCount;
+		});
+		return data;
+	});
 }
