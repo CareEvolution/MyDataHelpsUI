5728a8c3a3cec8b78ede884bce93950b63cbdf1d
Adam Mencarini <adam@careevolution.com>
2024-07-31 10:34:56 -0400

    Fix Area Chart color gradients

--- src/helpers/chartHelpers.tsx
+++ src/helpers/chartHelpers.tsx
@@ -95,10 +95,16 @@ export function createAreaChartDefs(layoutContext: LayoutContext, gradientKey: s
 
     return <defs>
         {series.map((s, i) =>
-            <linearGradient key={`lg-${s.dataKey}`} id={`${gradientKey}${i}`} x1="0" y1="0" x2="0" y2="1">
-                <stop offset="0%" stopColor={colorOrDefault(s.color, "var(--mdhui-color-primary)")} stopOpacity={0.5} />
-                <stop offset="100%" stopColor={colorOrDefault(s.color, "var(--mdhui-color-primary)")} stopOpacity={0.2} />
-            </linearGradient>
+            <>
+                <linearGradient key={`lg-${s.dataKey}-fill`} id={`${gradientKey}${i}-fill`} x1="0" y1="0" x2="0" y2="1">
+                    <stop offset="0%" stopColor={colorOrDefault(s.areaColor, "var(--mdhui-color-primary)")} stopOpacity={0.5} />
+                    <stop offset="100%" stopColor={colorOrDefault(s.areaColor, "var(--mdhui-color-primary)")} stopOpacity={0.2} />
+                </linearGradient>
+                <linearGradient key={`lg-${s.dataKey}-stroke`} id={`${gradientKey}${i}-stroke`} x1="0" y1="0" x2="0" y2="1">
+                    <stop offset="0%" stopColor={colorOrDefault(s.color, "var(--mdhui-color-primary)")} />
+                    <stop offset="100%" stopColor={colorOrDefault(s.color, "var(--mdhui-color-primary)")} />
+                </linearGradient>
+            </>
         )}
     </defs>;
 }
