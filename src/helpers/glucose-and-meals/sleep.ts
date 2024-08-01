ad4db66d9327796234e26158f3ef7178f2ba44a8
Garrett Reinard <garrett@careevolution.com>
2024-08-01 16:20:42 -0400

    Add V1 garmin sleep minutes provider to the glucose chart sleep data providers.

--- src/helpers/glucose-and-meals/sleep.ts
+++ src/helpers/glucose-and-meals/sleep.ts
@@ -1,12 +1,20 @@
 import MyDataHelps from '@careevolution/mydatahelps-js';
 import { getMaxValue } from './util';
-import { totalSleepMinutes } from '../daily-data-providers/fitbit-sleep-v2';
+import { totalSleepMinutes as totalSleepMinutesFitbit } from '../daily-data-providers/fitbit-sleep-v2';
+import { totalSleepMinutes as totalSleepMinutesGarmin } from '../daily-data-providers/garmin-sleep';
 import getDayKey from '../get-day-key';
 import { asleepTime } from '../daily-data-providers/apple-health-sleep';
 import { endOfDay, startOfDay } from 'date-fns';
 
 export function fitbitSleepMinutesProvider(date: Date): Promise<number | undefined> {
-    return totalSleepMinutes(startOfDay(date), endOfDay(date)).then(sleepData => {
+    return totalSleepMinutesFitbit(startOfDay(date), endOfDay(date)).then(sleepData => {
+        let dayKey = getDayKey(date);
+        return sleepData.hasOwnProperty(dayKey) ? sleepData[dayKey] : undefined;
+    }, () => undefined);
+}
+
+export function garminSleepMinutesProvider(date: Date): Promise<number | undefined> {
+    return totalSleepMinutesGarmin(startOfDay(date), endOfDay(date)).then(sleepData => {
         let dayKey = getDayKey(date);
         return sleepData.hasOwnProperty(dayKey) ? sleepData[dayKey] : undefined;
     }, () => undefined);
@@ -25,6 +33,9 @@ export async function getSleepMinutes(date: Date): Promise<number | undefined> {
     let settings = await MyDataHelps.getDataCollectionSettings();
     if (settings.fitbitEnabled) {
         providers.push(fitbitSleepMinutesProvider(date));
+    }
+    if (settings.garminEnabled) {
+        providers.push(garminSleepMinutesProvider(date));
     }
     if (settings.queryableDeviceDataTypes.find(s => s.namespace == 'AppleHealth' && s.type == 'SleepAnalysisInterval')) {
         providers.push(appleHealthSleepMinutesProvider(date));
