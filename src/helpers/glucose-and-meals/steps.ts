44b72c26a720b4bacdc4c3850ae72ea8250f2145
Garrett Reinard <garrett@careevolution.com>
2024-08-02 17:36:14 -0400

    Add support for Garmin half hour steps.

--- src/helpers/glucose-and-meals/steps.ts
+++ src/helpers/glucose-and-meals/steps.ts
@@ -26,6 +26,27 @@ export function fitbitHalfHourStepsDataProvider(date: Date): Promise<Reading[]> 
     }, () => []);
 }
 
+export function garminHalfHourStepsDataProvider(date: Date): Promise<Reading[]> {
+    const params: DeviceDataV2AggregateQuery = {
+        namespace: 'Garmin' as any,
+        type: 'epoch-steps',
+        observedAfter: startOfDay(date).toISOString(),
+        observedBefore: endOfDay(date).toISOString(),
+        intervalAmount: 30,
+        intervalType: 'Minutes',
+        aggregateFunctions: ['sum']
+    };
+
+    return queryAllDeviceDataV2(params).then(aggregates => {
+        return aggregates.map(aggregate => {
+            return {
+                timestamp: add(parseISO(aggregate.date), { minutes: 15 }),
+                value: aggregate.statistics['sum']
+            };
+        });
+    }, () => []);
+}
+
 export function appleHealthHalfHourStepsDataProvider(date: Date): Promise<Reading[]> {
     const params: DeviceDataPointQuery = {
         namespace: 'AppleHealth',
@@ -50,6 +71,9 @@ export async function getSteps(date: Date): Promise<Reading[]> {
     let settings = await MyDataHelps.getDataCollectionSettings();
     if (settings.fitbitEnabled) {
         providers.push(fitbitHalfHourStepsDataProvider(date));
+    }
+    if (settings.garminEnabled) {
+        providers.push(garminHalfHourStepsDataProvider(date));
     }
     if (settings.queryableDeviceDataTypes.find(s => s.namespace == 'AppleHealth' && s.type == 'HalfHourSteps')) {
         providers.push(appleHealthHalfHourStepsDataProvider(date));
