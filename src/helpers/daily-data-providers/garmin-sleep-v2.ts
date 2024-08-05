df66bb699f3ff189c541448a78f795b791741af6
Garrett Reinard <garrett@careevolution.com>
2024-08-05 17:14:53 -0400

    Remove temporary 'as any' modifiers on Garmin V2 data namespaces.

--- src/helpers/daily-data-providers/garmin-sleep-v2.ts
+++ src/helpers/daily-data-providers/garmin-sleep-v2.ts
@@ -1,19 +1,17 @@
 ﻿import { querySleepMinutesV2 } from './common-sleep-v2';
 
-// TODO: Remove namespace "as any" once MDH-JS gets updated.
-
 export function totalSleepMinutes(startDate: Date, endDate: Date) {
-    return querySleepMinutesV2('Garmin' as any, 'sleep-levels', startDate, endDate, ['light', 'rem', 'deep']);
+    return querySleepMinutesV2('Garmin', 'sleep-levels', startDate, endDate, ['light', 'rem', 'deep']);
 }
 
 export function lightSleepMinutes(startDate: Date, endDate: Date) {
-    return querySleepMinutesV2('Garmin' as any, 'sleep-levels', startDate, endDate, ['light']);
+    return querySleepMinutesV2('Garmin', 'sleep-levels', startDate, endDate, ['light']);
 }
 
 export function remSleepMinutes(startDate: Date, endDate: Date) {
-    return querySleepMinutesV2('Garmin' as any, 'sleep-levels', startDate, endDate, ['rem']);
+    return querySleepMinutesV2('Garmin', 'sleep-levels', startDate, endDate, ['rem']);
 }
 
 export function deepSleepMinutes(startDate: Date, endDate: Date) {
-    return querySleepMinutesV2('Garmin' as any, 'sleep-levels', startDate, endDate, ['deep']);
+    return querySleepMinutesV2('Garmin', 'sleep-levels', startDate, endDate, ['deep']);
 }
