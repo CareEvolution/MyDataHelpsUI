9dadc8af36b5743e8803672e4ae4563a8e2d0f4e
Garrett Reinard <garrett@careevolution.com>
2024-08-09 16:29:01 -0400

    Made some polish and preview data updates to the Inbox related components.

--- src/components/container/InboxItemList/InboxItemList.previewData.ts
+++ src/components/container/InboxItemList/InboxItemList.previewData.ts
@@ -1,7 +1,7 @@
 import { InboxItem, InboxMessage, InboxResource, InboxSurvey } from '@careevolution/mydatahelps-js';
-import resourceImage from "../../../assets/resource-image.png";
+import resourceImage from '../../../assets/resource-image.png';
 
-export type InboxItemListPreviewState = 'no items' | 'incomplete message' | 'incomplete survey' | 'incomplete resource' | 'incomplete items' | 'complete items';
+export type InboxItemListPreviewState = 'no items' | 'incomplete message' | 'incomplete survey' | 'incomplete resource' | 'incomplete message and survey' | 'incomplete message and resource' | 'incomplete survey and resource' | 'incomplete items' | 'complete items';
 
 export interface InboxItemListPreviewData {
     items: InboxItem[];
@@ -32,6 +32,54 @@ export const previewData: Record<InboxItemListPreviewState, InboxItemListPreview
     },
     'incomplete resource': {
         items: [
+            {
+                type: 'resource',
+                status: 'incomplete',
+                title: 'Using your Apple Watch',
+                subTitle: 'Setup and troubleshooting',
+                imageUrl: resourceImage
+            } as InboxResource
+        ]
+    },
+    'incomplete message and survey': {
+        items: [
+            {
+                type: 'message',
+                status: 'incomplete',
+                title: 'Congrats! You have a notification!'
+            } as InboxMessage,
+            {
+                type: 'survey',
+                status: 'incomplete',
+                name: 'Some Survey',
+                description: 'This is the survey description.'
+            } as InboxSurvey
+        ]
+    },
+    'incomplete message and resource': {
+        items: [
+            {
+                type: 'message',
+                status: 'incomplete',
+                title: 'Congrats! You have a notification!'
+            } as InboxMessage,
+            {
+                type: 'resource',
+                status: 'incomplete',
+                title: 'Using your Apple Watch',
+                subTitle: 'Setup and troubleshooting',
+                imageUrl: resourceImage
+            } as InboxResource
+        ]
+    },
+    'incomplete survey and resource': {
+        items: [
+            {
+                type: 'survey',
+                status: 'incomplete',
+                name: 'Some Survey',
+                description: 'This is the survey description.'
+            } as InboxSurvey,
             {
                 type: 'resource',
                 status: 'incomplete',
