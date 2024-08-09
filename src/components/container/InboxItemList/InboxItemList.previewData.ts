dc5fe1d05cb37d931b85f89ca9feeb0581b5e76a
Garrett Reinard <garrett@careevolution.com>
2024-08-09 18:27:29 -0400

    More inbox item list preview data updates.

--- src/components/container/InboxItemList/InboxItemList.previewData.ts
+++ src/components/container/InboxItemList/InboxItemList.previewData.ts
@@ -1,7 +1,22 @@
 import { InboxItem, InboxMessage, InboxResource, InboxSurvey } from '@careevolution/mydatahelps-js';
 import resourceImage from '../../../assets/resource-image.png';
 
-export type InboxItemListPreviewState = 'no items' | 'incomplete message' | 'incomplete survey' | 'incomplete resource' | 'incomplete message and survey' | 'incomplete message and resource' | 'incomplete survey and resource' | 'incomplete items' | 'complete items';
+export type InboxItemListPreviewState =
+    'no items'
+    | 'incomplete message'
+    | 'incomplete survey'
+    | 'incomplete resource'
+    | 'incomplete message and survey'
+    | 'incomplete message and resource'
+    | 'incomplete survey and resource'
+    | 'incomplete items'
+    | 'complete message'
+    | 'complete survey'
+    | 'complete resource'
+    | 'complete message and survey'
+    | 'complete message and resource'
+    | 'complete survey and resource'
+    | 'complete items';
 
 export interface InboxItemListPreviewData {
     items: InboxItem[];
@@ -108,6 +123,78 @@ export const previewData: Record<InboxItemListPreviewState, InboxItemListPreview
                 title: 'Using your Apple Watch',
                 subTitle: 'Setup and troubleshooting',
                 imageUrl: resourceImage
+            } as InboxResource
+        ]
+    },
+    'complete message': {
+        items: [
+            {
+                type: 'message',
+                status: 'complete',
+                title: 'Congrats! You have a notification!',
+            } as InboxMessage
+        ]
+    },
+    'complete survey': {
+        items: [
+            {
+                type: 'survey',
+                status: 'complete',
+                name: 'Some Survey',
+            } as InboxSurvey,
+        ]
+    },
+    'complete resource': {
+        items: [
+            {
+                type: 'resource',
+                status: 'complete',
+                title: 'Using your Apple Watch',
+                imageUrl: resourceImage,
+            } as InboxResource
+        ]
+    },
+    'complete message and survey': {
+        items: [
+            {
+                type: 'message',
+                status: 'complete',
+                title: 'Congrats! You have a notification!',
+            } as InboxMessage,
+            {
+                type: 'survey',
+                status: 'complete',
+                name: 'Some Survey',
+            } as InboxSurvey
+        ]
+    },
+    'complete message and resource': {
+        items: [
+            {
+                type: 'message',
+                status: 'complete',
+                title: 'Congrats! You have a notification!',
+            } as InboxMessage,
+            {
+                type: 'resource',
+                status: 'complete',
+                title: 'Using your Apple Watch',
+                imageUrl: resourceImage,
+            } as InboxResource
+        ]
+    },
+    'complete survey and resource': {
+        items: [
+            {
+                type: 'survey',
+                status: 'complete',
+                name: 'Some Survey',
+            } as InboxSurvey,
+            {
+                type: 'resource',
+                status: 'complete',
+                title: 'Using your Apple Watch',
+                imageUrl: resourceImage,
             } as InboxResource
         ]
     },
