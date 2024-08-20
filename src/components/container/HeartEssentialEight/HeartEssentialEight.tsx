fe6b17f7ed495d194d4c886cf1df33b3c9c5fb2d
Summer Schrader <n400@users.noreply.github.com>
2024-08-20 13:49:36 -0400

    HeartEssentialEight styling

--- src/components/container/HeartEssentialEight/HeartEssentialEight.tsx
+++ src/components/container/HeartEssentialEight/HeartEssentialEight.tsx
@@ -1,17 +1,17 @@
 import MyDataHelps from "@careevolution/mydatahelps-js";
 import { useInitializeView } from "../../../helpers";
-import { Action, LoadingIndicator, ProgressRing } from "../../presentational";
+import { Action, LoadingIndicator, ProgressRing, Card } from "../../presentational";
 import React, { useState } from "react";
 import { previewHeartEssentialData } from "./HeartEssentialEight.previewdata";
 import "./HeartEssentialEight.css";
-import iconCholesterol from "./controlcholesterol.svg";
-import iconEatWell from "./eatwell.svg";
-import iconGetActive from "./getactive.svg";
-import iconLoseWeight from "./loseweight.svg";
-import iconManageBloodPressure from "./managebloodpressure.svg";
-import iconReduceBloodSugar from "./reducebloodsugar.svg";
-import iconSleep from "./sleep.svg";
-import iconStopSmoking from "./stopsmoking.svg";
+import iconCholesterol from "./icons/controlcholesterol.svg";
+import iconEatWell from "./icons/eatwell.svg";
+import iconGetActive from "./icons/getactive.svg";
+import iconLoseWeight from "./icons/loseweight.svg";
+import iconManageBloodPressure from "./icons/managebloodpressure.svg";
+import iconReduceBloodSugar from "./icons/reducebloodsugar.svg";
+import iconSleep from "./icons/sleep.svg";
+import iconStopSmoking from "./icons/stopsmoking.svg";
 
 export type HeartEssentialEightScores =
   | "Activity"
@@ -97,39 +97,48 @@ export default function (props: HeartEssentialEightProps) {
           return str.toUpperCase();
         });
         let icon: any;
+        let subtitle: any;
         let url: string;
         const circleClasses: string[] = ["circle"];
         switch (key) {
           case "Activity":
             icon = iconGetActive;
+            subtitle="Display Active Time from Fitbit / Garmin.  Probably AppleExerciseTime for Apple Health. Or manual entry";
             circleClasses.push("activity");
             break;
           case "BloodPressure":
             icon = iconManageBloodPressure;
+            subtitle="Display most recent value from EHR, or manual entry";
             circleClasses.push("bloodPressure");
             break;
           case "BloodSugar":
             icon = iconReduceBloodSugar;
+            subtitle="Display Glucose numbers from Dexcom, or manual entry";
             circleClasses.push("bloodSugar");
             break;
           case "BMI":
             icon = iconLoseWeight;
+            subtitle="Display BMI calculated from survey data"
             circleClasses.push("bmi");
             break;
           case "Cholesterol":
             icon = iconCholesterol;
+            subtitle="Display most recent value from EHR, or manual entry";
             circleClasses.push("cholesterol");
             break;
           case "Diet":
             icon = iconEatWell;
+            subtitle="Eat more whole foods / Cut back on candy / Whatever the key points are from the survey";
             circleClasses.push("diet");
             break;
           case "Nicotine":
             icon = iconStopSmoking;
+            subtitle="Stop smoking / Congratulations on { 5+ years/never smoking }"
             circleClasses.push("nicotine");
             break;
           case "Sleep":
             icon = iconSleep;
+            subtitle="Display value from Apple Health/Fitbit/Garmin, or manual entry"
             circleClasses.push("sleep");
             break;
           default:
@@ -143,14 +152,13 @@ export default function (props: HeartEssentialEightProps) {
         );
 
         const achievement = (
-          <div className="actionRow">
             <Action
               key={`achievement-${key}`}
               title={title}
-              titleIcon={achievementIcon}
+              subtitle={subtitle}
+              icon={achievementIcon}
               onClick={() => MyDataHelps.openExternalUrl(url)}
             />
-          </div>
         );
         achievementList.push(achievement);
       }
@@ -165,8 +173,10 @@ export default function (props: HeartEssentialEightProps) {
     <>
       {score === undefined && <LoadingIndicator />}
       {score !== undefined && (
-        <div>
+      <>
+        <Card>
           <div className="mdhui-heart-e8-score-container">
+            <h3 className="le8-header">Heart Health Score</h3>
             <ProgressRing
               percentCompleted={score}
               color="rgb(193, 14, 33)"
@@ -178,27 +188,27 @@ export default function (props: HeartEssentialEightProps) {
               }
               animate={true}
             />
-
-            {achievementMap && (
-              <>
-                <div className="achievementSection">
-                  <div className="bulletPoint improve" />
-                  <h4>IMPROVE</h4>
-                </div>
-                <div className="achievementIcons">
-                  {generateAchievementMap(false, achievementMap)}
-                </div>
-                <div className="achievementSection">
-                  <div className="bulletPoint celebrate" />
-                  <h4>CELEBRATE</h4>
-                </div>
-                <div className="achievementIcons">
-                  {generateAchievementMap(true, achievementMap)}
-                </div>
-              </>
-            )}
           </div>
-        </div>
+        </Card>
+        {achievementMap && (
+        <Card>
+          <div className="mdhui-heart-e8-achievement-container">
+            <h3 className="le8-header">
+              Improve
+            </h3>
+            <div className="achievementActionList">
+              {generateAchievementMap(false, achievementMap)}
+            </div>
+            <h3 className="le8-header">
+              Celebrate
+            </h3>
+            <div className="achievementActionList">
+              {generateAchievementMap(true, achievementMap)}
+            </div>
+          </div>
+        </Card>
+        )}
+      </>
       )}
     </>
   );
