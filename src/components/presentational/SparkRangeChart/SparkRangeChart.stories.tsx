f8890ba96c3a9b1eb505f5b9b0a0fbc41c399daf
Garrett Reinard <garrett@careevolution.com>
2024-09-11 11:26:12 -0400

    Add SparkRangeChart.

---
+++ src/components/presentational/SparkRangeChart/SparkRangeChart.stories.tsx
@@ -0,0 +1,51 @@
+﻿import React from 'react'
+import SparkRangeChart from './SparkRangeChart'
+import { Layout } from '../../presentational'
+import { Meta, StoryObj } from '@storybook/react';
+
+type SparkRangeChartStoryArgs = React.ComponentProps<typeof SparkRangeChart> & {
+    colorScheme: 'auto' | 'light' | 'dark'
+};
+
+const meta: Meta<SparkRangeChartStoryArgs> = {
+    title: 'Presentational/SparkRangeChart',
+    component: SparkRangeChart,
+    parameters: {
+        layout: 'fullscreen'
+    },
+    render: args => {
+        return <Layout colorScheme={args.colorScheme}>
+            <SparkRangeChart {...args} />
+        </Layout>;
+    }
+};
+export default meta;
+
+type Story = StoryObj<SparkRangeChartStoryArgs>;
+
+export const Default: Story = {
+    args: {
+        colorScheme: 'auto',
+        previewState: 'default',
+        domain: [0, 100],
+        ranges: [
+            { min: 10, max: 80, average: 40 },
+            { min: 30, max: 90, average: 70 },
+            { min: 20, max: 85, average: 35 },
+            { min: 25, max: 80, average: 65 },
+            { min: 15, max: 75, average: 30 }
+        ]
+    },
+    argTypes: {
+        colorScheme: {
+            name: 'color scheme',
+            control: 'radio',
+            options: ['auto', 'light', 'dark']
+        },
+        previewState: {
+            name: 'state',
+            control: 'radio',
+            options: ['default']
+        }
+    }
+};
---
+++ src/components/presentational/SparkRangeChart/SparkRangeChart.tsx
@@ -0,0 +1,59 @@
+﻿import React, { useContext } from 'react';
+import './SparkRangeChart.css'
+import { ColorDefinition, getColorFromAssortment, resolveColor } from "../../../helpers";
+import { LayoutContext } from "../Layout";
+
+export interface SparkRangeChartRange {
+    min: number;
+    max: number;
+    average: number;
+    color?: ColorDefinition;
+}
+
+export interface SparkRangeChartProps {
+    previewState?: 'default';
+    domain: [number, number]
+    ranges: SparkRangeChartRange[],
+    innerRef?: React.Ref<HTMLDivElement>;
+}
+
+export default function (props: SparkRangeChartProps) {
+    const layoutContext = useContext(LayoutContext);
+    const chartWidth = 10;
+
+    const domainMin = Math.min(...props.domain);
+    const domainMax = Math.max(...props.domain);
+
+    const convertValueToPercent = (value: number): number => {
+        return ((value - domainMin) / (domainMax - domainMin)) * 100.0;
+    };
+
+    const createSvg = (range: SparkRangeChartRange, index: number) => {
+        const minValue = Math.max(range.min, domainMin);
+        const maxValue = Math.min(range.max, domainMax);
+
+        const minValuePercent = convertValueToPercent(minValue);
+        const maxValuePercent = convertValueToPercent(maxValue);
+
+        let averageValuePercent: number | undefined;
+        if (range.average != undefined) {
+            const averageValue = Math.min(Math.max(range.average, domainMin), domainMax);
+            averageValuePercent = convertValueToPercent(averageValue);
+        }
+
+        let color = resolveColor(layoutContext.colorScheme, range.color) ?? getColorFromAssortment(index);
+
+        return <svg key={index} width={chartWidth} height={50} fill="cyan" overflow="visible">
+            <line x1={0} y1={minValuePercent + '%'} x2={chartWidth} y2={minValuePercent + '%'} stroke={color} strokeWidth={2} />
+            <line x1={0} y1={maxValuePercent + '%'} x2={chartWidth} y2={maxValuePercent + '%'} stroke={color} strokeWidth={2} />
+            <line x1={chartWidth / 2} y1={minValuePercent + '%'} x2={chartWidth / 2} y2={maxValuePercent + '%'} stroke={color} strokeWidth={2} />
+            {averageValuePercent !== undefined &&
+                <circle cx={chartWidth / 2} cy={averageValuePercent + '%'} r={chartWidth / 2} fill={color} />
+            }
+        </svg>;
+    };
+
+    return <div className="mdhui-spark-range-chart" ref={props.innerRef}>
+        {props.ranges.map(createSvg)}
+    </div>;
+}
