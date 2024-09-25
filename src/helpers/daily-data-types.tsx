7752c12be2a4bb8eb2b6ec085b0c3045f7b2797f
Vlad Taralunga <vladt@careevolution.com>
2024-09-25 14:34:31 -0400

    Added numerOfAlcoholicBeverages to daily data providers.

--- src/helpers/daily-data-types.tsx
+++ src/helpers/daily-data-types.tsx
@@ -17,6 +17,7 @@ export enum DailyDataType {
 	AppleHealthSteps = "AppleHealthSteps",
 	AppleHealthWalkingHeartRateAverage = "AppleHealthWalkingHeartRateAverage",
 	AppleHealthActiveEnergyBurned = "AppleHealthActiveEnergyBurned",
+	AppleHealthNumberOfAlcoholicBeverages = "AppleHealthNumberOfAlcoholicBeverages",
 	FitbitSedentaryMinutes = "FitbitSedentaryMinutes",
 	FitbitActiveMinutes = "FitbitActiveMinutes",
 	FitbitLightlyActiveMinutes = "FitbitLightlyActiveMinutes",
