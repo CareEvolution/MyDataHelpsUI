d6808b7f89b8572c1e9c57427c2475ca36e4aa7d
Brandon Charnesky <brandoncharnesky@careevolution.com>
2024-09-30 15:12:45 -0400

    Add availability checks for v2 queries

--- src/helpers/daily-data-types/availability-check.ts
+++ src/helpers/daily-data-types/availability-check.ts
@@ -1,4 +1,4 @@
-import MyDataHelps, { DeviceDataNamespace, DeviceDataPointQuery } from "@careevolution/mydatahelps-js";
+import MyDataHelps, { DeviceDataNamespace, DeviceDataPointQuery, DeviceDataV2AggregateQuery, DeviceDataV2Namespace, DeviceDataV2Query } from "@careevolution/mydatahelps-js";
 
 export function simpleAvailabilityCheck(namespace: DeviceDataNamespace, type: string | string[]) {
 	return function (modifiedAfter?: Date) {
@@ -8,6 +8,49 @@ export function simpleAvailabilityCheck(namespace: DeviceDataNamespace, type: st
 		}
 		return MyDataHelps.queryDeviceData(parameters).then(function (result) {
 			return result.deviceDataPoints.length > 0;
+		}).catch(function () {
+			return false;
+		});
+	}
+}
+
+// todo: test
+export function simpleAvailabilityCheckV2(namespace: DeviceDataV2Namespace, type: string) {
+	return function (modifiedAfter?: Date) {
+		var parameters: DeviceDataV2Query = { 
+			namespace: namespace, 
+			type: type, 
+			limit: 1 
+		};
+		if (modifiedAfter) {
+			parameters.modifiedAfter = modifiedAfter.toISOString();
+		}
+		
+		return MyDataHelps.queryDeviceDataV2(parameters).then(function (result) {
+			return result.deviceDataPoints.length > 0;
+		}).catch(function () {
+			return false;
+		});
+	}
+}
+
+// todo: test
+export function simpleAvailabilityCheckV2Aggregate(namespace: DeviceDataV2Namespace, type: string, aggregateFunctions: string | string[]) {
+	return function (modifiedAfter?: Date) {
+		var parameters: DeviceDataV2AggregateQuery = { 
+			namespace: namespace, 
+			type: type, 
+			limit: 1,
+			intervalAmount: 1,
+			intervalType: "Days",
+			aggregateFunctions: aggregateFunctions
+		};
+		if (modifiedAfter) {
+			parameters.modifiedAfter = modifiedAfter.toISOString();
+		}
+		
+		return MyDataHelps.queryDeviceDataV2Aggregate(parameters).then(function (result) {
+			return result.intervals.length > 0;
 		}).catch(function () {
 			return false;
 		});
