7251c61f5b3a98b983c943ccc12fdd5508b672cc
Garrett Reinard <garrett@careevolution.com>
2024-10-17 11:46:27 -0400

    More value provider adjustments.

--- src/helpers/single-value-providers.ts
+++ src/helpers/single-value-providers.ts
@@ -7,22 +7,37 @@ export interface SingleValueProvider<T> {
     getValue: () => Promise<T | undefined>;
 }
 
+export interface StaticIntegerValueProvider extends SingleValueProvider<number> {
+    staticValue: number;
+}
+
+export interface RandomIntegerValueProvider extends SingleValueProvider<number> {
+    maxValue: number;
+}
+
+export interface CustomFieldIntegerValueProvider extends SingleValueProvider<number> {
+    customField: string;
+}
+
 export const ValueProviderFactory = {
-    createStaticIntegerValueProvider: (value: number): SingleValueProvider<number> => {
+    createStaticIntegerValueProvider: (staticValue: number): StaticIntegerValueProvider => {
         return {
             type: 'static integer',
-            getValue: () => Promise.resolve(value)
+            staticValue: staticValue,
+            getValue: () => Promise.resolve(staticValue)
         };
     },
-    createRandomIntegerValueProvider: (maxValue: number): SingleValueProvider<number> => {
+    createRandomIntegerValueProvider: (maxValue: number): RandomIntegerValueProvider => {
         return {
             type: 'random integer',
+            maxValue: maxValue,
             getValue: () => Promise.resolve(Math.round(Math.random() * maxValue))
         };
     },
-    createCustomFieldIntegerValueProvider: (customField: string): SingleValueProvider<number> => {
+    createCustomFieldIntegerValueProvider: (customField: string): CustomFieldIntegerValueProvider => {
         return {
             type: 'customField integer',
+            customField: customField,
             getValue: async () => {
                 if (!customField) return undefined;
 
