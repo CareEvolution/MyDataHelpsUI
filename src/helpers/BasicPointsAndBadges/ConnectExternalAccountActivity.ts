5879c78e6f6d3c4a3a4a37c8992368f43c6ee788
Renee Fromhold <reneefromhold@gmail.com>
2024-10-18 16:32:55 -0400

    Introduce jest unit test, unit tests for Custom and ConnectExternalAccount activity, and showing jsdoc component descriptions in the default story

--- src/helpers/BasicPointsAndBadges/ConnectExternalAccountActivity.ts
+++ src/helpers/BasicPointsAndBadges/ConnectExternalAccountActivity.ts
@@ -1,9 +1,11 @@
 import MyDataHelps from "@careevolution/mydatahelps-js";
 import type { BasicPointsForBadgesActivity, BasicPointsForBadgesActivityState } from "./Activities";
 
+export type ConnectExternalActivityType = "Provider" | "Health Plan" | "Device Manufacturer";
+
 export interface ConnectExternalAccountActivity extends BasicPointsForBadgesActivity {
     type: "connectExternalAccount";
-    providerCategories: string[];
+    providerCategories: ConnectExternalActivityType[];
 }
 
 interface ConnectExternalAccountActivityState extends BasicPointsForBadgesActivityState {
@@ -11,8 +13,14 @@ interface ConnectExternalAccountActivityState extends BasicPointsForBadgesActivi
 }
 
 export async function awardConnectExternalAccountActivityPoints(activity: ConnectExternalAccountActivity, activityState: ConnectExternalAccountActivityState) {
+    //convert to array to protect against new types added in consumers but not yet here
+    const activityProviderCategories = activity.providerCategories as string[]; 
+
+    //Get the external accounts
     const connectedAccounts = await MyDataHelps.getExternalAccounts();
-    const allProviders = connectedAccounts.filter(account => activity.providerCategories.includes(account.provider.category)).map(t => t.id);
+    //Filter for the accounts that match this activity. Return the external account id
+    const allProviders = connectedAccounts.filter(account => activityProviderCategories.includes(account.provider.category)).map(t => t.id);
+    //Filter the external accounts that have not already been connected
     const newProviders = allProviders.filter(provider => !activityState.providersConnected?.includes(provider));
     if (newProviders.length > 0) {
         const newPoints = newProviders.length * activity.points;
---
+++ src/helpers/BasicPointsAndBadges/CustomActivity.test.ts
@@ -0,0 +1,60 @@
+import { ParticipantInfo } from "@careevolution/mydatahelps-js";
+import { CustomActivity } from "../../../dist";
+import { awardCustomActivityPoints } from "./CustomActivity";
+
+describe("Custom Activity Awards", () => {
+    const pptInfo : ParticipantInfo = {
+        participantID: "123",
+        participantIdentifier: "123",
+        secondaryIdentifier: "123",
+        linkIdentifier: "123",
+        demographics: {
+            city: 'Naples',
+            utcOffset: '-05:00:00',
+            email: "abc@ce.com",
+            firstName: "John",
+            lastName: "Doe",
+            mobilePhone: "1234567890",
+            state: "FL",
+            middleName: "",
+            dateOfBirth: "",
+            gender: "",
+            preferredLanguage: "",
+            street1: "",
+            street2: "",
+            postalCode: "",
+            unsubscribedFromEmails: "",
+            timeZone: "",
+            unsubscribedFromSms: ""
+        },
+        enrollmentDate: new Date().toString(),
+        projectID: "123",
+        customFields: {
+            ["DailyGoals"]: "8"
+        }
+    };
+    it("should award points based on custom field value", async () => {
+         const activity : CustomActivity = {
+            key: "CustomGoals",
+            type: "custom",
+            points: 10,
+            customField: "DailyGoals"
+        };
+
+         const newActivityState = await awardCustomActivityPoints(activity, pptInfo);
+         expect(newActivityState.pointsAwarded).toBe(80);
+    });
+
+
+    it("should NOT award points where missing custom field value", async () => {
+        const activity : CustomActivity = {
+           key: "CustomGoals",
+           type: "custom",
+           points: 10,
+           customField: "DailyGoalsMissing"
+       };
+
+        const newActivityState = await awardCustomActivityPoints(activity, pptInfo);
+        expect(newActivityState.pointsAwarded).toBe(0);
+   });
+ });
