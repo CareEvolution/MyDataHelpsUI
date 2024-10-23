e65b6850c0c25fe89796b771e7b03ca731e76c56
Renee Fromhold <reneefromhold@gmail.com>
2024-10-23 12:00:48 -0400

    Mock fixture for ppt.

--- __tests__/helpers/BasicPointsAndBadges/CustomActivity.test.ts
+++ __tests__/helpers/BasicPointsAndBadges/CustomActivity.test.ts
@@ -1,37 +1,9 @@
-import { ParticipantInfo } from "@careevolution/mydatahelps-js";
+import { mockParticipantInfo } from "../../fixtures/participantInfo";
 import { awardCustomActivityPoints, CustomActivity } from "../../../src/helpers/BasicPointsAndBadges/CustomActivity";
 
 describe("Custom Activity Awards", () => {
-    const pptInfo : ParticipantInfo = {
-        participantID: "123",
-        participantIdentifier: "123",
-        secondaryIdentifier: "123",
-        linkIdentifier: "123",
-        demographics: {
-            city: 'Naples',
-            utcOffset: '-05:00:00',
-            email: "abc@ce.com",
-            firstName: "John",
-            lastName: "Doe",
-            mobilePhone: "1234567890",
-            state: "FL",
-            middleName: "",
-            dateOfBirth: "",
-            gender: "",
-            preferredLanguage: "",
-            street1: "",
-            street2: "",
-            postalCode: "",
-            unsubscribedFromEmails: "",
-            timeZone: "",
-            unsubscribedFromSms: ""
-        },
-        enrollmentDate: new Date().toString(),
-        projectID: "123",
-        customFields: {
-            ["DailyGoals"]: "8"
-        }
-    };
+    const pptInfo = {...mockParticipantInfo, customFields: { DailyGoals: "8" }};
+    
     it("should award points based on custom field value", async () => {
          const activity : CustomActivity = {
             key: "CustomGoals",
