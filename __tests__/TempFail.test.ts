0e4f8318e4c62f811894eb1c6ff5fe624920ac45
Renee Fromhold <reneefromhold@gmail.com>
2024-11-12 13:15:30 -0500

    Delete unit test fail test;

--- __tests__/TempFail.test.ts
+++
@@ -1,5 +0,0 @@
-describe("Testing the new github workflow", () => {
-    it("should fail and message should go to slack channel mdhui-build", async () => {
-         expect(true).toBe(false);
-    });
- });
