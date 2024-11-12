5f704acee4852616eac11bc8551f096f85b8009f
Renee Fromhold <reneefromhold@gmail.com>
2024-11-12 11:43:25 -0500

    Test failed test posting to slack

---
+++ __tests__/TempFail.test.ts
@@ -0,0 +1,5 @@
+describe("Testing the new github workflow", () => {
+    it("should fail and message should go to slack channel mdhui-build", async () => {
+         expect(true).toBe(false);
+    });
+ });
