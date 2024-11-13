8e42bd28ec3489fdf37e453a3118ddcd93139e9a
lynnfaraday <linda@careevolution.com>
2024-11-13 11:54:54 -0500

    Remove formatting overrides from relative today stories.

--- src/components/container/RelativeActivityToday/RelativeActivityToday.stories.tsx
+++ src/components/container/RelativeActivityToday/RelativeActivityToday.stories.tsx
@@ -25,47 +25,30 @@ let dataTypes = [{
     dailyDataType: DailyDataType.Steps,
     label: "Steps",
     icon: <FontAwesomeSvgIcon icon={faPersonRunning} />,
-    color: "rgba(255, 166, 102, 1)",
-    formatter: function (number: number) {
-        return Math.floor(number).toLocaleString()
-    }
+    color: "rgba(255, 166, 102, 1)"
 }, {
     dailyDataType: DailyDataType.FitbitActiveMinutes,
     label: "Active Minutes",
     icon: <FontAwesomeSvgIcon icon={faPersonRunning} />,
-    color: "rgba(255, 166, 102, 1)",
-    formatter: function (number: number) {
-        return Math.floor(number).toString() + " minutes";
-    }
+    color: "rgba(255, 166, 102, 1)"
 },
 {
     dailyDataType: DailyDataType.SleepMinutes,
     label: "Sleep Time",
     icon: <FontAwesomeSvgIcon icon={faBed} />,
-    color: "rgba(74, 144, 226, 1)",
-    formatter: function (number: number) {
-        var hours = Math.floor(number / 60);
-        var displayValue = hours + "h " + Math.round(number - (hours * 60)) + "m";
-        return displayValue;
-    }
+    color: "rgba(74, 144, 226, 1)"
 },
 {
     dailyDataType: DailyDataType.AppleHealthMaxHeartRate,
     label: "Max Heart Rate",
     icon: <FontAwesomeSvgIcon icon={faHeartbeat} />,
-    color: "rgba(239, 132, 129, 1)",
-    formatter: function (number: number) {
-        return Math.floor(number).toString() + " bpm";
-    }
+    color: "rgba(239, 132, 129, 1)"
 },
 {
     dailyDataType: DailyDataType.FitbitElevatedHeartRateMinutes,
     label: "Elevated Heart Rate Time",
     icon: <FontAwesomeSvgIcon icon={faHeartbeat} />,
-    color: "rgba(239, 132, 129, 1)",
-    formatter: function (number: number) {
-        return Math.floor(number).toString() + " minutes";
-    }
+    color: "rgba(239, 132, 129, 1)"
 }];
 
 export const Default = {
