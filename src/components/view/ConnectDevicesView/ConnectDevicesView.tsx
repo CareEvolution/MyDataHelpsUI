46dbba6d4446a153268c2b6d8a19b92cc167bbd1
chrisnowak <chris@careevolution.com>
2025-01-05 11:34:44 -0500

    Add Monthly activity view and refactor

--- src/components/view/ConnectDevicesView/ConnectDevicesView.tsx
+++ src/components/view/ConnectDevicesView/ConnectDevicesView.tsx
@@ -1,6 +1,11 @@
 import React from 'react'
-import { BlankView, Card } from "../.."
-import { ConnectDevicesMenu, FitbitDevices, GarminDevices, ProviderSearch } from '../../container'
+import { Action, BlankView, Card, CardTitle } from "../.."
+import { ConnectDevicesMenu, FitbitDevices, GarminDevices } from '../../container'
+import { useInitializeView } from '../../../helpers'
+import { faBarChart, faCalendarDay } from '@fortawesome/free-solid-svg-icons'
+import MyDataHelps from '@careevolution/mydatahelps-js'
+import { FontAwesomeSvgIcon } from 'react-fontawesome-svg-icon'
+import standardDailyDataTypes from '../../../helpers/daily-data-types/standard'
 
 export interface ConnectDevicesViewProps {
     externalAccountsApplicationUrl: string
@@ -9,9 +14,35 @@ export interface ConnectDevicesViewProps {
     colorScheme?: "auto" | "light" | "dark"
     enableAppleHealthSurvey?: string
     enableGoogleFitSurvey?: string
+    monthlyActivityViewUrl?: string
+    relativeDailyActivityViewUrl?: string
 }
 
 export default function ConnectDevicesView(props: ConnectDevicesViewProps) {
+    let [showDetailViews, setShowDetailViews] = React.useState(false);
+
+    useInitializeView(function () {
+        if (!props.relativeDailyActivityViewUrl && !props.monthlyActivityViewUrl) {
+            return;
+        }
+
+        if (props.previewState == "default") {
+            setShowDetailViews(true);
+            return;
+        }
+
+        //instead of using Promise.all, we can show detail views when the first availability check returns true
+        //to avoid waiting for all of the checks
+        standardDailyDataTypes.forEach(dataType => {
+            dataType.availabilityCheck().then(available => {
+                if (available) {
+                    setShowDetailViews(true);
+                }
+            });
+        });
+    }, []);
+
+
     return (
         <BlankView showBackButton={props.presentation == "Push"}
             showCloseButton={props.presentation == "Modal"}>
@@ -20,6 +51,17 @@ export default function ConnectDevicesView(props: ConnectDevicesViewProps) {
                     enableAppleHealthSurvey={props.enableAppleHealthSurvey}
                     enableGoogleFitSurvey={props.enableGoogleFitSurvey} />
             </Card>
+            {showDetailViews &&
+                <Card>
+                    <CardTitle title="My Activity" />
+                    {props.relativeDailyActivityViewUrl &&
+                        <Action bottomBorder icon={<FontAwesomeSvgIcon icon={faCalendarDay} color={"var(--mdhui-color-primary)"} />} subtitle="View Daily Activity" onClick={() => MyDataHelps.openApplication(props.relativeDailyActivityViewUrl!)} />
+                    }
+                    {props.monthlyActivityViewUrl &&
+                        <Action icon={<FontAwesomeSvgIcon icon={faBarChart} rotation={270} color={"var(--mdhui-color-primary)"} />} subtitle="View Montly Activity" onClick={() => MyDataHelps.openApplication(props.monthlyActivityViewUrl!)} />
+                    }
+                </Card>
+            }
             <Card>
                 <FitbitDevices previewState={props.previewState ? "connected" : undefined} />
             </Card>
--- src/components/view/RelativeDailyActivityView/RelativeDailyActivityView.tsx
+++ src/components/view/RelativeDailyActivityView/RelativeDailyActivityView.tsx
@@ -1,66 +1,22 @@
 import React from 'react'
 import { BlankView, Card, DateRangeTitle } from "../.."
 import { RelativeActivity, RelativeActivityDayCoordinator } from '../../container'
-import { DailyDataType, RelativeActivityDataType } from '../../../helpers'
+import { RelativeActivityDataType } from '../../../helpers'
+import standardDailyDataTypes from '../../../helpers/daily-data-types/standard'
 
 export interface RelativeDailyActivityViewProps {
     presentation?: "Modal" | "Push"
     previewState?: "default"
     colorScheme?: "auto" | "light" | "dark"
-    dataTypes?: RelativeActivityDataType[]
 }
 
-export default function ConnectDevicesView(props: RelativeDailyActivityViewProps) {
-    let activityColor = "#f5b722";
-    let sleepColor = "#7b88c6";
-    let heartColor = "#e35c33";
-
-    let dataTypes: RelativeActivityDataType[] = props.dataTypes ?? [
-        {
-            dailyDataType: DailyDataType.SleepMinutes,
-            color: sleepColor
-        },
-        {
-            dailyDataType: DailyDataType.Steps,
-            color: activityColor
-        },
-        {
-            dailyDataType: DailyDataType.FitbitActiveMinutes,
-            color: activityColor
-        },
-        {
-            dailyDataType: DailyDataType.GarminActiveMinutes,
-            color: activityColor
-        },
-        {
-            dailyDataType: DailyDataType.AppleHealthActiveEnergyBurned,
-            color: activityColor
-        },
-        {
-            dailyDataType: DailyDataType.GarminTotalCalories,
-            color: heartColor
-        },
-        {
-            dailyDataType: DailyDataType.FitbitCaloriesBurned,
-            color: heartColor
-        },
-        {
-            dailyDataType: DailyDataType.FitbitElevatedHeartRateMinutes,
-            color: heartColor
-        },
-        {
-            dailyDataType: DailyDataType.AppleHealthMaxHeartRate,
-            color: heartColor
-        },
-        {
-            dailyDataType: DailyDataType.GarminMaxHeartRate,
-            color: heartColor
-        },
-        {
-            dailyDataType: DailyDataType.RestingHeartRate,
-            color: heartColor
+export default function RelativeDailyActivityView(props: RelativeDailyActivityViewProps) {
+    let dataTypes = standardDailyDataTypes.map((dataType): RelativeActivityDataType => {
+        return {
+            dailyDataType: dataType.type,
+            color: dataType.color
         }
-    ];
+    });
 
     return (
         <BlankView showBackButton={props.presentation == "Push"}
