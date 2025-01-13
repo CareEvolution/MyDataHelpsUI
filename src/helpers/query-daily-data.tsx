8c1023f426cef2dca3367cd81aaa14bfce43b48a
Garrett Reinard <garrett@careevolution.com>
2025-01-13 15:37:12 -0500

    - New Apple Health steps data provider that requires corresponding watch wear. - Add support for custom combined daily data types.

--- src/helpers/query-daily-data.tsx
+++ src/helpers/query-daily-data.tsx
@@ -32,29 +32,50 @@ export function registerDailyDataTypeDefinition(typeDefinition: DailyDataTypeDef
 	dailyDataTypes.set(typeDefinition.type, typeDefinition);
 }
 
-export function checkDailyDataAvailability(type: string, modifiedAfter?: Date) {
-	var dailyDataType = dailyDataTypes.get(type);
-	if (!dailyDataType) { throw "Unknown data type:" + type; }
-	return dailyDataType.availabilityCheck(modifiedAfter);
+function parseDailyDataTypes(type: string) : DailyDataTypeDefinition[] {
+	const findDailyDataType = (type: string) : DailyDataTypeDefinition => {
+		const dailyDataType = dailyDataTypes.get(type);
+		if (!dailyDataType) {
+			throw "Unknown data type: " + type;
+		}
+		return dailyDataType;
+	};
+
+	if ( type.startsWith( "Custom:" )){
+		return (JSON.parse(type.substring(7)) as string[]).map(findDailyDataType);
+	}
+
+	return [findDailyDataType(type)];
 }
 
-export function queryDailyData(type: string, startDate: Date, endDate: Date, preview?: boolean) {
-	if (preview) {
-		return Promise.resolve(queryPreviewDailyData(type, startDate, endDate));
+export async function checkDailyDataAvailability(type: string, modifiedAfter?: Date): Promise<boolean> {
+	for (let dailyDataType of parseDailyDataTypes(type)) {
+		if (await dailyDataType.availabilityCheck(modifiedAfter)) {
+			return true;
+		}
 	}
-	var dailyDataType = dailyDataTypes.get(type);
-	if (!dailyDataType) { throw "Unknown data type:" + type; }
-	return dailyDataType.dataProvider(startDate, endDate).then(function (data) {
-		var result: DailyDataQueryResult = {};
-		while (startDate < endDate) {
-			var dayKey = getDayKey(startDate);
-			if (data[dayKey]) {
+	return false;
+}
+
+export async function queryDailyData(type: string, startDate: Date, endDate: Date, preview?: boolean): Promise<DailyDataQueryResult> {
+	if (preview) {
+		return await queryPreviewDailyData(type, startDate, endDate);
+	}
+
+	const result: DailyDataQueryResult = {};
+	for (let dailyDataType of parseDailyDataTypes(type)) {
+		const data = await dailyDataType.dataProvider(startDate, endDate);
+
+		let currentDate = startDate;
+		while (currentDate < endDate) {
+			const dayKey = getDayKey(currentDate);
+			if (!result[dayKey] && data[dayKey]) {
 				result[dayKey] = data[dayKey];
 			}
-			startDate = add(startDate, { days: 1 });
+			currentDate = add(currentDate, { days: 1 });
 		}
-		return result;
-	});
+	}
+	return result;
 }
 
 export async function queryPreviewDailyData(type: string, startDate: Date, endDate: Date, fillPercentage?: number) {
@@ -80,8 +101,8 @@ export function getAllDailyDataTypes() {
 	return Array.from(dailyDataTypes.values());
 }
 
-export function getDailyDataTypeDefinition(dataType: string): DailyDataTypeDefinition {
-	return dailyDataTypes.get(dataType)!;
+export function getDailyDataTypeDefinition(type: string): DailyDataTypeDefinition {
+	return parseDailyDataTypes(type)[0];
 }
 
 allTypeDefinitions.forEach(function (typeDefinition) {
