d27c309eba9622761152577646e4a13ffc7b2469
Garrett Reinard <garrett@careevolution.com>
2025-01-15 18:54:20 -0500

    PR Revision: Adjust the new Apple Health steps data provider to use the V2 API when determining watchwear dates.

--- src/helpers/daily-data-providers/apple-health-steps-while-wearing-watch.ts
+++ src/helpers/daily-data-providers/apple-health-steps-while-wearing-watch.ts
@@ -2,34 +2,39 @@
 import getDayKey from '../get-day-key';
 import queryAllDeviceData from './query-all-device-data';
 import { DailyDataQueryResult } from '../query-daily-data';
+import queryAllDeviceDataV2Aggregates from '../query-all-device-data-v2-aggregates';
 
-export default function (startDate: Date, endDate: Date): Promise<DailyDataQueryResult> {
-    return queryAllDeviceData({
+export default async function (startDate: Date, endDate: Date): Promise<DailyDataQueryResult> {
+    const watchDates = await queryAllDeviceDataV2Aggregates({
         namespace: 'AppleHealth',
-        type: ['Steps', 'HourlySteps'],
+        type: 'Steps',
+        observedAfter: add(startDate, { days: -1 }).toISOString(),
+        observedBefore: add(endDate, { days: 1 }).toISOString(),
+        dataSource: { deviceModel: 'Watch' },
+        intervalAmount: 1,
+        intervalType: 'Days',
+        aggregateFunctions: ['sum']
+    }).then(aggregates => aggregates.reduce((dates, aggregate) => {
+        const dayKey = getDayKey(parseISO(aggregate.date));
+        if (!dates.includes(dayKey) && aggregate.statistics['sum'] > 0) {
+            dates.push(dayKey);
+        }
+        return dates;
+    }, [] as string[]));
+
+    const hourlyStepsDataPoints = await queryAllDeviceData({
+        namespace: 'AppleHealth',
+        type: 'HourlySteps',
         observedAfter: add(startDate, { days: -1 }).toISOString(),
         observedBefore: add(endDate, { days: 1 }).toISOString()
-    }).then(dataPoints => {
-        const watchDates = dataPoints.filter(dataPoint =>
-            dataPoint.type === 'Steps'
-            && dataPoint.startDate
-            && dataPoint.source?.properties?.['SourceProductType']?.includes('Watch')
-            && parseInt(dataPoint.value) > 0
-        ).map(dataPoint => getDayKey(parseISO(dataPoint.startDate!)));
+    }).then(dataPoints => dataPoints.filter(dataPoint => dataPoint.startDate && parseInt(dataPoint.value) > 0));
 
-        const hourlyStepsDataPoints = dataPoints.filter(dataPoint =>
-            dataPoint.type === 'HourlySteps'
-            && dataPoint.startDate
-            && parseInt(dataPoint.value) > 0
-        );
-
-        const result: DailyDataQueryResult = {};
-        hourlyStepsDataPoints.forEach(dataPoint => {
-            const dayKey = getDayKey(parseISO(dataPoint.startDate!));
-            if (watchDates.includes(dayKey)) {
-                result[dayKey] = (result[dayKey] ?? 0) + parseInt(dataPoint.value);
-            }
-        });
-        return result;
+    const result: DailyDataQueryResult = {};
+    hourlyStepsDataPoints.forEach(dataPoint => {
+        const dayKey = getDayKey(parseISO(dataPoint.startDate!));
+        if (watchDates.includes(dayKey)) {
+            result[dayKey] = (result[dayKey] ?? 0) + parseInt(dataPoint.value);
+        }
     });
+    return result;
 }
