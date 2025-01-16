85cc22bc8d34682cd3e492b239b2828046af0cd1
Garrett Reinard <garrett@careevolution.com>
2025-01-16 12:16:07 -0500

    PR Revision: Update the new Apple Health steps provider to also include days with Oura steps.

--- src/helpers/daily-data-providers/apple-health-steps-while-wearing-device.ts
+++ src/helpers/daily-data-providers/apple-health-steps-while-wearing-device.ts
@@ -3,8 +3,19 @@ import getDayKey from '../get-day-key';
 import queryAllDeviceData from './query-all-device-data';
 import { DailyDataQueryResult } from '../query-daily-data';
 import queryAllDeviceDataV2Aggregates from '../query-all-device-data-v2-aggregates';
+import { DeviceDataV2Aggregate } from '@careevolution/mydatahelps-js';
 
 export default async function (startDate: Date, endDate: Date): Promise<DailyDataQueryResult> {
+    const convertAggregatesToDates = (aggregates: DeviceDataV2Aggregate[]): string[] => {
+        return aggregates.reduce((dates, aggregate) => {
+            const dayKey = getDayKey(parseISO(aggregate.date));
+            if (!dates.includes(dayKey) && aggregate.statistics['sum'] > 0) {
+                dates.push(dayKey);
+            }
+            return dates;
+        }, [] as string[]);
+    };
+
     const watchDates = await queryAllDeviceDataV2Aggregates({
         namespace: 'AppleHealth',
         type: 'Steps',
@@ -14,13 +25,18 @@ export default async function (startDate: Date, endDate: Date): Promise<DailyDat
         intervalAmount: 1,
         intervalType: 'Days',
         aggregateFunctions: ['sum']
-    }).then(aggregates => aggregates.reduce((dates, aggregate) => {
-        const dayKey = getDayKey(parseISO(aggregate.date));
-        if (!dates.includes(dayKey) && aggregate.statistics['sum'] > 0) {
-            dates.push(dayKey);
-        }
-        return dates;
-    }, [] as string[]));
+    }).then(convertAggregatesToDates);
+
+    const ouraDates = await queryAllDeviceDataV2Aggregates({
+        namespace: 'AppleHealth',
+        type: 'Steps',
+        observedAfter: add(startDate, { days: -1 }).toISOString(),
+        observedBefore: add(endDate, { days: 1 }).toISOString(),
+        dataSource: { sourceIdentifier: 'com.ouraring.oura' },
+        intervalAmount: 1,
+        intervalType: 'Days',
+        aggregateFunctions: ['sum']
+    }).then(convertAggregatesToDates);
 
     const hourlyStepsDataPoints = await queryAllDeviceData({
         namespace: 'AppleHealth',
@@ -32,7 +48,7 @@ export default async function (startDate: Date, endDate: Date): Promise<DailyDat
     const result: DailyDataQueryResult = {};
     hourlyStepsDataPoints.forEach(dataPoint => {
         const dayKey = getDayKey(parseISO(dataPoint.startDate!));
-        if (watchDates.includes(dayKey)) {
+        if (watchDates.includes(dayKey) || ouraDates.includes(dayKey)) {
             result[dayKey] = (result[dayKey] ?? 0) + parseInt(dataPoint.value);
         }
     });
