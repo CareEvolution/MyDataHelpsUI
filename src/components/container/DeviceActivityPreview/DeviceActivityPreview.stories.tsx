b20ec6c744135a06a01686d80e655a8fda70073c
chrisnowak <chris@careevolution.com>
2025-02-05 10:04:47 -0500

    Various improvements

--- src/components/container/DeviceActivityPreview/DeviceActivityPreview.stories.tsx
+++ src/components/container/DeviceActivityPreview/DeviceActivityPreview.stories.tsx
@@ -2,7 +2,6 @@ import React from "react"
 import DeviceActivityPreview, { DeviceActivityPreviewProps } from "./DeviceActivityPreview";
 import { Card, Layout } from "../../presentational";
 import { DailyDataType } from "../../../helpers/daily-data-types";
-import { RelativeActivityDataType } from "../../../helpers";
 
 export default {
     title: "Container/DeviceActivityPreview",
@@ -16,30 +15,25 @@ let render = (args: DeviceActivityPreviewProps) => <Layout colorScheme="auto">
     <Card><DeviceActivityPreview {...args} /></Card>
 </Layout>;
 
-let dataTypes: RelativeActivityDataType[] = [{
-    dailyDataType: DailyDataType.Steps,
-    color: "rgba(255, 166, 102, 1)"
-},
-{
-    dailyDataType: DailyDataType.SleepMinutes,
-    color: "rgba(74, 144, 226, 1)"
-}];
-
-export const NoThresholds = {
+export const StepsNoThreshold = {
     args: {
-        dataTypes: dataTypes,
+        dataType: {
+            dailyDataType: DailyDataType.Steps,
+            color: "rgba(255, 166, 102, 1)"
+        },
         previewState: "default"
     },
     render: render
 };
 
-let dataTypesWithThresholds: RelativeActivityDataType[] = [
-    { ...dataTypes[0], threshold: 5000, overThresholdColor: "rgba(255, 0, 0, 1)" },
-    { ...dataTypes[1], threshold: 60 * 8, overThresholdColor: "rgba(255, 0, 0, 1)" }]
-
-export const AllThresholds = {
+export const StepsWithThresholds = {
     args: {
-        dataTypes: dataTypesWithThresholds,
+        dataTypes: {
+            dailyDataType: DailyDataType.Steps,
+            color: "rgba(255, 166, 102, 1)",
+            threshold: 5000,
+            overThresholdColor: "rgba(255, 0, 0, 1)"
+        },
         previewState: "default"
     },
     render: render
--- src/components/container/DeviceActivityPreview/DeviceActivityPreview.tsx
+++ src/components/container/DeviceActivityPreview/DeviceActivityPreview.tsx
@@ -1,82 +1,61 @@
 import React, { useContext, useState } from "react"
 import { getDailyDataTypeDefinition, getDayKey, language, queryRelativeActivity, RelativeActivityDataType, RelativeActivityQueryResult, resolveColor, useInitializeView } from "../../../helpers"
-import { Action, DateRangeContext, LayoutContext, LoadingIndicator, SparkBarChart, SparkBarChartBar } from "../../presentational"
+import { DateRangeContext, LayoutContext, SparkBarChart, SparkBarChartBar } from "../../presentational"
 import { add, startOfDay } from "date-fns"
-import icon from '../../../assets/fitness-wearable.svg';
 import "../HealthPreviewSection/HealthPreviewSection.css"
 import "./DeviceActivityPreview.css"
 
 export interface DeviceActivityPreviewProps {
     date?: Date
-    dataTypes: RelativeActivityDataType[]
+    dataType: RelativeActivityDataType
     previewState?: "default"
     innerRef?: React.Ref<HTMLDivElement>
 }
 
 export default function DeviceActivityPreview(props: DeviceActivityPreviewProps) {
     let context = useContext(LayoutContext);
-    let [results, setResults] = useState<{ [key: string]: { [key: string]: RelativeActivityQueryResult } } | undefined>(undefined);
+    let [results, setResults] = useState<{ [key: string]: RelativeActivityQueryResult } | undefined>(undefined);
     let dateRangeContext = useContext(DateRangeContext);
     let date = props.date ?? dateRangeContext?.intervalStart ?? startOfDay(new Date());
 
     function loadData() {
         //prevent requests from returning back out of order, since some of these can be long running
-        queryRelativeActivity(add(date, { days: -6 }), date, props.dataTypes, !!props.previewState).then(results => {
-            setResults(results);
+        queryRelativeActivity(add(date, { days: -6 }), date, [props.dataType], !!props.previewState).then(results => {
+            setResults(results[props.dataType.dailyDataType]);
         });
     }
 
     useInitializeView(loadData, ["externalAccountSyncComplete"]);
 
-    function drilldown() {
-
-    };
-
-    function getSparkBarChart(dataType: RelativeActivityDataType, data: { [key: string]: RelativeActivityQueryResult },) {
-        var bars: SparkBarChartBar[] = [];
-        for (var i = -6; i <= 0; i++) {
-            let dayKey = getDayKey(add(new Date(), { days: i }));
-            let dayData = data[dayKey];
-            bars.push({
-                barFillPercent: dayData.relativePercent,
-                color: (dayData.value > dayData.threshold ? (dataType.overThresholdColor ?? dataType.color) : dataType.color) || "var(--mdhui-color-primary)"
-            });
-        }
-
-        return <SparkBarChart style={{ height: "100%" }} bars={bars} averageFillPercent={0.5} />
+    if (!results) {
+        return null;
     }
 
-    function getPreviewItem(dataType: RelativeActivityDataType) {
-        let definition = getDailyDataTypeDefinition(dataType.dailyDataType);
-        let value = results![dataType.dailyDataType][getDayKey(new Date())].value;
-        let formatter = dataType.formatter ?? definition.formatter;
-        return <div className="mdhui-device-activity-preview-item" key={dataType.dailyDataType}>
-            <div className="mdhui-device-activity-preview-info">
-                <div className="mdhui-device-activity-preview-label">
-                    {language(getDailyDataTypeDefinition(dataType.dailyDataType).labelKey!)}
-                </div>
-                <span className="mdhui-device-activity-preview-value">
-                    <span style={{ color: resolveColor(context.colorScheme, dataType.color) || "var(--mdhui-color-primary)" }}>{dataType.icon ?? getDailyDataTypeDefinition(dataType.dailyDataType).icon}</span>
-                    &nbsp;{formatter(value)}
-                </span>
+    let definition = getDailyDataTypeDefinition(props.dataType.dailyDataType);
+    let value = results![getDayKey(new Date())].value;
+    let formatter = props.dataType.formatter ?? definition.formatter;
+    var bars: SparkBarChartBar[] = [];
+    for (var i = -6; i <= 0; i++) {
+        let dayKey = getDayKey(add(new Date(), { days: i }));
+        let dayData = results[dayKey];
+        bars.push({
+            barFillPercent: dayData.relativePercent,
+            color: (dayData.value > dayData.threshold ? (props.dataType.overThresholdColor ?? props.dataType.color) : props.dataType.color) || "var(--mdhui-color-primary)"
+        });
+    }
+
+    return <div ref={props.innerRef} className="mdhui-device-activity-preview" key={props.dataType.dailyDataType}>
+        <div className="mdhui-device-activity-preview-info">
+            <div className="mdhui-device-activity-preview-label">
+                {language(getDailyDataTypeDefinition(props.dataType.dailyDataType).labelKey!)}
             </div>
-            <div className="mdhui-device-activity-preview-chart">
-                {getSparkBarChart(dataType, results![dataType.dailyDataType])}
-            </div>
+            <span className="mdhui-device-activity-preview-value">
+                <span style={{ color: resolveColor(context.colorScheme, props.dataType.color) || "var(--mdhui-color-primary)" }}>{props.dataType.icon ?? getDailyDataTypeDefinition(props.dataType.dailyDataType).icon}</span>
+                &nbsp;{formatter(value)}
+            </span>
         </div>
-    }
-
-    return <div ref={props.innerRef} className="mdhui-device-activity-preview">
-
-        {!results &&
-            <LoadingIndicator />
-        }
-        <div className="mdhui-device-activity-preview-items">
-            {results && props.dataTypes.map(d =>
-                getPreviewItem(d)
-            )}
+        <div className="mdhui-device-activity-preview-chart">
+            <SparkBarChart style={{ height: "100%" }} bars={bars} averageFillPercent={0.5} />
         </div>
     </div>
-
-
 }
