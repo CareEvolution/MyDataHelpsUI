73208f6ec9b75c86f5501814a899754e93efcf44
Bangxin Hu <bxh@careevolution.com>
2025-02-07 16:10:11 -0500

    More rhr test cases

--- src/components/container/DeviceDataMonthChart/DeviceDataMonthChart.stories.tsx
+++ src/components/container/DeviceDataMonthChart/DeviceDataMonthChart.stories.tsx
@@ -48,6 +48,19 @@ FitbitSteps.args = {
 	syncId: "Fitbit"
 };
 
+export const HealthConnectAverageRestingHeartRate = Template.bind({});
+HealthConnectAverageRestingHeartRate.args = {
+	title: "Average Resting Heart Rate",
+	month: new Date().getMonth(),
+	year: new Date().getFullYear(),
+	lines: [{
+			showAverage: true,
+			dailyDataType: DailyDataType.HealthConnectAverageRestingHeartRate,
+			label: "Average Resting Heart Rate"
+		}],
+	syncId: "HealthConnect"
+};
+
 export const NoData = Template.bind({});
 NoData.args = {
 	title: "Resting Heart Rate",
