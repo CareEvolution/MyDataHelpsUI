179814c01f4cbd6f385d4c541710a80d7559f386
Kyle Nuemeier <kneumei@gmail.com>
2025-02-10 15:53:05 -0600

    Add DaysWithDataValueProvider

--- src/helpers/single-value-providers.ts
+++ src/helpers/single-value-providers.ts
@@ -1,6 +1,8 @@
 import MyDataHelps from '@careevolution/mydatahelps-js';
+import { queryDailyData } from './query-daily-data';
+import { startOfDay, subDays } from 'date-fns';
 
-export type SingleValueProviderType = 'static integer' | 'random integer' | 'custom field integer';
+export type SingleValueProviderType = 'static integer' | 'random integer' | 'custom field integer' | 'days with data';
 
 export abstract class SingleValueProvider<T> {
     protected constructor(public type: SingleValueProviderType) {
@@ -47,6 +49,19 @@ export class CustomFieldIntegerValueProvider extends SingleValueProvider<number>
         }
 
         return undefined;
+    }
+}
+
+export class DaysWithDataValueProvider extends SingleValueProvider<number> {
+    constructor(public dataType: string, public daysInPast: number) {
+        super('days with data');
+    }
+
+    async getValue(): Promise<number | undefined> {
+        const today = new Date();
+        const startDate = startOfDay(subDays(today, this.daysInPast));
+        const data = await queryDailyData(this.dataType, startDate, today);
+        return new Set(Object.keys(data)).size;
     }
 }
 
