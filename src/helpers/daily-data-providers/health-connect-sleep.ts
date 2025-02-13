9068e77e0e21661478ad394238fcb7afbb0866fe
Bangxin Hu <bxh@careevolution.com>
2025-02-12 21:20:03 -0500

    whitespace

--- src/helpers/daily-data-providers/health-connect-sleep.ts
+++ src/helpers/daily-data-providers/health-connect-sleep.ts
@@ -1,17 +1,27 @@
-﻿import { querySleepMinutesV2 } from './common-sleep-v2';
+﻿import { querySleepMinutesV2 } from "./common-sleep-v2";
 
 export function totalSleepMinutes(startDate: Date, endDate: Date) {
-    return querySleepMinutesV2('HealthConnect', 'sleep', startDate, endDate, ['STAGE_TYPE_LIGHT', 'STAGE_TYPE_REM', 'STAGE_TYPE_DEEP']);
+    return querySleepMinutesV2("HealthConnect", "sleep", startDate, endDate, [
+        "STAGE_TYPE_LIGHT",
+        "STAGE_TYPE_REM",
+        "STAGE_TYPE_DEEP",
+    ]);
 }
 
 export function lightSleepMinutes(startDate: Date, endDate: Date) {
-    return querySleepMinutesV2('HealthConnect', 'sleep', startDate, endDate, ['STAGE_TYPE_LIGHT']);
+    return querySleepMinutesV2("HealthConnect", "sleep", startDate, endDate, [
+        "STAGE_TYPE_LIGHT",
+    ]);
 }
 
 export function remSleepMinutes(startDate: Date, endDate: Date) {
-    return querySleepMinutesV2('HealthConnect', 'sleep', startDate, endDate, ['STAGE_TYPE_REM']);
+    return querySleepMinutesV2("HealthConnect", "sleep", startDate, endDate, [
+        "STAGE_TYPE_REM",
+    ]);
 }
 
 export function deepSleepMinutes(startDate: Date, endDate: Date) {
-    return querySleepMinutesV2('HealthConnect', 'sleep', startDate, endDate, ['STAGE_TYPE_DEEP']);
+    return querySleepMinutesV2("HealthConnect", "sleep", startDate, endDate, [
+        "STAGE_TYPE_DEEP",
+    ]);
 }
