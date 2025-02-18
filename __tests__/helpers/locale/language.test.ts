8dfc7cdddfc3cf3ddb066c5ab0794e1058309eed
basyl <88673856+basyl-durnan@users.noreply.github.com>
2025-02-18 12:54:57 -0500

    Switch to hyphens and downcase
    Per https://github.com/CareEvolution/MyDataHelpsWelcomeScreen/pull/52/files#r1955156105

--- __tests__/helpers/locale/language.test.ts
+++ __tests__/helpers/locale/language.test.ts
@@ -4,7 +4,7 @@ import { describe, it } from '@jest/globals';
 describe('Language Tests', () => {
     describe('getLanguageCodeFromIso', () => {
         it('Should return lowercase language code if specified.', () => {
-            const result = getLanguageCodeFromIso("EN-US");
+            const result = getLanguageCodeFromIso("en-us");
             expect(result).toBe("en");
         });
         it('Should return empty string if no language.', () => {
@@ -12,7 +12,7 @@ describe('Language Tests', () => {
             expect(result).toBe("");
         });
         it('Should handle underscores too.', () => {
-            const result = getLanguageCodeFromIso("en_UK");
+            const result = getLanguageCodeFromIso("en-uk");
             expect(result).toBe("en");
         });
     });
@@ -27,18 +27,18 @@ describe('Language Tests', () => {
             expect(result).toBe(undefined);
         });
         it('Should handle underscores too.', () => {
-            const result = getCountryCodeFromIso("en_UK");
+            const result = getCountryCodeFromIso("en-uk");
             expect(result).toBe("uk");
         });
     });
 
     describe('language', () => {
         it('Should return a string from a specific supported locale.', () => {
-            const result = language("settings", "PT-PT");
+            const result = language("settings", "pt-pt");
             expect(result).toBe("Definições");
         });
         it('Should return a string from the base language if specific locale unsupported.', () => {
-            const result = language("settings", "PT-xx");
+            const result = language("settings", "pt-xx");
             expect(result).toBe("Configurações");
         });
         it('Should return English if entire language is unsupported.', () => {
