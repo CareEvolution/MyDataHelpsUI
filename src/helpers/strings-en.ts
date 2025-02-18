9f8c49d7329ec9b243575d9f9885bcc5b6b40686
chrisnowak <chris@careevolution.com>
2025-02-18 11:12:46 -0500

    Fix sentence describing PHR sync prompt

--- src/helpers/strings-en.ts
+++ src/helpers/strings-en.ts
@@ -526,7 +526,7 @@
     "bonus": "Bonus",
     "syncing-data": "Syncing data...",
     "health-connect-phr-sync-title": "Sync with Health Connect",
-    "health-connect-phr-sync-prompt": "Choose health records to read to and write from Health Connect"
+    "health-connect-phr-sync-prompt": "Choose health records to read from and write to Health Connect"
 };
 
 export default strings;
