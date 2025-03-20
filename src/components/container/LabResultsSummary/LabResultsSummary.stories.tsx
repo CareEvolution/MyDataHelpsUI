00b0fb9a32c8d7f5fd600598b1e17cc18940be4c
Renee Fromhold <renee@careevolution.com>
2025-03-20 12:20:28 -0400

    Component interaction testing for HealthAndWellnessView components (#402)
    * Component interation testing for HealthAndWellnessView components

--- src/components/container/LabResultsSummary/LabResultsSummary.stories.tsx
+++ src/components/container/LabResultsSummary/LabResultsSummary.stories.tsx
@@ -1,44 +1,100 @@
 import React from "react"
-import { ComponentStory, ComponentMeta } from "@storybook/react"
 import LabResultsSummary, { LabResultsSummaryProps } from "./LabResultsSummary"
 import Layout from "../../presentational/Layout"
+import { Meta, StoryObj } from "@storybook/react"
+import { within, userEvent, expect, fn } from "@storybook/test"
+import { language } from "../../../helpers"
+import { Description } from "@storybook/blocks"
+import { action } from "@storybook/addon-actions"
+import { importantLabs } from "./LabResultsSummary.previewdata"
 
-export default {
+const meta: Meta<typeof LabResultsSummary> = {
     title: "Container/LabResultsSummary",
     component: LabResultsSummary,
     parameters: {
         layout: 'fullscreen',
+        docs: {
+            Description: <Description />
+        }
+    },
+    tags: ['autodocs']
+};
+
+export default meta;
+type Story = StoryObj<typeof LabResultsSummary>;
+
+const render = (args: LabResultsSummaryProps) => <Layout colorScheme="auto">
+    <LabResultsSummary {...args} />
+</Layout>;
+
+async function interaction(canvasElement: HTMLElement, args: LabResultsSummaryProps) {
+    const canvas = within(canvasElement);
+    const labResultsAction = await canvas.findByText(language(`lab-results-title`));
+
+    expect(labResultsAction).toBeInTheDocument();
+    const actionDiv = canvasElement.getElementsByClassName('mdhui-action');
+    expect(actionDiv.length).toBe(1);
+    await userEvent.click(actionDiv[0]);
+    expect(args.onClick).toHaveBeenCalled();
+
+    let expectedIndicatorValue = 0;
+    if (args.previewState === "ImportantLabs") {
+        expectedIndicatorValue = importantLabs.RecentLabs.TotalLabReports;
+        const expectedTermInfoButtons = importantLabs.ImportantLabs.flat().filter(lab => lab.TermInformation);
+        const onTermInfoButtons = canvasElement.getElementsByClassName(`mdhui-lab-result-with-sparkline-term-info`);
+        expect(onTermInfoButtons.length).toBe(expectedTermInfoButtons.length);
+
+        const onTermInfoButton = onTermInfoButtons[0];
+        await userEvent.click(onTermInfoButton);
+        expect(args.onViewTermInfo).toHaveBeenCalled();
+    } else {
+        expectedIndicatorValue = importantLabs.RecentLabs.TotalLabReports;
     }
-} as ComponentMeta<typeof LabResultsSummary>;
 
-const Template: ComponentStory<typeof LabResultsSummary> = (args: LabResultsSummaryProps) =>
-    <Layout colorScheme="auto">
-        <LabResultsSummary {...args} />
-    </Layout>;
+    const indicatorValue = canvasElement.getElementsByClassName(`mdhui-action-indicator-value`);
+    expect(indicatorValue.length).toBeGreaterThan(0);
 
+    if (indicatorValue.length > 0) {
+        const expectedStartsWith = indicatorValue[0]?.textContent?.startsWith(`${expectedIndicatorValue}`);
+        expect(expectedStartsWith).toBeTruthy();
+    }
+}
 
-export const ImportantLabs = Template.bind({});
-ImportantLabs.args = {
-    previewState: "ImportantLabs",
-    onClick: () => alert("Clicked"),
-    onViewTermInfo(termInfo) {
-        console.log(termInfo);
+export const ImportantLabs: Story = {
+    args: {
+        previewState: "ImportantLabs",
+        onClick: fn((...args) => action("Parent On Click")(...args)),
+        onViewTermInfo: fn((...args) => action(`Click term info ${args}`)(...args))
     },
-};
-
-export const RecentLabs = Template.bind({});
-RecentLabs.args = {
-    previewState: "RecentLabs",
-    onClick: () => alert("Clicked"),
-    onViewTermInfo(termInfo) {
-        console.log(termInfo);
+    play: async ({ canvasElement, args }) => {
+        await interaction(canvasElement, args);
     },
+    render: render
 };
 
-export const NoData = Template.bind({});
-NoData.args = {
-    previewState: "NoData"
+export const RecentLabs: Story = {
+    args: {
+        previewState: "RecentLabs",
+        onClick: fn((...args) => action("Parent On Click")(...args))
+    },
+    play: async ({ canvasElement, args }) => {
+        await interaction(canvasElement, args);
+    },
+    render: render
 };
 
-export const Live = Template.bind({});
-Live.args = {};
+export const NoData: Story = {
+    args: {
+        previewState: "NoData",
+    },
+    play: async ({ canvasElement, args }) => {
+        const component = canvasElement.getElementsByClassName(`mdhui-lab-results-summary`);
+        expect(component.length).toBe(0);
+    },
+    render: render
+};
+
+export const Live: Story = {
+    args: {},
+    render: render
+};
