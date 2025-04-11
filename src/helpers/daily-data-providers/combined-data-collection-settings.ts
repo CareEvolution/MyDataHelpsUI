8c15fb9eee3cd03735e83f98ac9233e76f4a2f5a
Bangxin Hu <bxh@careevolution.com>
2025-04-10 23:51:35 -0400

    Add Health Connect support to daily data types (#429)
    * Support exercise and rhr
    
    * Fix case
    
    * Get data back
    
    * Add HC to RestingHeartRateCalendar
    
    * Add HC to combined-rhr
    
    * More rhr test cases
    
    * Add HealthConnectMonthCharts
    
    * Replace exercise with steps
    
    * Whitespace
    
    * Add sleep components
    
    * whitespace
    
    * Add tests for sleep
    
    * Allow partial success in queryRelativeActivity
    
    * Update combined
    
    * Simplification
    
    * Remove HCMonthChart and HC steps
    
    * Update combined components
    
    * Reduce boilerplate code
    
    * Only invoke getDeviceDataV2AllDataTypes if HC is enabled
    
    * whitespace
    
    * Add glucose
    
    * Add live tests
    
    * Drop 'average'
    
    * Fix glucose date range
    
    * Fix test
    
    * Refactor combined logic
    
    * Update glucose
    
    * Fix naming
    
    * Bug fix
    
    * Remove setting cache. Pass setting in as a param. Compute setting only once per call.
    
    * Short circuit availability checks
    
    * Trailing comma none
    
    * Revert unnecessary test changes
    
    * Review
    
    * Fix merge flog
    
    * Fix merge flog
    
    * Add unit tests
    
    * Fix typo
    
    * Fix build
    
    * Test relative activity
    
    * Minor updates
    
    * Fix availability check
    
    * Review
    
    * Use mydatahelps-js 3.31.0
    
    * Do not set modifiedAfter when it is undefined.

---
+++ src/helpers/daily-data-providers/combined-data-collection-settings.ts
@@ -0,0 +1,25 @@
+import MyDataHelps, {
+    DataCollectionSettings as MDHDataCollectionSettings,
+    SupportedDeviceDataV2DataType
+} from "@careevolution/mydatahelps-js";
+
+export interface CombinedDataCollectionSettings {
+    settings: MDHDataCollectionSettings;
+    deviceDataV2Types: SupportedDeviceDataV2DataType[];
+}
+
+export async function getCombinedDataCollectionSettings(
+    useV2: boolean = false
+): Promise<CombinedDataCollectionSettings> {
+    const [settings, deviceDataV2Types] = await Promise.all([
+        MyDataHelps.getDataCollectionSettings(),
+        useV2
+            ? MyDataHelps.getDeviceDataV2AllDataTypes(true)
+            : Promise.resolve([])
+    ]);
+
+    return {
+        settings,
+        deviceDataV2Types
+    };
+}
