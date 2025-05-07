0bee3b798ddf6aa06418f58613ceeaffcc1996d1
Garrett Reinard <garrett@careevolution.com>
2025-05-07 16:07:00 -0400

    Add LatestBloodPressureReading component.

--- src/helpers/blood-pressure-data-providers/combined-blood-pressure-data-provider.ts
+++ src/helpers/blood-pressure-data-providers/combined-blood-pressure-data-provider.ts
@@ -1,8 +1,8 @@
-import { BloodPressureDataPoint, SurveyBloodPressureDataParameters } from "."
-import surveyBloodPressureDataProvider from "./survey-blood-pressure-data-provider"
-import deviceDataBloodPressureDataProvider from "./device-blood-pressure-data-provider"
+import { BloodPressureDataPoint, SurveyBloodPressureDataParameters } from '.'
+import surveyBloodPressureDataProvider from './survey-blood-pressure-data-provider'
+import deviceDataBloodPressureDataProvider from './device-blood-pressure-data-provider'
 
-export type BloodPressureDeviceDataSource = 'AppleHealth' | 'GoogleFit' | 'Omron';
+export type BloodPressureDeviceDataSource = 'AppleHealth' | 'GoogleFit' | 'Omron' | 'HealthConnect';
 
 export default async function (surveyDataSource?: SurveyBloodPressureDataParameters, bloodPressureDeviceDataSources?: BloodPressureDeviceDataSource[]): Promise<BloodPressureDataPoint[]> {
     let providers: Promise<BloodPressureDataPoint[]>[] = [];
@@ -14,11 +14,13 @@ export default async function (surveyDataSource?: SurveyBloodPressureDataParamet
     if (bloodPressureDeviceDataSources) {
         bloodPressureDeviceDataSources.forEach((source) => {
             if (source === 'AppleHealth') {
-                providers.push(deviceDataBloodPressureDataProvider(source, "BloodPressureSystolic", "BloodPressureDiastolic"));
+                providers.push(deviceDataBloodPressureDataProvider(source, 'BloodPressureSystolic', 'BloodPressureDiastolic'));
             } else if (source === 'GoogleFit') {
-                providers.push(deviceDataBloodPressureDataProvider(source, "blood_pressure_systolic", "blood_pressure_diastolic"));
+                providers.push(deviceDataBloodPressureDataProvider(source, 'blood_pressure_systolic', 'blood_pressure_diastolic'));
             } else if (source === 'Omron') {
-                providers.push(deviceDataBloodPressureDataProvider(source, "BloodPressureSystolic", "BloodPressureDiastolic"));
+                providers.push(deviceDataBloodPressureDataProvider(source, 'BloodPressureSystolic', 'BloodPressureDiastolic'));
+            } else if (source === 'HealthConnect') {
+                // TODO: providers.push(deviceDataBloodPressureDataProvider(source, 'blood-pressure-systolic', 'blood-pressure-diastolic'));
             }
         });
     }
