01a5abddc5f6b3c8bf729e44d11aa0634c2fc767
Garrett Reinard <garrett@careevolution.com>
2025-05-08 10:55:24 -0400

    Update the combined blood pressure data provider to use data collection settings.

--- src/helpers/blood-pressure-data-providers/combined-blood-pressure-data-provider.ts
+++ src/helpers/blood-pressure-data-providers/combined-blood-pressure-data-provider.ts
@@ -2,6 +2,8 @@ import { BloodPressureDataPoint, SurveyBloodPressureDataParameters } from '.'
 import surveyBloodPressureDataProvider from './survey-blood-pressure-data-provider'
 import deviceDataBloodPressureDataProvider from './device-data-blood-pressure-data-provider'
 import deviceDataBloodPressureDataProviderV2 from './device-data-v2-blood-pressure-data-provider'
+import { getCombinedDataCollectionSettings } from '../daily-data-providers/combined-data-collection-settings';
+import { DataCollectionSettings, SupportedDeviceDataV2DataType } from '@careevolution/mydatahelps-js';
 
 export type BloodPressureDeviceDataSource = 'AppleHealth' | 'GoogleFit' | 'Omron' | 'HealthConnect';
 
@@ -13,15 +15,18 @@ export default async function (surveyDataSource?: SurveyBloodPressureDataParamet
     }
 
     if (bloodPressureDeviceDataSources) {
+        const useV2 = bloodPressureDeviceDataSources.includes('HealthConnect');
+        const { settings, deviceDataV2Types } = await getCombinedDataCollectionSettings(useV2);
+
         bloodPressureDeviceDataSources.forEach(source => {
-            if (source === 'AppleHealth') {
-                providers.push(deviceDataBloodPressureDataProvider(source, 'BloodPressureSystolic', 'BloodPressureDiastolic'));
-            } else if (source === 'GoogleFit') {
-                providers.push(deviceDataBloodPressureDataProvider(source, 'blood_pressure_systolic', 'blood_pressure_diastolic'));
-            } else if (source === 'Omron') {
-                providers.push(deviceDataBloodPressureDataProvider(source, 'BloodPressureSystolic', 'BloodPressureDiastolic'));
-            } else if (source === 'HealthConnect') {
-                providers.push(deviceDataBloodPressureDataProviderV2(source, 'blood-pressure-systolic', 'blood-pressure-diastolic'));
+            if (source === 'AppleHealth' && appleHealthBloodPressureEnabled(settings)) {
+                providers.push(deviceDataBloodPressureDataProvider('AppleHealth', 'BloodPressureSystolic', 'BloodPressureDiastolic'));
+            } else if (source === 'GoogleFit' && googleFitBloodPressureEnabled(settings)) {
+                providers.push(deviceDataBloodPressureDataProvider('GoogleFit', 'blood_pressure_systolic', 'blood_pressure_diastolic'));
+            } else if (source === 'Omron' && settings.omronEnabled) {
+                providers.push(deviceDataBloodPressureDataProvider('Omron', 'BloodPressureSystolic', 'BloodPressureDiastolic'));
+            } else if (source === 'HealthConnect' && healthConnectBloodPressureEnabled(settings, deviceDataV2Types)) {
+                providers.push(deviceDataBloodPressureDataProviderV2('HealthConnect', 'blood-pressure-systolic', 'blood-pressure-diastolic'));
             }
         });
     }
@@ -30,3 +35,22 @@ export default async function (surveyDataSource?: SurveyBloodPressureDataParamet
 
     return (await Promise.all(providers)).reduce((allDataPoints, dataPoints) => allDataPoints.concat(dataPoints), []);
 }
+
+function appleHealthBloodPressureEnabled(settings: DataCollectionSettings): boolean {
+    return settings.appleHealthEnabled && settings.queryableDeviceDataTypes.some(
+        dt => dt.namespace === 'AppleHealth' && ['BloodPressureSystolic', 'BloodPressureDiastolic'].includes(dt.type)
+    );
+}
+
+function googleFitBloodPressureEnabled(settings: DataCollectionSettings): boolean {
+    return settings.googleFitEnabled && settings.queryableDeviceDataTypes.some(
+        dt => dt.namespace === 'GoogleFit' && ['blood_pressure_systolic', 'blood_pressure_diastolic'].includes(dt.type)
+    );
+}
+
+function healthConnectBloodPressureEnabled(settings: DataCollectionSettings, deviceDataV2Types: SupportedDeviceDataV2DataType[]): boolean {
+    return settings.healthConnectEnabled && deviceDataV2Types.some(
+        dt => dt.namespace === 'HealthConnect' && ['blood-pressure-systolic', 'blood-pressure-diastolic'].includes(dt.type)
+    );
+}
+
