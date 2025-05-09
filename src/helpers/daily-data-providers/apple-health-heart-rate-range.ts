5ed5592c6fa7d508eded619a4bae80041d6c788c
Garrett Reinard <garrett@careevolution.com>
2025-05-09 12:27:14 -0400

    Refactor helper function and simplify heart rate range collation.

--- src/helpers/daily-data-providers/apple-health-heart-rate-range.ts
+++ src/helpers/daily-data-providers/apple-health-heart-rate-range.ts
@@ -1,20 +1,21 @@
 ﻿import { DailyDataQueryResult } from "../query-daily-data";
 import { getFloatValue, getStartDate, queryForDailyData } from "./daily-data";
+import { DeviceDataPoint } from "@careevolution/mydatahelps-js";
 
 export default async function (startDate: Date, endDate: Date): Promise<DailyDataQueryResult> {
     const dailyData = await queryForDailyData("AppleHealth", ["HourlyMaximumHeartRate", "HourlyMinimumHeartRate"], startDate, endDate, getStartDate);
 
-    const result: DailyDataQueryResult = {};
-    Object.keys(dailyData).forEach(dayKey => {
-        const maxHeartRateDataPoints = dailyData[dayKey].filter(dataPoint => dataPoint.type === "HourlyMaximumHeartRate");
-        const maxHeartRate = maxHeartRateDataPoints.length > 0 ? Math.max(...maxHeartRateDataPoints.map(getFloatValue)) : 0;
+    const getHeartRate = (dataPoints: DeviceDataPoint[], type: string, aggregateFn: typeof Math.min | typeof Math.max): number => {
+        const typeFilteredDataPoints = dataPoints.filter(dataPoint => dataPoint.type === type);
+        return typeFilteredDataPoints.length > 0 ? aggregateFn(...typeFilteredDataPoints.map(getFloatValue)) : 0;
+    };
 
-        const minHeartRateDataPoints = dailyData[dayKey].filter(dataPoint => dataPoint.type === "HourlyMinimumHeartRate");
-        const minHeartRate = minHeartRateDataPoints.length > 0 ? Math.min(...minHeartRateDataPoints.map(getFloatValue)) : 0;
-
+    return Object.entries(dailyData).reduce((result, [dayKey, dataPoints]) => {
+        const maxHeartRate = getHeartRate(dataPoints, "HourlyMaximumHeartRate", Math.max);
+        const minHeartRate = getHeartRate(dataPoints, "HourlyMinimumHeartRate", Math.min);
         if (maxHeartRate && minHeartRate) {
             result[dayKey] = maxHeartRate - minHeartRate;
         }
-    });
-    return result;
+        return result;
+    }, {} as DailyDataQueryResult);
 }
