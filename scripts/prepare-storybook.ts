465b6da4398b191064601a4a31dae2a4a5862d15
Garrett Reinard <garrett@careevolution.com>
2025-05-30 17:35:13 -0400

    More PDF tinkering.

--- scripts/prepare-storybook.ts
+++ scripts/prepare-storybook.ts
@@ -6,7 +6,7 @@ const targetDir = path.resolve(__dirname, '../.storybook/public/assets');
 mkdirp.sync(targetDir);
 
 const src = path.resolve(__dirname, '../node_modules/pdfjs-dist/build/pdf.worker.min.mjs');
-const dest = path.join(targetDir, 'pdf.worker.min.mjs');
+const dest = path.join(targetDir, 'pdf.worker.min.js');
 
 fs.copyFileSync(src, dest);
 console.log(`Copied pdf.worker.min.mjs to ${dest}`);
