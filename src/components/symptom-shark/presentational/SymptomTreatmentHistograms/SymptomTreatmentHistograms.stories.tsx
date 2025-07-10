ba707a2ef477a3359fe15a58af4ef5e4e3e30a2e
chrisnowak <chris@careevolution.com>
2025-07-10 13:50:32 -0400

    Custom color for symptom treatment histograms

--- src/components/symptom-shark/presentational/SymptomTreatmentHistograms/SymptomTreatmentHistograms.stories.tsx
+++ src/components/symptom-shark/presentational/SymptomTreatmentHistograms/SymptomTreatmentHistograms.stories.tsx
@@ -1,22 +1,62 @@
-import React from "react";
-import { DateRangeCoordinator, Layout } from "../../../presentational";
-import SymptomTreatmentHistograms, { SymptomTreatmentHistogramsProps } from "./SymptomTreatmentHistograms";
-import { SymptomSharkVisualizationCoordinator } from "../../container";
+import React from 'react';
+import { Layout } from '../../../presentational';
+import SymptomTreatmentHistograms, { SymptomTreatmentHistogramsProps } from './SymptomTreatmentHistograms';
+import VisualizationCoordinator from '../../container/VisualizationCoordinator/VisualizationCoordinator';
 
-export default { title: "SymptomShark/Presentational/SymptomTreatmentHistograms", component: SymptomTreatmentHistograms, parameters: { layout: 'fullscreen' } };
-let render = (args: SymptomTreatmentHistogramsProps) => <Layout colorScheme="auto">
-    <DateRangeCoordinator intervalType={"Month"} variant="rounded">
-        <SymptomSharkVisualizationCoordinator previewState="default" showFilters>
-            <SymptomTreatmentHistograms {...args} />
-        </SymptomSharkVisualizationCoordinator>
-    </DateRangeCoordinator>
-</Layout>
-
-let defaultProps: SymptomTreatmentHistogramsProps = {
-    onSymptomSelected: (symptomId: string) => { }
+export default {
+    title: 'Symptom Shark/SymptomTreatmentHistograms',
+    component: SymptomTreatmentHistograms,
+    parameters: { layout: 'fullscreen' }
 };
 
+// Sample data for the stories
+const sampleSymptoms = [
+    { id: 'symptom1', name: 'Headache', color: '#c4291c', severityTracking: 'Scale' },
+    { id: 'symptom2', name: 'Nausea', color: '#e35c33', severityTracking: 'Scale' },
+    { id: 'symptom3', name: 'Fatigue', color: '#5db37e', severityTracking: 'None' }
+];
+
+const sampleTreatments = [
+    { id: 'treatment1', name: 'Medication A', color: '#429bdf' },
+    { id: 'treatment2', name: 'Medication B', color: '#7b88c6' }
+];
+
+const sampleLogEntries = {
+    '2025-07-01': {
+        symptoms: [{ id: 'symptom1' }, { id: 'symptom2' }],
+        treatments: [{ id: 'treatment1' }]
+    },
+    '2025-07-02': {
+        symptoms: [{ id: 'symptom1' }],
+        treatments: [{ id: 'treatment1' }, { id: 'treatment2' }]
+    },
+    '2025-07-03': {
+        symptoms: [{ id: 'symptom3' }],
+        treatments: [{ id: 'treatment2' }]
+    }
+};
+
+const render = (args: SymptomTreatmentHistogramsProps) => (
+    <Layout colorScheme="auto">
+        <VisualizationCoordinator previewState="default">
+            <SymptomTreatmentHistograms {...args} />
+        </VisualizationCoordinator>
+    </Layout>
+);
+
 export const Default = {
-    args: defaultProps,
+    args: {
+        intervalStart: new Date(2025, 6, 1), // July 1, 2025
+        onSymptomSelected: (symptom: string, intervalStart: Date) => console.log('Selected symptom:', symptom, 'for interval starting:', intervalStart)
+    },
+    render: render
+};
+
+export const WithLinkColorDefinition = {
+    args: {
+        intervalStart: new Date(2025, 6, 1), // July 1, 2025
+        onSymptomSelected: (symptom: string, intervalStart: Date) => console.log('Selected symptom:', symptom, 'for interval starting:', intervalStart),
+        linkColor: { lightMode: '#71b345', darkMode: '#a1e375' }
+    },
     render: render
 };
