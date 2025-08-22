d22913fc3d1ce9fd4af7eb3ad0e3cb25134910f3
chrisnowak <chris@careevolution.com>
2025-08-22 10:12:19 -0400

    Fixes from review

--- src/components/symptom-shark/presentational/SymptomTreatmentHistograms/SymptomTreatmentHistograms.stories.tsx
+++ src/components/symptom-shark/presentational/SymptomTreatmentHistograms/SymptomTreatmentHistograms.stories.tsx
@@ -4,36 +4,9 @@ import SymptomTreatmentHistograms, { SymptomTreatmentHistogramsProps } from './S
 import VisualizationCoordinator from '../../container/VisualizationCoordinator/VisualizationCoordinator';
 
 export default {
-    title: 'Symptom Shark/SymptomTreatmentHistograms',
+    title: 'SymptomShark/SymptomTreatmentHistograms',
     component: SymptomTreatmentHistograms,
     parameters: { layout: 'fullscreen' }
-};
-
-// Sample data for the stories
-const sampleSymptoms = [
-    { id: 'symptom1', name: 'Headache', color: '#c4291c', severityTracking: 'Scale' },
-    { id: 'symptom2', name: 'Nausea', color: '#e35c33', severityTracking: 'Scale' },
-    { id: 'symptom3', name: 'Fatigue', color: '#5db37e', severityTracking: 'None' }
-];
-
-const sampleTreatments = [
-    { id: 'treatment1', name: 'Medication A', color: '#429bdf' },
-    { id: 'treatment2', name: 'Medication B', color: '#7b88c6' }
-];
-
-const sampleLogEntries = {
-    '2025-07-01': {
-        symptoms: [{ id: 'symptom1' }, { id: 'symptom2' }],
-        treatments: [{ id: 'treatment1' }]
-    },
-    '2025-07-02': {
-        symptoms: [{ id: 'symptom1' }],
-        treatments: [{ id: 'treatment1' }, { id: 'treatment2' }]
-    },
-    '2025-07-03': {
-        symptoms: [{ id: 'symptom3' }],
-        treatments: [{ id: 'treatment2' }]
-    }
 };
 
 const render = (args: SymptomTreatmentHistogramsProps) => (
