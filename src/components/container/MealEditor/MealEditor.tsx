d249d4d7d074f43162133fb3f7e4a31a05f3d5b2
Garrett Reinard <garrett@careevolution.com>
2025-09-04 10:41:22 -0400

    Fix the meal image input's accept value.

--- src/components/container/MealEditor/MealEditor.tsx
+++ src/components/container/MealEditor/MealEditor.tsx
@@ -222,7 +222,7 @@ export default function MealEditor(props: MealEditorProps) {
         return <label>
             <input
                 type="file"
-                accept="image/*;capture=camera"
+                accept="image/*"
                 style={{ display: 'none' }}
                 onChange={event => onFileChanged(event.target.files?.[0])}
             />
