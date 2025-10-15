da5bca1fd86ece645dc58f3309560c227e7db507
Garrett Reinard <garrett@careevolution.com>
2025-10-15 18:40:43 -0400

    Work in progress on a survey answer log.

---
+++ src/components/container/SurveyAnswerLogCalendar/SurveyAnswerLogCalendar.previewData.ts
@@ -0,0 +1,35 @@
+import { parseISO, startOfToday } from 'date-fns';
+import { fnvPredictableRandomNumber, generateSurveyAnswers, getDayKey, SurveyAnswerLog, SurveyAnswerRenderingConfiguration } from '../../../helpers';
+import { v4 as uuid } from 'uuid';
+import { CalendarDayStateConfiguration } from '../../presentational';
+import { SurveyAnswer } from '@careevolution/mydatahelps-js';
+
+export async function generatePreviewSurveyAnswerLogs(answerRenderingConfigurations: SurveyAnswerRenderingConfiguration[] | undefined, startDate: Date): Promise<Partial<Record<string, SurveyAnswerLog>>> {
+    const resultIdentifiers = answerRenderingConfigurations?.map(answerRenderingConfiguration => answerRenderingConfiguration.resultIdentifier)
+        ?? ['result1', 'result2', 'result3', 'result4'];
+    const surveyAnswers = (await generateSurveyAnswers(startDate, startOfToday(), resultIdentifiers, 0, 5, { days: 1 })).flat();
+
+    return surveyAnswers.reduce((surveyAnswerLogs, surveyAnswer) => {
+        const dayKey = getDayKey(surveyAnswer.date);
+        if (fnvPredictableRandomNumber(dayKey) % 5 !== 0) {
+            surveyAnswerLogs[dayKey] ??= { resultId: uuid(), date: parseISO(dayKey), surveyAnswers: [] };
+            if (fnvPredictableRandomNumber(dayKey + '_' + surveyAnswer.resultIdentifier) % 5 !== 0) {
+                surveyAnswerLogs[dayKey].surveyAnswers.push(surveyAnswer);
+            }
+        }
+        return surveyAnswerLogs;
+    }, {} as Record<string, SurveyAnswerLog>);
+}
+
+export function computePreviewState(stateConfiguration: CalendarDayStateConfiguration | undefined, date: Date, surveyAnswers: SurveyAnswer[]): string | undefined {
+    if (!stateConfiguration || !surveyAnswers || !surveyAnswers.length) {
+        return undefined;
+    }
+
+    const states = Object.keys(stateConfiguration).filter(key => !['today', 'future', 'no-data'].includes(key));
+    if (surveyAnswers.some(surveyAnswer => parseInt(surveyAnswer.answers[0]) > 0)) {
+        return states[fnvPredictableRandomNumber(getDayKey(date)) % states.length];
+    }
+    return undefined;
+}
+
---
+++ src/helpers/survey-answer/log.ts
@@ -0,0 +1,59 @@
+import { getDayKey } from '../index';
+import MyDataHelps, { SurveyAnswer } from '@careevolution/mydatahelps-js';
+import queryAllSurveyAnswers from '../query-all-survey-answers';
+import { parseISO } from 'date-fns';
+
+export interface SurveyAnswerLog {
+    resultId: string;
+    date: Date;
+    surveyAnswers: SurveyAnswer[];
+    event?: string;
+}
+
+export function enterSurveyAnswerLog(surveyName: string, priorSurveyAnswerLog?: SurveyAnswerLog, date?: Date) {
+    if (priorSurveyAnswerLog) {
+        if (priorSurveyAnswerLog.event) {
+            MyDataHelps.startSurvey(surveyName, { event: priorSurveyAnswerLog.event });
+        } else {
+            MyDataHelps.startSurvey(surveyName, { editResultID: priorSurveyAnswerLog.resultId });
+        }
+    } else if (date) {
+        MyDataHelps.startSurvey(surveyName, { event: getDayKey(date) });
+    } else {
+        MyDataHelps.startSurvey(surveyName);
+    }
+}
+
+export async function loadSurveyAnswerLog(surveyName: string, date: Date): Promise<SurveyAnswerLog | undefined> {
+    const surveyAnswerLogs = await loadSurveyAnswerLogs(surveyName);
+    return surveyAnswerLogs[getDayKey(date)];
+}
+
+export async function loadSurveyAnswerLogs(surveyName: string): Promise<Partial<Record<string, SurveyAnswerLog>>> {
+    const allSurveyAnswers = (await queryAllSurveyAnswers({ surveyName: surveyName })).sort((a, b) => b.date.localeCompare(a.date));
+
+    const latestResultIdsByDate = allSurveyAnswers.reduce((latestResultIdsByDate, surveyAnswer) => {
+        const dayKey = getSurveyAnswerDayKey(surveyAnswer);
+        latestResultIdsByDate[dayKey] ??= surveyAnswer.surveyResultID.toString();
+        return latestResultIdsByDate;
+    }, {} as Record<string, string>);
+
+    const latestResultIds = Object.values(latestResultIdsByDate);
+    const latestSurveyAnswers = allSurveyAnswers.filter(surveyAnswer => latestResultIds.includes(surveyAnswer.surveyResultID.toString()));
+
+    return latestSurveyAnswers.reduce((surveyAnswerLogs, surveyAnswer) => {
+        const dayKey = getSurveyAnswerDayKey(surveyAnswer);
+        surveyAnswerLogs[dayKey] ??= {
+            resultId: surveyAnswer.surveyResultID.toString(),
+            date: parseISO(dayKey),
+            surveyAnswers: [],
+            event: surveyAnswer.event
+        };
+        surveyAnswerLogs[dayKey].surveyAnswers.push(surveyAnswer);
+        return surveyAnswerLogs;
+    }, {} as Record<string, SurveyAnswerLog>);
+}
+
+export function getSurveyAnswerDayKey(surveyAnswer: SurveyAnswer): string {
+    return surveyAnswer.event ?? getDayKey(surveyAnswer.date);
+}
