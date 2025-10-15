5e3045ac457081096a1cc84053b5402290623686
Garrett Reinard <garrett@careevolution.com>
2025-10-15 18:40:43 -0400

    Work in progress on a survey answer log.

---
+++ src/components/container/SurveyAnswerLogCalendar/SurveyAnswerLogCalendar.stories.tsx
@@ -0,0 +1,202 @@
+import React, { CSSProperties } from 'react';
+import { CalendarDayState, DateRangeCoordinator, Layout } from '../../presentational';
+import { StoryObj } from '@storybook/react';
+import { argTypesToHide } from '../../../../.storybook/helpers';
+import SurveyAnswerLogCalendar from './SurveyAnswerLogCalendar';
+import { fnvPredictableRandomNumber, getDayKey, SurveyAnswerRenderingConfiguration } from '../../../helpers';
+import { faBed, faBicycle, faSwimmer, faWalking } from '@fortawesome/free-solid-svg-icons';
+import { SurveyAnswer } from '@careevolution/mydatahelps-js';
+import { isAfter, isSameDay } from 'date-fns';
+
+const customHighlightStyling: CSSProperties = {
+    boxShadow: 'inset -5px -5px 10px rgba(255, 255, 255, 0.3), inset 5px 5px 10px rgba(0, 0, 0, 0.3), 0 4px 6px rgba(0, 0, 0, 0.3)',
+    transition: 'border-radius: 0.5s ease, transform 0.2s ease, box-shadow 0.2s ease'
+};
+
+type SurveyAnswerLogCalendarStoryArgs = React.ComponentProps<typeof SurveyAnswerLogCalendar> & {
+    colorScheme: 'auto' | 'light' | 'dark';
+    state: 'loading' | 'default';
+    includeStates: boolean;
+    showLegend: boolean;
+    customizeToday: boolean;
+    customizeFuture: boolean;
+    customizeNoData: boolean;
+    showLogs: boolean;
+};
+
+export default {
+    title: 'Container/SurveyAnswerLogCalendar',
+    component: SurveyAnswerLogCalendar,
+    parameters: {
+        layout: 'fullscreen'
+    },
+    render: (args: SurveyAnswerLogCalendarStoryArgs) => {
+        const states: CalendarDayState[] = args.includeStates ? [
+            {
+                label: 'Sleep',
+                backgroundColor: '#664cda',
+                borderColor: '#664cda',
+                textColor: '#f4d6ff',
+                style: customHighlightStyling,
+                streakIdentifier: 'purple-state',
+                streakColor: '#664cda',
+                combineWhenSolo: true
+            },
+            {
+                label: 'Activity',
+                backgroundColor: '#3c973c',
+                borderColor: '#3c973c',
+                textColor: '#bdead7',
+                style: customHighlightStyling,
+                streakIdentifier: 'green-state',
+                streakColor: '#3c973c',
+                combineWhenSolo: true
+            },
+            {
+                label: 'Swimming',
+                backgroundColor: '#0877b8',
+                borderColor: '#0877b8',
+                textColor: '#abe0ff',
+                style: customHighlightStyling,
+                streakIdentifier: 'blue-state',
+                streakColor: '#0877b8',
+                combineWhenSolo: true
+            }
+        ] : [];
+
+        const answerRenderingConfigurations: SurveyAnswerRenderingConfiguration[] | undefined = args.showLogs ? [
+            {
+                resultIdentifier: 'activity',
+                icon: faWalking,
+                iconColor: '#3c973c',
+                label: 'Activity',
+                shouldHighlight: surveyAnswer => surveyAnswer.answers[0] !== '0',
+                customHighlightStyling: customHighlightStyling,
+                formatDisplayValue: surveyAnswer => `An activity level of ${surveyAnswer.answers[0]} was recorded on this day.`
+            },
+            {
+                resultIdentifier: 'sleep',
+                icon: faBed,
+                iconColor: '#664cda',
+                label: 'Sleep',
+                shouldHighlight: surveyAnswer => surveyAnswer.answers[0] !== '0',
+                customHighlightStyling: customHighlightStyling,
+                formatDisplayValue: surveyAnswer => `A sleep level of ${surveyAnswer.answers[0]} was recorded on this day.`
+            },
+            {
+                resultIdentifier: 'swimming',
+                icon: faSwimmer,
+                iconColor: '#0877b8',
+                label: 'Swimming',
+                shouldHighlight: surveyAnswer => surveyAnswer.answers[0] !== '0',
+                customHighlightStyling: customHighlightStyling,
+                formatDisplayValue: surveyAnswer => `A swimming level of ${surveyAnswer.answers[0]} was recorded on this day.`
+            },
+            {
+                resultIdentifier: 'cycling',
+                icon: faBicycle,
+                iconColor: '#976d1e',
+                label: 'Cycling',
+                shouldHighlight: surveyAnswer => surveyAnswer.answers[0] !== '0',
+                customHighlightStyling: customHighlightStyling,
+                formatDisplayValue: surveyAnswer => `A cycling level of ${surveyAnswer.answers[0]} was recorded on this day.`
+            }
+        ] : undefined;
+
+        const computePreviewStatesForDay = (states: CalendarDayState[], date: Date, surveyAnswers: SurveyAnswer[]): CalendarDayState[] => {
+            if (states.length === 0) {
+                return [];
+            }
+
+            if (surveyAnswers.some(surveyAnswer => parseInt(surveyAnswer.answers[0]) > 0)) {
+                const dayKey = getDayKey(date);
+                const statesCount = (fnvPredictableRandomNumber(dayKey + '-states-count') % states.length) + 1;
+
+                const statesToReturn: CalendarDayState[] = [];
+                let currentStateIndex = fnvPredictableRandomNumber(dayKey + '-states-start-index') % states.length;
+                while (statesToReturn.length < statesCount) {
+                    statesToReturn.push(states[currentStateIndex]);
+                    currentStateIndex = (currentStateIndex + 1) % states.length;
+                }
+
+                return statesToReturn.sort((a, b) => states.indexOf(a) - states.indexOf(b));
+            }
+
+            if (isSameDay(date, new Date())) return args.customizeToday ? [{ borderColor: '#fff' }] : [];
+            if (isAfter(date, new Date())) return args.customizeFuture ? [{ borderColor: '#fff' }] : [];
+            return args.customizeNoData ? [{ borderColor: '#fff' }] : [];
+        };
+
+        return <Layout colorScheme={args.colorScheme}>
+            <DateRangeCoordinator intervalType="Month">
+                <SurveyAnswerLogCalendar
+                    {...args}
+                    previewState={args.state}
+                    computeStatesForDay={(date, surveyAnswers) => computePreviewStatesForDay(states, date, surveyAnswers)}
+                    legend={args.showLegend ? states : undefined}
+                    answerRenderingConfigurations={answerRenderingConfigurations}
+                />
+            </DateRangeCoordinator>
+        </Layout>;
+    }
+};
+
+export const Default: StoryObj<SurveyAnswerLogCalendarStoryArgs> = {
+    args: {
+        colorScheme: 'auto',
+        state: 'default',
+        includeStates: true,
+        showLegend: false,
+        multiStateStartAngle: 270,
+        customizeToday: false,
+        customizeFuture: false,
+        customizeNoData: false,
+        showLogs: false
+    },
+    argTypes: {
+        colorScheme: {
+            name: 'color scheme',
+            control: 'radio',
+            options: ['auto', 'light', 'dark']
+        },
+        state: {
+            name: 'state',
+            control: 'radio',
+            options: ['loading', 'default', 'reloading']
+        },
+        includeStates: {
+            name: 'include states',
+            control: 'boolean'
+        },
+        showLegend: {
+            name: 'show legend',
+            control: 'boolean'
+        },
+        multiStateStartAngle: {
+            name: 'multi-state start angle',
+            control: {
+                type: 'range',
+                min: 0,
+                max: 360,
+                step: 1
+            }
+        },
+        customizeToday: {
+            name: 'customize today',
+            control: 'boolean'
+        },
+        customizeFuture: {
+            name: 'customize future',
+            control: 'boolean'
+        },
+        customizeNoData: {
+            name: 'customize no-data',
+            control: 'boolean'
+        },
+        showLogs: {
+            name: 'show logs',
+            control: 'boolean'
+        },
+        ...argTypesToHide(['previewState', 'surveyName', 'computeStatesForDay', 'answerRenderingConfigurations', 'innerRef'])
+    }
+};
---
+++ src/components/container/SurveyAnswerLogCalendar/SurveyAnswerLogCalendar.tsx
@@ -0,0 +1,126 @@
+import { Calendar, CalendarDay, CalendarDayState, Card, DateRangeContext, LayoutContext, LoadingIndicator, SurveyAnswerLogSummary } from '../../presentational';
+import React, { useContext, useState } from 'react';
+import { add, compareDesc, isAfter, startOfDay, startOfMonth } from 'date-fns';
+import { SurveyAnswer } from '@careevolution/mydatahelps-js';
+import { enterSurveyAnswerLog, getDayKey, loadSurveyAnswerLogs, resolveColor, SurveyAnswerLog, SurveyAnswerRenderingConfiguration, useInitializeView } from '../../../helpers';
+import './SurveyAnswerLogCalendar.css';
+import { generatePreviewSurveyAnswerLogs } from './SurveyAnswerLogCalendar.previewData';
+
+export interface SurveyAnswerLogCalendarProps {
+    previewState?: 'loading' | 'reloading' | 'default';
+    surveyName: string;
+    computeStatesForDay: (date: Date, surveyAnswers: SurveyAnswer[]) => CalendarDayState[];
+    multiStateStartAngle?: number;
+    legend?: CalendarDayState[];
+    answerRenderingConfigurations?: SurveyAnswerRenderingConfiguration[];
+    innerRef?: React.Ref<HTMLDivElement>;
+}
+
+export default function SurveyAnswerLogCalendar(props: SurveyAnswerLogCalendarProps) {
+    const layoutContext = useContext(LayoutContext);
+    const dateRangeContext = useContext(DateRangeContext);
+
+    const [loading, setLoading] = useState<boolean>(true);
+    const [surveyAnswerLogs, setSurveyAnswerLogs] = useState<Partial<Record<string, SurveyAnswerLog>>>({});
+
+    const intervalStart = startOfMonth(dateRangeContext?.intervalStart ?? new Date());
+    const intervalEnd = add(intervalStart, { months: 1 });
+
+    const currentSurveyAnswerLogs = Object.values(surveyAnswerLogs as Record<string, SurveyAnswerLog>)
+        .filter(surveyAnswerLog => surveyAnswerLog.date >= intervalStart && surveyAnswerLog.date < intervalEnd)
+        .sort((surveyAnswerLog1, surveyAnswerLog2) => compareDesc(surveyAnswerLog1.date, surveyAnswerLog2.date));
+
+    const applyPreviewState = (previewState: 'loading' | 'reloading' | 'default') => {
+        setSurveyAnswerLogs({});
+        if (previewState === 'loading') return;
+        generatePreviewSurveyAnswerLogs(props.answerRenderingConfigurations, startOfDay(add(new Date(), { months: -6 }))).then(surveyAnswerLogs => {
+            setSurveyAnswerLogs(surveyAnswerLogs);
+            if (previewState !== 'reloading') {
+                setLoading(false);
+            }
+        });
+    };
+
+    const loadState = () => {
+        loadSurveyAnswerLogs(props.surveyName).then(surveyAnswerLogs => {
+            setSurveyAnswerLogs(surveyAnswerLogs);
+            setLoading(false);
+        });
+    };
+
+    useInitializeView(() => {
+        setLoading(true);
+        if (props.previewState) {
+            applyPreviewState(props.previewState);
+            return;
+        }
+        loadState();
+    }, [], [props.previewState, props.surveyName, props.answerRenderingConfigurations, getDayKey(new Date())]);
+
+    const computeStatesForDay = (date: Date): CalendarDayState[] => {
+        const surveyAnswers = surveyAnswerLogs[getDayKey(date)]?.surveyAnswers ?? [];
+        const calendarDayStates = props.computeStatesForDay(date, surveyAnswers);
+        if (calendarDayStates.length > 0) return calendarDayStates;
+        if (isAfter(date, new Date())) return [{ style: { cursor: 'default' } }];
+        return [];
+    };
+
+    const onDayClicked = (date: Date): void => {
+        if (props.previewState || isAfter(date, new Date())) return;
+        enterSurveyAnswerLog(props.surveyName, surveyAnswerLogs[getDayKey(date)], date);
+    };
+
+    const renderDay = (year: number, month: number, day?: number): React.JSX.Element => {
+        return <CalendarDay
+            year={year}
+            month={month}
+            day={day}
+            computeStatesForDay={computeStatesForDay}
+            onClick={onDayClicked}
+            multiStateStartAngle={props.multiStateStartAngle}
+        />;
+    };
+
+    const onEnterLog = (priorSurveyAnswerLog: SurveyAnswerLog): void => {
+        if (props.previewState) return;
+        enterSurveyAnswerLog(props.surveyName, priorSurveyAnswerLog);
+    };
+
+    return <div ref={props.innerRef} className="mdhui-sa-log-calendar">
+        <Card>
+            <Calendar year={intervalStart.getFullYear()} month={intervalStart.getMonth()} dayRenderer={renderDay} />
+            {props.legend && props.legend.length > 0 &&
+                <div className="mdhui-sa-log-calendar-legend">
+                    {props.legend.map(state => {
+                        const backgroundColor = resolveColor(layoutContext.colorScheme, state.backgroundColor) ?? 'var(--mdhui-border-color-2)';
+                        const borderColor = resolveColor(layoutContext.colorScheme, state.borderColor) ?? backgroundColor;
+
+                        return <div key={state.label} className="mdhui-sa-log-calendar-legend-entry">
+                            <div className="mdhui-sa-log-calendar-legend-entry-color" style={{
+                                background: backgroundColor,
+                                border: `2px solid ${borderColor}`,
+                                ...state.style
+                            }}>&nbsp;</div>
+                            <div className="mdhui-sa-log-calendar-legend-entry-label">{state.label}</div>
+                        </div>;
+                    })}
+                </div>
+            }
+            {loading &&
+                <div className="mdhui-sa-log-calendar-loading-indicator-overlay">
+                    <LoadingIndicator className="mdhui-sa-log-calendar-loading-indicator" />
+                </div>
+            }
+        </Card>
+        {props.answerRenderingConfigurations && currentSurveyAnswerLogs.map((surveyAnswerLog, index) => {
+            return <Card key={index}>
+                <SurveyAnswerLogSummary
+                    surveyAnswerLog={surveyAnswerLog}
+                    onEdit={() => onEnterLog(surveyAnswerLog)}
+                    answerRenderingConfigurations={props.answerRenderingConfigurations!}
+                />
+                {loading && <div className="mdhui-sa-log-calendar-loading-indicator-overlay" />}
+            </Card>;
+        })}
+    </div>;
+}
