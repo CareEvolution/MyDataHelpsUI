ccf2eec3c85b6e644e69d09b47a09e03f2e38115
Garrett Reinard <garrett@careevolution.com>
2026-04-24 12:57:34 -0400

    Add a new UploadedFileAction component.

---
+++ src/components/container/UploadedFileAction/UploadedFileAction.tsx
@@ -0,0 +1,66 @@
+import React, { ReactElement, Ref, useState } from 'react';
+import { Action } from '../../presentational';
+import { FontAwesomeSvgIcon } from 'react-fontawesome-svg-icon';
+import { faFile } from '@fortawesome/free-solid-svg-icons';
+import MyDataHelps from '@careevolution/mydatahelps-js';
+import { UploadedFile } from '@careevolution/mydatahelps-js/types';
+import { ColorDefinition, useInitializeView } from '../../../helpers';
+import queryAllFiles from '../../../helpers/query-all-files';
+
+export interface UploadedFileActionProps {
+    preview?: boolean;
+    icon?: ReactElement;
+    title?: string;
+    titleColor?: ColorDefinition;
+    subtitle?: string;
+    subtitleColor?: ColorDefinition;
+    category: string;
+    fileNamePattern?: string | RegExp;
+    trackUsage?: boolean;
+    embedded?: boolean;
+    innerRef: Ref<HTMLDivElement>;
+}
+
+export default function UploadedFileAction(props: UploadedFileActionProps) {
+    const [file, setFile] = useState<UploadedFile>();
+
+    useInitializeView(async () => {
+        if (props.preview) {
+            setFile({} as UploadedFile);
+            return;
+        }
+
+        const allFiles = await queryAllFiles({ category: props.category });
+        if (allFiles.length > 0) {
+            const sortedFiles = allFiles.sort((a, b) => b.lastModified.localeCompare(a.lastModified));
+            setFile(props.fileNamePattern ? sortedFiles.find(file => file.fileName.match(props.fileNamePattern!)) : sortedFiles[0]);
+        }
+    }, [], [props.category, props.fileNamePattern]);
+
+    if (!file) return null;
+
+    const openFile = async (): Promise<void> => {
+        if (props.preview) return;
+
+        const downloadedFile = await MyDataHelps.getFileDownloadUrl(file.key);
+        if (props.trackUsage) {
+            void MyDataHelps.trackCustomEvent({ eventType: 'OpenUploadedFile', properties: { fileName: file.fileName, fileKey: file.key } });
+        }
+        if (props.embedded) {
+            MyDataHelps.openEmbeddedUrl(downloadedFile.preSignedUrl);
+        } else {
+            MyDataHelps.openExternalUrl(downloadedFile.preSignedUrl);
+        }
+    };
+
+    return <Action
+        icon={props.icon ?? <FontAwesomeSvgIcon icon={faFile} />}
+        title={props.title}
+        titleColor={props.titleColor}
+        subtitle={props.subtitle}
+        subtitleColor={props.subtitleColor}
+        bottomBorder
+        onClick={openFile}
+        innerRef={props.innerRef}
+    />;
+}
