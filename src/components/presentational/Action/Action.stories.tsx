6f16b56795a137cf2411fe5a366e2ef8d49a8051
Garrett Reinard <garrett@careevolution.com>
2026-05-12 14:15:50 -0400

    A few minor component updates.

--- src/components/presentational/Action/Action.stories.tsx
+++ src/components/presentational/Action/Action.stories.tsx
@@ -1,49 +1,152 @@
-﻿import React from "react";
-import Action, { ActionProps } from "./Action";
-import Layout from "../Layout";
+﻿import React from 'react';
+import Action, { ActionProps } from './Action';
+import Layout from '../Layout';
 import './Action.stories.css';
-import { FontAwesomeSvgIcon } from "react-fontawesome-svg-icon";
-import { faFile } from "@fortawesome/free-solid-svg-icons";
-import { Meta, StoryObj } from "@storybook/react";
+import { FontAwesomeSvgIcon } from 'react-fontawesome-svg-icon';
+import { faArrowRight, faFile, faHeart } from '@fortawesome/free-solid-svg-icons';
+import { Meta, StoryObj } from '@storybook/react';
+import { argTypesToHide } from '../../../../.storybook/helpers';
 
-const meta: Meta<typeof Action> = {
-	title: "Presentational/Action",
-	component: Action,
-	parameters: {
-		layout: 'fullscreen'
-	}
+type ActionStoryArgs = React.ComponentProps<typeof Action> & {
+    colorScheme: 'auto' | 'light' | 'dark';
+    withClass: boolean;
+    withIcon: boolean;
+    withTitleIcon: boolean;
+    withIndicator: boolean;
+    withIndicatorIcon: boolean;
+    withIndicatorValue: boolean;
+    withClickHandler: boolean;
+    parentCursor: 'not set' | 'pointer' | 'crosshair';
 };
 
+const meta: Meta<ActionStoryArgs> = {
+    title: 'Presentational/Action',
+    component: Action,
+    parameters: {
+        layout: 'fullscreen'
+    },
+    render: (storyArgs: ActionStoryArgs) => {
+        const componentArgs: ActionProps = {
+            ...storyArgs,
+            ...(storyArgs.withClass && { className: 'action-story-primary', renderAs: 'div' }),
+            ...(storyArgs.withIcon && { icon: <FontAwesomeSvgIcon icon={faFile} color="#2e6e9e" /> }),
+            ...(storyArgs.withTitleIcon && { titleIcon: <FontAwesomeSvgIcon icon={faHeart} color="#df4747" style={{ marginRight: '4px' }} /> }),
+            ...(storyArgs.withIndicator && { indicator: <FontAwesomeSvgIcon icon={faArrowRight} color="#2e6e9e" /> }),
+            ...(storyArgs.withIndicatorIcon && { indicatorIcon: faArrowRight }),
+            ...(storyArgs.withIndicatorValue && { indicatorValue: '3' }),
+            ...(storyArgs.withClickHandler && { onClick: () => alert('Clicked') })
+        };
+
+        return <Layout colorScheme={storyArgs.colorScheme}>
+            <div style={{ cursor: storyArgs.parentCursor }}>
+                <Action {...componentArgs} />
+                {componentArgs.bottomBorder && <div style={{ padding: '16px', fontSize: '0.75em', color: '#888' }}>
+                    Note: Adding this here because the bottom border seen above only renders when the action is not the last child of its parent.
+                </div>}
+            </div>
+        </Layout>;
+    }
+};
 export default meta;
-type Story = StoryObj<typeof Action>;
 
-const render = (args: ActionProps) =>
-	<Layout colorScheme="auto">
-		<Action {...args} />
-	</Layout>;
-
-const baseArgs : ActionProps = {
-	title: "Baseline Survey",
-	subtitle: "Tap here to start your baseline survey",
-	onClick: () => alert("Clicked")
+export const Default: StoryObj<ActionStoryArgs> = {
+    args: {
+        colorScheme: 'auto',
+        title: 'Baseline Survey',
+        subtitle: 'Tap here to start your baseline survey',
+        withClass: false,
+        withIcon: false,
+        withTitleIcon: false,
+        withIndicator: false,
+        withIndicatorIcon: false,
+        withIndicatorValue: false,
+        indicatorPosition: 'default',
+        bottomBorder: false,
+        withClickHandler: false,
+        titleColor: '',
+        subtitleColor: '',
+        parentCursor: 'not set',
+        renderAs: 'default' as any
+    },
+    argTypes: {
+        colorScheme: {
+            name: 'color scheme',
+            control: 'radio',
+            options: ['auto', 'light', 'dark']
+        },
+        title: {
+            name: 'title',
+            control: 'text'
+        },
+        subtitle: {
+            name: 'subtitle',
+            control: 'text'
+        },
+        withClass: {
+            name: 'with class',
+            control: 'boolean'
+        },
+        withIcon: {
+            name: 'with icon',
+            control: 'boolean'
+        },
+        withTitleIcon: {
+            name: 'with title icon',
+            control: 'boolean'
+        },
+        withIndicator: {
+            name: 'with indicator',
+            control: 'boolean'
+        },
+        withIndicatorIcon: {
+            name: 'with indicator icon',
+            control: 'boolean'
+        },
+        withIndicatorValue: {
+            name: 'with indicator value',
+            control: 'boolean'
+        },
+        indicatorPosition: {
+            name: 'indicator position',
+            control: 'radio',
+            options: ['default', 'topRight']
+        },
+        bottomBorder: {
+            name: 'with bottom border',
+            control: 'boolean'
+        },
+        withClickHandler: {
+            name: 'with click handler',
+            control: 'boolean'
+        },
+        titleColor: {
+            name: 'title color',
+            control: 'color'
+        },
+        subtitleColor: {
+            name: 'subtitle color',
+            control: 'color'
+        },
+        parentCursor: {
+            name: 'parent cursor',
+            control: 'radio',
+            options: ['not set', 'pointer', 'crosshair'],
+            mapping: {
+                ['not set']: undefined
+            }
+        },
+        renderAs: {
+            name: 'render as',
+            control: 'radio',
+            options: ['default', 'button', 'div'],
+            mapping: {
+                default: undefined
+            }
+        },
+        ...argTypesToHide([
+            'titleIcon', 'icon', 'onClick', 'className',
+            'indicator', 'indicatorIcon', 'indicatorValue',
+            'innerRef'
+        ])
+    }
 };
-
-export const WithClickEvent: Story = {
-	args: {...baseArgs},
-	render: render
-}
-
-export const WithClass: Story = {
-	args: {...baseArgs, className: "action-story-primary", renderAs: "div"},
-	render: render
-}
-
-export const WithIcon: Story = {
-	args: {...baseArgs, icon: <FontAwesomeSvgIcon icon={faFile} color="#2e6e9e" />},
-	render: render
-}
-
-export const WithTitleIcon: Story = {
-	args: {...baseArgs, titleIcon: <FontAwesomeSvgIcon icon={faFile} color="#2e6e9e" />},
-	render: render
-}
--- src/components/presentational/Card/Card.stories.tsx
+++ src/components/presentational/Card/Card.stories.tsx
@@ -3,6 +3,7 @@ import Action from "../Action/Action";
 import Layout from "../Layout"
 import Card, { CardProps } from "./Card"
 import "./Card.stories.css";
+import { Goal } from '../../container';
 
 export default {
 	title: "Presentational/Card",
@@ -41,6 +42,20 @@ export const HighlightVariant = {
 	args: {
 		variant: "highlight",
 		children: <Action onClick={() => {}} title="Baseline Survey" subtitle="Tap here to start your baseline survey" />
+	},
+	render: render
+};
+
+export const ClickableCard = {
+	args: {
+		children: <>
+			<Action title="See more details" bottomBorder />
+			<div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+				<Goal variant="compact" label="Goal 1" targetValue={5} maxValue={10} valueProvider={{ type: 'static integer', getValue: async () => 7 }} />
+				<Goal variant="compact" label="Goal 2" targetValue={5} maxValue={10} valueProvider={{ type: 'static integer', getValue: async () => 3 }} />
+			</div>
+		</>,
+		onClick: () => console.log("Card Clicked!")
 	},
 	render: render
 };
