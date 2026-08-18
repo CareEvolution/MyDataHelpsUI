186e99537da6d9576d56fe9511003929fceb0504
Garrett Reinard <garrett@careevolution.com>
2026-08-18 13:09:15 -0400

    Fix therapy mins data provider silvercloud filtering.

--- src/helpers/daily-data-providers/common-mindful-and-therapy.ts
+++ src/helpers/daily-data-providers/common-mindful-and-therapy.ts
@@ -1,15 +1,6 @@
-import { DeviceDataPoint, DeviceDataV2Point } from '@careevolution/mydatahelps-js';
+import { DeviceDataV2Point } from '@careevolution/mydatahelps-js';
 
-export function isSilverCloudCbtDataPoint(dataPoint: DeviceDataPoint | DeviceDataV2Point): boolean {
-    if (dataPoint.properties?.['Metadata_sub-type'] !== 'CBT') return false;
-
-    if ("dataSource" in dataPoint) {
-        return dataPoint.dataSource?.['SourceIdentifier'] === 'com.silvercloudhealth.SilverCloud';
-    }
-
-    if ("source" in dataPoint) {
-        return dataPoint.source?.properties?.['SourceIdentifier'] === 'com.silvercloudhealth.SilverCloud';
-    }
-
-    return false;
+export function isSilverCloudCbtDataPoint(dataPoint: DeviceDataV2Point): boolean {
+    if (dataPoint.properties?.['metadata_sub-type'] !== 'CBT') return false;
+    return dataPoint.dataSource?.['sourceIdentifier'] === 'com.silvercloudhealth.SilverCloud';
 }
